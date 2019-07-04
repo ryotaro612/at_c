d4f588f8b35d8a0af195e3ed8e17bd5ef744cabb
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2019-07-04 13:18:15 +0900

    fifty_fifty

--- src/abc132/fifty_fifty.cc
+++ src/abc132/fifty_fifty.cc
@@ -3,6 +3,9 @@
 #include <string>
 #include "fifty_fifty.h"
 
+// https://atcoder.jp/contests/abc132
+// ctest -V -R Test*
+// ctest --test-timeout
 std::string solve_fifty_fifty(std::string arg) {
     return std::string("No");
 }
--- tests/abc132/test_fifty_fifty.cc
+++ tests/abc132/test_fifty_fifty.cc
@@ -7,10 +7,10 @@
 TEST(TestFiftyFifty, First) {
     /* 
     char s[] = {'A', 'C', 'D', 'B', 'C','B'};
-    std::string ans = "ABCBCD";
     EXPECT_EQ(ans, solve_best_cow_line(6,s));
     */
-    EXPECT_EQ(1, 1);
+    std::string arg = "ASSA";
+    EXPECT_EQ("Yes", solve_fifty_fifty(arg));
 }
 
 
