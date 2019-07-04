46a59946ae2002becab2a4f18ba4a41e382c995e
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2019-07-04 13:03:42 +0900

    at_c

---
+++ tests/abc132/test_fifty_fifty.cc
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include "gtest/gtest.h"
+#include "abc132/fifty_fifty.h"
+
+TEST(TestFiftyFifty, First) {
+    /* 
+    char s[] = {'A', 'C', 'D', 'B', 'C','B'};
+    std::string ans = "ABCBCD";
+    EXPECT_EQ(ans, solve_best_cow_line(6,s));
+    */
+    EXPECT_EQ(1, 1);
+}
+
+
