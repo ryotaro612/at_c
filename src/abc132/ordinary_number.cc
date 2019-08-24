a9389eb093c9d20b660295777b9031a5c3d3a6fa
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2019-08-24 12:42:09 +0900

    ordinary_number

--- src/abc132/ordinary_number.cc
+++ src/abc132/ordinary_number.cc
@@ -1,8 +1,31 @@
 #include<algorithm>
 #include<string>
+#include<iostream>
+#include<list>
 #include <boost/algorithm/string.hpp>
 #include "ordinary_number.h"
 
+using namespace std;
+
 int ordinary_number(int n, std::string arg) {
-    return 0;
+
+    vector<string> strs;
+    boost::split(strs, arg, boost::is_space());
+    vector<int> nums(n);
+
+    for(int i=0;i<n;i++)
+        nums[i] = stoi(strs.at(i));
+
+    int peek = 1;
+    int count = 0;
+
+    while(peek < n-1) {
+        if(nums.at(peek-1) < nums.at(peek) && nums.at(peek) < nums.at(peek+1)) {
+            count++;
+        } else if (nums.at(peek+1) < nums.at(peek) && nums.at(peek) < nums.at(peek-1)) {
+            count++;
+        }
+        peek++;
+    }
+    return count;
 }
--- tests/abc132/test_ordinary_number.cc
+++ tests/abc132/test_ordinary_number.cc
@@ -2,8 +2,11 @@
 #include "gtest/gtest.h"
 #include "abc132/ordinary_number.h"
 
-
 TEST(TestOrdinaryNumber, First) {
     std::string arg = "1 3 5 4 2";
     EXPECT_EQ(2, ordinary_number(5, arg));
+}
+TEST(TestOrdinaryNumber, Second) {
+    std::string arg = "9 6 3 2 5 8 7 4 1";
+    EXPECT_EQ(5, ordinary_number(9, arg));
 }
