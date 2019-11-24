660d5a27ddae4af0e84e13fbbe2ddcc24d38099b
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2019-11-24 09:43:55 +0900

    up

--- src/abc128/enqueue.cc
+++ src/abc128/enqueue.cc
@@ -9,10 +9,10 @@ using namespace std;
 // 10,000,000 -> 1s
 long long enqueue(int n, int k, long long v[]) {
   int threshold = min(n, k);
-  priority_queue<long long, vector<long long>, greater<long>> que;
   long long ans = 0;
-  for(int max_fetch=0;max_fetch<=threshold;max_fetch++) {
+  for(int max_fetch=1;max_fetch<=threshold;max_fetch++) {
     for(int left = 0; left<=max_fetch;left++) {
+      priority_queue<long long, vector<long long>, greater<long long>> que;      
       int right = max_fetch - left;
       long long value = 0;
 
@@ -20,18 +20,20 @@ long long enqueue(int n, int k, long long v[]) {
 	value +=v[i];
 	que.push(v[i]);
       }
-      for(int j=right-1;j>=0;j--) {
+      // for(int j=right-1;j>=0;j--) {
+      for(int j=0;j<right;j++) {
 	value += v[n-1-j];
 	que.push(v[n-1-j]);
       }
       int max_enqueue = threshold - max_fetch;
       while(true) {
-	if(que.empty() || que.top() >= 0 || max_enqueue-- <= 0) {
+	if(que.empty() || que.top() >= 0 || max_enqueue <= 0) {
 	  break;
 	}
 	auto item = que.top();
 	value -= item;
 	que.pop();
+	max_enqueue--;
       }
       ans = max(ans, value);
     }
--- tests/abc128/test_enqueue.cc
+++ tests/abc128/test_enqueue.cc
@@ -19,3 +19,25 @@ TEST(TestEnqueue, Third) {
   long long v[] = {-6, -100, 50, -2, -5, -3};
   EXPECT_EQ(0, enqueue(6, 3, v));
 }
+
+TEST(TestEnqueue, Fourth) {
+  long long v[] = {-10};
+  EXPECT_EQ(0, enqueue(1, 1, v));
+}
+
+TEST(TestEnqueue, Fifth) {
+  long long v[] = {10, 10};
+  EXPECT_EQ(20, enqueue(2, 5, v));
+}
+
+
+TEST(TestEnqueue, Sixth) {
+  long long v[] = {10, -10};
+  EXPECT_EQ(10, enqueue(2, 5, v));
+}
+
+
+TEST(TestEnqueue, Seventh) {
+  long long v[] = {-10, 10, -10};
+  EXPECT_EQ(0, enqueue(3, 2, v));
+}
