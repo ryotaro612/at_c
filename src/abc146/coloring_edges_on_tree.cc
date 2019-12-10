99a2e064cbc81fd6b22f798e5f60b922d045f495
Nakamura, Ryotaro <nryotaro@nryotaro.local>
2019-12-10 12:46:41 +0900

    test_coloring edges on tree

--- src/abc146/coloring_edges_on_tree.cc
+++ src/abc146/coloring_edges_on_tree.cc
@@ -4,36 +4,82 @@
 #include <vector>
 #include <utility>
 #include <queue>
+#include <set>
 #include <map>
 
 using namespace std;
 
-vector<int> coloring_edges_on_tree(int n, int a[], int b[]) {
-  vector<vector<int>> graph;
-  for(int i=0;i<n;i++) {
+
+pair<int, int> get_key(int a, int b) {
+  if(a > b)
+    return make_pair(b, a);
+  return make_pair(a, b);
+}
+
+vector<int> coloring_edges_on_tree(int n, vector<int> a, vector<int> b) {
+  vector<vector<int>> graph(n);
+  for(int i=0;i<a.size();i++) {    
     int vertex_a = a[i] - 1;
     int vertex_b = b[i] - 1;
-    graph[vertex_a].push_back(vertex_b);
-    graph[vertex_b].push_back(vertex_a);
+    graph[vertex_a].emplace_back(vertex_b);
+    graph[vertex_b].emplace_back(vertex_a);
   }
+  
   int color_num = 0;  
   queue<int> que;
   map<pair<int, int>, int> mp;
+
+  int used[100000] = {};
   que.push(0);
-  vector<int> ans;
+  vector<int> ans;  
   while(!que.empty()) {
+    //    cout << "@@@@@@111111111111111" << endl;    
     int vertex = que.front();
+    que.pop();    
     vector<int> vertex_vec = graph[vertex];
     int degree = vertex_vec.size();
-    color_num = color_num > degree ? color_num : degree;
-
-    for(int i=0; i< vertex_vec.size(); i++){
-      int vertex2 = vertex_vec[i];
-      make_pair(_T1 &&__t1, _T2 &&__t2)
+    color_num = color_num > degree ? color_num : degree; //最大はあっている。
+    // cout << "@@@@@@22222222222" << endl;
+    set<int> colors;
+    int color_index = 1;
+    for(int j=0;j<vertex_vec.size();j++) {
+      int vertex2 = vertex_vec[j];
+      if(used[vertex2] == 1)
+	colors.insert(mp[get_key(vertex, vertex2)]);
     }
+    //
+    cout << vertex << "#####" << endl;
+    set<int>::iterator iter = colors.begin();
+    while (iter != colors.end())
+      cout << *iter++ << " ";
+    cout << endl;
+    cout << "#####" << endl;
+    // cout << "@@@@@" << endl;
+    for(int i=0;i<vertex_vec.size();i++) {
+      int vertex2 = vertex_vec[i];
+      if(used[vertex2] == 1)
+	continue;
 
-    que.pop();
+      que.push(vertex2);
+      
+      pair<int, int> key = get_key(vertex, vertex2);
+
+      while(true) {
+	if(colors.count(color_index) == 1) {
+	  color_index++;
+	  continue;
+	} else {
+	  break;
+	}
+      }
+      mp[key] = color_index++;
+      
+    }
+    used[vertex] = 1;
   }
-
+  ans.push_back(color_num);
+  for(int i = 0; i< a.size();i++) {
+    ans.push_back(mp[get_key(a[i]-1, b[i]-1)]);
+  }
   return ans;
 }
--- src/abc146/coloring_edges_on_tree.h
+++ src/abc146/coloring_edges_on_tree.h
@@ -1,3 +1,3 @@
 
 
-std::vector<int> coloring_edges_on_tree(int n, int a[], int b[]);
+std::vector<int> coloring_edges_on_tree(int n, std::vector<int> a, std::vector<int> b);
