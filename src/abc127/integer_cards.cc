99f4a51d20b01b67b763826d5ea515e77729f3b5
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2019-12-22 19:25:33 +0900

    pass

--- src/abc127/integer_cards.cc
+++ src/abc127/integer_cards.cc
@@ -1,53 +1,49 @@
 #include <iostream>
 #include <algorithm>
+#include <utility>
 #include <vector>
 #include <functional>
 using namespace std;
-struct Replacement {
-  int bb;
-  long long cc;
 
-  bool operator>(const Replacement &another) const {
-    return cc > another.cc;
-  }
-};
-
+// http://drken1215.hatenablog.com/entry/2019/06/15/021000
 long long integer_cards(int n, int m, long long a[], int b[], long long c[]) {
-  vector<long long> cc;
-  for(int i=0;i<m;i++) {
-    for(int j=0;j<b[i];j++) {
-     cc.push_back(c[i]);
-    }
+  vector<pair<long long, int>> aa;
+  for(int i=0; i<n;i++) {
+    aa.push_back(make_pair(a[i], 1));
   }
+  for(int i=0;i<m;i++) {
+    aa.push_back(make_pair(c[i], b[i]));
+  }
+  sort(aa.begin(), aa.end(), greater<pair<long long, int>>());
+  int cons = 0;
   long long ans = 0;
-  sort(cc.begin(), cc.end(), greater<long long>());
-  long long peek = 0;
-  for(int i=0;i<n;i++) {
-    if(peek < cc.size()) {
-      if(a[i] < cc[peek]) {
-	ans += cc[peek];
-        peek++;
-	continue;
-      }
-      ans += a[i];
-      continue;
-    } 
-    ans += a[i];
+  int index = 0;
+  while(cons < n) {
+    int size = n - cons > aa[index].second ? aa[index].second : n - cons;
+    //cout << "index: " << index << ", item: " << aa[index].first << ", " << "size:" << size << endl;
+    ans += aa[index].first * size;
+    index++;
+    cons += size;
   }
-  /*
-  vector<Replacement> bc;
-  for(int i=0;i<m;i++) {
-    Replacement replacement = {b[i], c[i]};
-    bc.push_back(replacement);
-  }
-  sort(bc.begin(), bc.end(), greater<Replacement>());
-  sort(a, a + n);
-
-  */
   return ans;
 }
-
 /*
 int main() {
+  int n;
+  cin >> n;
+  int m;
+  cin >> m;
+  long long a[n];
+  for(int i=0; i<n; i++) {
+    cin >> a[i];
+  }
+  int b[m];
+  long long c[m];
+  for(int i =0;i<m;i++) {
+    cin >> b[i];
+    cin >> c[i];
+  }
+  auto ans = integer_cards(n, m, a, b, c);
+  cout << ans;
 }
 */
