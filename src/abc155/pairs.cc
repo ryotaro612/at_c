1e30e09d99c84a22ed4cc0e9c30f190a2c1433d7
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2020-02-19 12:52:46 +0900

    155

--- src/abc155/pairs.cc
+++ src/abc155/pairs.cc
@@ -1,12 +1,61 @@
 #include <iostream>
+#include <algorithm>
+#include <cmath>
 #include <vector>
 using namespace std;
 
-long long pairs(int n, int k, std::vector<long long> a) {
-  return -1;
+long long pairs(int n, int k, vector<long long> a) {
+  sort(a.begin(), a.end());
+  auto it = lower_bound(a.begin(), a.end(), 0);
+  vector<long long> minus_a(a.begin(), it);
+  auto it_n = upper_bound(a.begin(), a.end(), 0);
+  vector<long long> zero_a(it, it_n);
+  vector<long long> plus_a(it_n, a.end());
+
+  // マイナスか?
+  if(k <= minus_a.size()*plus_a.size()) {
+    cout << "===enter-minus====" << endl;
+    int left = minus_a[0] * plus_a[plus_a.size() - 1];
+    int right = 0;
+    int mid;
+    while(left +1 != right) {
+      mid = (left + right) / 2;
+      int count = 0;
+      for(int i=minus_a.size() - 1;i>=0;i--) {
+	for(int j=plus_a.size() - 1; j>=0;j--) {
+	  cout << "multiple: " << minus_a[i]*plus_a[j] << endl;
+	  long long mul = minus_a[i]*plus_a[j];
+	  if(mul <= mid) {
+	    count++;
+	    continue;
+	  }
+	  break;
+	}
+      }
+      cout << "left: " << left << " right: " << right << " mid: " << mid << " count: " << count << endl;
+      if(count >= k) {
+	right = mid;
+	continue;
+      }
+      left = mid;
+    }
+    return mid;
+  }
+
+  return 0;
 }
+
 /*
 int main() {
+  int n, k;
+  cin >> n;
+  cin >> k;
+  vector<long long> a;
+  for(int i=0;i<n;i++) {
+    long long temp;
+    cin >> temp;
+    a.push_back(temp);
+  }
+  cout << pairs(n, k, a);
 }
 */
-
--- tests/abc155/test_pairs.cc
+++ tests/abc155/test_pairs.cc
@@ -4,17 +4,28 @@
 
 using namespace std;
 
-TEST(Pairs, First) {
+TEST(Pairs, 1) {
   vector<long long> a = {3, 3, -4, -2};
   EXPECT_EQ(-6, pairs(4, 3, a));
 }
 
-TEST(Pairs, Second) {
+TEST(Pairs, 2) {
   vector<long long> a = {5, 4, 3, 2, -1, 0, 0, 0, 0, 0};
   EXPECT_EQ(6, pairs(10, 40, a));
 }
 
-TEST(Pairs, Third) {
+TEST(Pairs, 3) {
   vector<long long> a = {-170202098, -268409015, 537203564, 983211703, 21608710, -443999067, -937727165, -97596546, -372334013, 398994917, -972141167, 798607104, -949068442, -959948616, 37909651, 0, 886627544, -20098238, 0, -948955241, 0, -214720580, 277222296, -18897162, 834475626, 0, -425610555, 110117526, 663621752, 0};
   EXPECT_EQ(448283280358331064, pairs(30, 413, a));
+}
+
+TEST(Pairs, 4) {
+  vector<long long> a = {-1, 2};
+  EXPECT_EQ(-2, pairs(a.size(), 1, a));
+}
+
+TEST(Pairs, 5) {
+  vector<long long> a = {-2, -1, 0, 2, 1};
+  // -4 -2  -2 -1
+  EXPECT_EQ(-2, pairs(a.size(), 2, a));
 }
