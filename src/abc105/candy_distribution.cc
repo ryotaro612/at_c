114ccb80677c32e86d6f4a21bc8b5efc9835a752
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2020-02-24 13:04:46 +0900

    d

--- src/abc105/candy_distribution.cc
+++ src/abc105/candy_distribution.cc
@@ -3,9 +3,9 @@
 #include<map>
 #include<utility>
 #include "candy_distribution.h"
-
+ 
 using namespace std;
-
+ 
 long long candy_distribution(int n, long long m, std::vector<long long> a) {
   map<long long, long long> mp;
   long long sum = 0;
@@ -14,7 +14,7 @@ long long candy_distribution(int n, long long m, std::vector<long long> a) {
     sum += a[i];
     mp[sum % m]++;
   }
-
+ 
   long long ans = 0;
   
   for (auto i=mp.begin(); i !=mp.end();++i) {
@@ -22,10 +22,10 @@ long long candy_distribution(int n, long long m, std::vector<long long> a) {
     long long size = i->second;
     ans += (size*(size-1))/2;
   }
-
+ 
   return ans;
 }
-/*
+/* 
 int main() {
   int n;
   long long m;
@@ -33,7 +33,9 @@ int main() {
   cin >> m;
   vector<long long> a;
   for(int i=0;i<n;i++) {
-    cin >> a[i];
+    long long a_i;
+    cin >> a_i;
+    a.push_back(a_i);
   }
   cout << candy_distribution(n, m, a);
 }
