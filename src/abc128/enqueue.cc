7ac2598a2bf1e427e1cc4a9f9883f2a4561486b1
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2020-07-22 11:57:28 +0900

    pass abc128

--- src/abc128/enqueue.cc
+++ src/abc128/enqueue.cc
@@ -3,40 +3,54 @@
 #include <string>
 #include <algorithm>
 #include <limits>
+typedef long long ll;
 
 using namespace std;
 
 // 10,000,000 -> 1s
 long long enqueue(int n, int k, long long v[]) {
-  int threshold = min(n, k);
-  long long ans = 0;
-  for(int max_fetch=1;max_fetch<=threshold;max_fetch++) {
-    for(int left = 0; left<=max_fetch;left++) {
-      priority_queue<long long, vector<long long>, greater<long long>> que;      
-      int right = max_fetch - left;
-      long long value = 0;
-
-      for(int i =0;i<left;i++) {
-	value +=v[i];
-	que.push(v[i]);
+  ll ans = 0;
+  for (int kk=0;kk<=k;kk++) {
+    for(int num_pick = 0; num_pick <= kk; num_pick++) {
+      if(n < num_pick) {
+	continue;
       }
-      // for(int j=right-1;j>=0;j--) {
-      for(int j=0;j<right;j++) {
-	value += v[n-1-j];
-	que.push(v[n-1-j]);
+      int num_remove = kk - num_pick;
+      if(num_pick < num_remove) {
+	continue;
       }
-      int max_enqueue = threshold - max_fetch;
-      while(true) {
-	if(que.empty() || que.top() >= 0 || max_enqueue <= 0) {
-	  break;
+      for(int a = 0; a<=num_pick; a++) {
+	priority_queue<ll, vector<ll>, greater<ll>> que;      
+	for(int aa =0;aa<a;aa++) {
+	  que.push(v[aa]);
 	}
-	auto item = que.top();
-	value -= item;
-	que.pop();
-	max_enqueue--;
+	int b= num_pick - a;
+	for(int bb = 0;bb<b;bb++) {
+	  que.push(v[n - 1 - bb]);
+	}
+	for(int i =0;i<num_remove;i++) {
+	  que.pop();
+	}
+	ll temp = 0;
+	while(!que.empty()) {
+	  temp += que.top();
+	  que.pop();
+	}
+	ans = max(ans, temp);
       }
-      ans = max(ans, value);
     }
   }
+
   return ans;
 }
+/*
+int main() {
+  int n, k;
+  cin >> n >> k;
+  ll v [n];
+  for(int i=0;i<n;i++) {
+    cin >> v[i];
+  }
+  cout << enqueue(n, k, v);
+}
+*/
--- tests/abc128/test_enqueue.cc
+++ tests/abc128/test_enqueue.cc
@@ -5,39 +5,39 @@
 
 using namespace std;
 
-TEST(TestEnqueue, First) {
+TEST(Enqueue, 1) {
   long long v[] = {-10, 8, 2, 1, 2, 6};
   EXPECT_EQ(14, enqueue(6, 4, v));
 }
 
-TEST(TestEnqueue, Second) {
+TEST(Enqueue, 2) {
   long long v[] = {-6, -100, 50, -2, -5, -3};
   EXPECT_EQ(44, enqueue(6, 4, v));
 }
 
-TEST(TestEnqueue, Third) {
+TEST(Enqueue, 3) {
   long long v[] = {-6, -100, 50, -2, -5, -3};
   EXPECT_EQ(0, enqueue(6, 3, v));
 }
 
-TEST(TestEnqueue, Fourth) {
+TEST(Enqueue, 4) {
   long long v[] = {-10};
   EXPECT_EQ(0, enqueue(1, 1, v));
 }
 
-TEST(TestEnqueue, Fifth) {
+TEST(Enqueue, 5) {
   long long v[] = {10, 10};
   EXPECT_EQ(20, enqueue(2, 5, v));
 }
 
 
-TEST(TestEnqueue, Sixth) {
+TEST(Enqueue, 6) {
   long long v[] = {10, -10};
   EXPECT_EQ(10, enqueue(2, 5, v));
 }
 
 
-TEST(TestEnqueue, Seventh) {
+TEST(Enqueue, 7) {
   long long v[] = {-10, 10, -10};
   EXPECT_EQ(0, enqueue(3, 2, v));
 }
