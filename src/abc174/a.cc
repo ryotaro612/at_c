2daed373eea53ef12e1996694345fdc748a3a277
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2020-08-30 18:04:28 +0900

    a

---
+++ src/abc174/a.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+typedef long long ll;
+using namespace std;
+
+string a(int x) {
+  return x >= 30 ? "Yes" : "No";
+}
+/*
+int main() {
+  int x;
+  cin >> x;
+  cout << a(x);
+}
+*/ 
