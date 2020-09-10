2e6e9ebb7ceeccb8f9bd30b05184ffaeabbe9e20
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2020-09-10 11:43:18 +0900

    abc171a

---
+++ src/abc171/a.cc
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+char a(char alpha) {
+  if(isupper(alpha)) {
+    return 'A';
+  }
+  return 'a';
+
+}
+/*
+int main() {
+  char alpha;
+  cin >> alpha; 
+  cout << a(alpha);
+}
+*/
