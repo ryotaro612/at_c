ded772578953a6996cc8c9aecf114690044db0e1
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2020-09-13 16:11:15 +0900

    99a

---
+++ src/abc099/a.cc
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+string a(int n) {
+  return n <1000 ? "ABC" : "ABD";
+}
+/*
+int main() {
+  int n;
+  cin >> n;
+  cout << a(n);
+}
+*/
