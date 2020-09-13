7ae026b8c0d2648ba8e0038aed7da99d4274dcfb
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2020-09-13 12:32:09 +0900

    abc170b

--- src/abc170/b.cc
+++ src/abc170/b.cc
@@ -2,25 +2,19 @@
 using namespace std;
 typedef long long ll;
 
-int b(int x1, int x2, int x3, int x4, int x5) {
-  if(x1 == 0) {
-    return 1;
+
+string b(int x, int y) {
+  for(int turtle=0;turtle<=x; turtle++) {
+    if(turtle  * 4 + 2 * (x - turtle) == y) {
+      return "Yes";
+    }
   }
-  if(x2 == 0) {
-    return 2;
-  }
-  if(x3 == 0) {
-    return 3;
-  }
-  if(x4 == 0) {
-    return 4;
-  }
-  return 5;
+  return "No";
 }
 /*
 int main() {
-  int x1, x2, x3, x4, x5;
-  cin >> x1 >> x2 >> x3 >> x4 >> x5;
-  cout << a(x1, x2, x3, x4, x5);
+  int x, y;
+  cin >> x >> y;
+  cout << b(x, y);
 }
 */
