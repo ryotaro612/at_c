1a8a0965ff1eda2dcf28bbffb6d5211982006573
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2020-09-16 12:19:23 +0900

    ac099

--- src/abc099/c.cc
+++ src/abc099/c.cc
@@ -4,15 +4,33 @@ typedef long long ll;
 
 
 int c(int n) {
-  int d = n / 3;
-
-  return -1;
+  int res = n;
+  for(int six=0;six<=n;six++) {
+    int count = 0;
+    int temp = six;
+    for(int i=0;temp>0;i++) {
+      int p = pow(6, i);
+      int cnt = ((int) (six / p)) % 6;
+      temp -= cnt * p;
+      count += cnt;
+    }
+    //cout << six << " -> " << count << endl;
+    int nine = n - six;
+    temp = nine;
+    for(int i=0;temp>0;i++) {
+      int p = pow(9, i);
+      int cnt = ((int) (nine / p)) % 9;
+      temp -= cnt * p;
+      count += cnt;
+    }
+    res = min(count, res);
+  }
+  return res;
 }
 /*
 int main() {
   int n;
   cin >> n;
   cout << c(n);
-
 }
 */
