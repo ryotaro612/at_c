43e4e16ed2d007e0e2d72bb4261c063e8cd8f81f
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2020-10-07 10:46:57 +0900

    format

--- src/abc093/d.cc
+++ src/abc093/d.cc
@@ -2,33 +2,30 @@
 using namespace std;
 typedef long long ll;
 
-ll sub(ll a, ll b){
+ll sub(ll a, ll b) {
+  if (a == b)
+    return 2 * a - 2;
 
-    if (a == b)
-        return 2 * a - 2;
+  ll lb = -1, ub = 1LL << 31;
 
-    ll lb = -1, ub = 1LL << 31;
+  while (ub - lb > 1) {
+    ll mid = (lb + ub) / 2;
 
-    while (ub - lb > 1) {
-        ll mid = (lb + ub) / 2;
-
-        if((mid/2 + 1)*(mid-mid/2) > a*b - 1) {
-            ub = mid;
-        } else {
-            lb = mid;
-        }
+    if ((mid / 2 + 1) * (mid - mid / 2) > a * b - 1) {
+      ub = mid;
+    } else {
+      lb = mid;
     }
-    return lb -1;
+  }
+  return lb - 1;
 }
 
-vector<ll> solve(int q, vector<ll> a, vector<ll> b)
-{
-    vector<ll> res(q);
-    for (int i = 0; i < q; i++)
-    {
-        res[i] = sub(a[i], b[i]);
-    }
-    return res;
+vector<ll> solve(int q, vector<ll> a, vector<ll> b) {
+  vector<ll> res(q);
+  for (int i = 0; i < q; i++) {
+    res[i] = sub(a[i], b[i]);
+  }
+  return res;
 }
 /*
 int main() {
