8b28372341b4cf7dffa4d0a05c2bfb8c674f1214
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2020-10-18 12:51:00 +0900

    abc090

--- src/abc090/d.cc
+++ src/abc090/d.cc
@@ -4,16 +4,13 @@ typedef long long ll;
 
 ll solve(int n, int k) {
   ll res = 0;
-  //cout << "n: " << n << " k: " << k << endl;
   for (int b = 1;b<=n; b++) {
     if(k >= b) {
       continue;
     }
     int div = n / b;
-    //cout << "b: " << b << " div: " << div << " per: " << b - k << " -> " << div * (b - k) << " ";
     res += div * (b - k);
     int tail = min(n, div * b + b - 1) - (div * b + k) + 1;
-    //cout << "tail: " << tail << endl;
     if(k == 0) {
       res--;
     }
