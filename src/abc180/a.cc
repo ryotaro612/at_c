7677cdd953eb395934b24aa65dba4bfa1211caf5
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2020-10-19 11:03:56 +0900

    a

---
+++ src/abc180/a.cc
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int solve(int n, int a, int b) {
+    return n - a + b;
+}
+/*
+int main() {
+    int n, a, b;
+    cin >> n >> a >> b;
+    cout << solve(n, a, b);
+}
+*/
