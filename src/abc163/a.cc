5350ec2ff0de1b84dfc4c67c3022569e22bf9ad2
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2020-10-25 11:26:03 +0900

    a

---
+++ src/abc163/a.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+double solve(int r) {
+  const double pi = acos(-1);
+  return 2 * (double) r * pi;
+}
+/*
+int main() {
+  int r;
+  cin >> r;
+  cout << fixed << setprecision(15) << solve(r);
+}
+*/
