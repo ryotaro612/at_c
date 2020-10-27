f57db0999880a14f5570fbe792475da53c94636f
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2020-10-27 12:26:44 +0900

    87

---
+++ src/abc087/d.cc
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define REP(i, n) for (int i = 0; (i) < int(n); ++(i))
+
+const ll MX = 200000000;
+struct edge {
+  ll to;
+  ll cost;
+};
+
+bool dfs(ll from, vector<vector<edge>> &edges, vector<ll> &d) {
+  for (edge e : edges[from]) {
+    if (d[e.to] == MX) {
+      d[e.to] = d[from] + e.cost;
+      if (!dfs(e.to, edges, d)) {
+        return false;
+      }
+    } else if (d[e.to] != d[from] + e.cost) {
+      return false;
+    }
+  }
+  return true;
+}
+
+string solve(ll n, ll m, vector<ll> l, vector<ll> r, vector<ll> d) {
+  if (m == 0) {
+    return "Yes";
+  }
+  vector<vector<edge>> edges(n, vector<edge>());
+  for (ll i = 0; i < m; i++) {
+    edges[l[i] - 1].push_back({r[i] - 1, d[i]});
+    edges[r[i] - 1].push_back({l[i] - 1, -d[i]});
+  }
+  vector<ll> dist(n, MX);
+  for (ll i = 0; i < n; i++) {
+    if (dist[i] == MX) {
+      dist[i] = 0;
+      if (!dfs(i, edges, dist)) {
+        return "No";
+      }
+    }
+  }
+
+  return "Yes";
+}
+/*
+int main() {
+  ll n, m;
+  cin >> n >> m;
+  vector<ll> l(m), r(m), d(m);
+  for (ll i = 0; i < m; i++) {
+    cin >> l[i] >> r[i] >> d[i];
+  }
+  cout << solve(n, m, l, r, d);
+}
+*/
---
+++ tests/abc087/d.cc
@@ -0,0 +1,43 @@
+#include "abc087/d.cc"
+
+#include <gtest/gtest.h>
+
+TEST(abc087d, 1) {
+  vector<ll> l = {1, 2, 1};
+  vector<ll> r = {2, 3, 3};
+  vector<ll> d = {1, 1, 2};
+
+  EXPECT_EQ("Yes", solve(3, 3, l, r, d));
+}
+
+TEST(abc087d, 2) {
+  vector<ll> l = {1, 2, 1};
+  vector<ll> r = {2, 3, 3};
+  vector<ll> d = {1, 1, 5};
+
+  EXPECT_EQ("No", solve(3, 3, l, r, d));
+}
+
+TEST(abc087d, 3) {
+  vector<ll> l = {2, 2, 3};
+  vector<ll> r = {1, 3, 4};
+  vector<ll> d = {1, 5, 2};
+
+  EXPECT_EQ("Yes", solve(4, 3, l, r, d));
+}
+
+TEST(abc087d, 4) {
+  vector<ll> l = {8, 7, 9};
+  vector<ll> r = {7, 9, 8};
+  vector<ll> d = {100, 100, 100};
+
+  EXPECT_EQ("No", solve(10, 3, l, r, d));
+}
+
+TEST(abc087d, 5) {
+  vector<ll> l = {};
+  vector<ll> r = {};
+  vector<ll> d = {};
+
+  EXPECT_EQ("Yes", solve(100, 0, l, r, d));
+}
