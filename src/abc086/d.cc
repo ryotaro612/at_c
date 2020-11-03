ce7faa3a1ce5052c4656cedccd065808162dbdcb
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2020-11-03 12:06:47 +0900

    086

--- src/abc086/d.cc
+++ src/abc086/d.cc
@@ -2,46 +2,42 @@
 using namespace std;
 typedef long long ll;
 
-ll solve(ll n, ll k, vector<ll> x, vector<ll> y, vector<ll> c) {
-    vector<vector<ll>> a(2 * k, vector<ll>(2 * k, 0));
+ll solve(ll n, ll k, vector<ll> x, vector<ll> y, vector<char> c) {
+    vector<vector<ll>> s(4000, vector<ll>(4000, 0));
     for(ll i = 0; i < n; i++) {
         if(c[i] == 'B') {
-            a[x[i] % (2 * k)][y[i] % (2 * k)] += 1;
+            s[x[i] % (2 * k)][y[i] % (2 * k)] += 1;
         } else {
-            a[(x[i] + k) % (2 * k)][(y[i] + k) % (2 * k)] += 1;
+            s[x[i] % (2 * k)][(y[i] + k) % (2 * k)] += 1;
         }
     }
-    vector<vector<ll>> s(4000, vector<ll>(4000, 0));
-    s[0][0] = a[0][0];
-    for(ll j = 1; j < 2 * k; j++) {
-        s[0][j] = s[0][j - 1] + a[0][j];
-    }
-    for(ll i = 1; i < 2 * k; i++) {
-        s[i][0] = s[i - 1][0] + a[i][0];
-    }
-    for(ll i = 1; i < 2 * k; i++) {
+
+    for(ll i = 0; i < 2 * k; i++) {
         for(ll j = 1; j < 2 * k; j++) {
-            s[i][j] = s[i - 1][j - 1] + (s[i][j - 1] - s[i - 1][j - 1]) +
-                      (s[i - 1][j] - s[i - 1][j - 1]) + a[i][j];
+            s[i][j] += s[i][j - 1];
         }
     }
-    for(ll i = 0; i < 5; i++) {
-        for(ll j = 0; j < 5; j++) {
-            cout << s[i][j] << " ";
+    for(ll i = 1; i < 2 * k; i++) {
+        for(ll j = 0; j < 2 * k; j++) {
+            s[i][j] += s[i - 1][j];
         }
-        cout << endl;
     }
+
     ll res = s[2 * k - 1][k - 1] - s[k - 1][k - 1] + s[k - 1][2 * k - 1] -
              s[k - 1][k - 1];
     for(ll i = 0; i < k; i++) {
         for(ll j = 0; j < k; j++) {
             ll temp = s[i][j];
-            temp += s[i][2 * k - 1] - s[i][j + k];
-            temp += s[i + k][j + k] - s[i + k][j] - s[i][j + k];
-            temp += s[2 * k - 1][j] - s[i + k][j];
-            temp += s[2 * k - 1][2 * k - 1] - s[2 * k - 1][j + k] -
-                    s[i + k][2 * k - 1] + s[i + k][j + k];
-            res = max(temp, res);
+            ll b = s[i][2 * k - 1] - s[i][j + k];
+            temp += b;
+            ll c = s[i + k][j + k] - s[i + k][j] - s[i][j + k] + s[i][j];
+            temp += c;
+            ll d = s[2 * k - 1][j] - s[i + k][j];
+            temp += d;
+            ll e = s[2 * k - 1][2 * k - 1] - s[2 * k - 1][j + k] -
+                   s[i + k][2 * k - 1] + s[i + k][j + k];
+            temp += e;
+            res = max(n - temp, max(temp, res));
         }
     }
     return res;
@@ -50,7 +46,8 @@ ll solve(ll n, ll k, vector<ll> x, vector<ll> y, vector<ll> c) {
 int main() {
     ll n, k;
     cin >> n >> k;
-    vector<ll> x(n), y(n), c(n);
+    vector<ll> x(n), y(n);
+    vector<char> c(n);
 
     for(ll i = 0; i < n; i++) {
         cin >> x[i] >> y[i] >> c[i];
--- tests/abc086/d.cc
+++ tests/abc086/d.cc
@@ -5,7 +5,7 @@ TEST(abc086d, 1) {
     ll n = 4, k = 3;
     vector<ll> x = {0, 1, 5, 5};
     vector<ll> y = {1, 2, 3, 4};
-    vector<ll> c = {'W', 'W', 'B', 'B'};
+    vector<char> c = {'W', 'W', 'B', 'B'};
     EXPECT_EQ(4, solve(n, k, x, y, c));
 }
 
@@ -13,7 +13,7 @@ TEST(abc086d, 2) {
     ll n = 2, k = 1000;
     vector<ll> x = {0, 0};
     vector<ll> y = {0, 1};
-    vector<ll> c = {'B', 'W'};
+    vector<char> c = {'B', 'W'};
     EXPECT_EQ(2, solve(n, k, x, y, c));
 }
 
@@ -21,6 +21,6 @@ TEST(abc086d, 3) {
     ll n = 6, k = 2;
     vector<ll> x = {1, 2, 2, 1, 0, 4};
     vector<ll> y = {2, 1, 2, 0, 6, 5};
-    vector<ll> c = {'B', 'W', 'B', 'B', 'W', 'W'};
+    vector<char> c = {'B', 'W', 'B', 'B', 'W', 'W'};
     EXPECT_EQ(4, solve(n, k, x, y, c));
 }
