accf189043ec1fc0ea225ab76d1aafc55368cf2f
Nakamura, Ryotaro <ryotaro@T490.T490.local>
2020-11-04 12:09:35 +0900

    babc181

---
+++ src/abc181/a.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef unsigned long long ll;
+
+string solve(int n)
+{
+    return n % 2 == 0 ? "White" : "Black";
+}
+/*
+int main()
+{
+    int n;
+    cin >> n;
+    cout << solve(n);
+}
+*/
