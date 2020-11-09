de3470d55c07502ed9bfc0aba6678bc5917c8944
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2020-11-09 11:14:28 +0900

    a

---
+++ src/abc182/a.cc
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int solve(int a, int b) { return (2 * a + 100) - b; }
+/*
+int main() {
+    int a, b;
+    cin >> a >> b;
+    cout << solve(a, b);
+}
+*/
---
+++ src/abc182/b.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int solve(int a, int b) { return (2 * a + 100) - b; }
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for(int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    cout << solve(a, b);
