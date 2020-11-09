cf4c432168e5dba4f4d5db89bfd68d703a6273d9
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2020-11-09 11:28:01 +0900

    b

--- src/abc182/b.cc
+++ src/abc182/b.cc
@@ -2,8 +2,24 @@
 using namespace std;
 typedef long long ll;
 
-int solve(int a, int b) { return (2 * a + 100) - b; }
+int solve(int n, vector<int> a) {
+    int res = 0;
+    int ans;
 
+    for(int i = 2; i <= 1000; i++) {
+        int temp = 0;
+        for(auto aa : a) {
+            if(aa % i == 0)
+                temp++;
+        }
+        if(temp >= res) {
+            ans = i;
+        }
+        res = max(res, temp);
+    }
+    return ans;
+}
+/*
 int main() {
     int n;
     cin >> n;
@@ -11,4 +27,6 @@ int main() {
     for(int i = 0; i < n; i++) {
         cin >> a[i];
     }
-    cout << solve(a, b);
+    cout << solve(n, a);
+}
+*/
