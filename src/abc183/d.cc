5cc8cbad5219ea40dd4d7410c39d660049b51395
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2020-11-17 12:51:24 +0900

    abc183

--- src/abc183/d.cc
+++ src/abc183/d.cc
@@ -3,10 +3,25 @@ using namespace std;
 typedef long long ll;
 
 string solve(ll n, ll w, vector<ll> s, vector<ll> t, vector<ll> p) {
+    vector<ll> a(2 * pow(10, 5) + 1, 0);
 
-    return "No";
+    for(ll i = 0; i < n; i++) {
+        a[s[i]] += p[i];
+        a[t[i]] -= p[i];
+    }
+    if(a[0] > w) {
+        return "No";
+    }
+    for(ll i = 1; i < (ll)a.size(); i++) {
+        a[i] += a[i - 1];
+        if(a[i] > w) {
+            return "No";
+        }
+    }
+
+    return "Yes";
 }
-
+/*
 int main() {
     ll n, w;
     cin >> n >> w;
@@ -16,3 +31,5 @@ int main() {
     }
     cout << solve(n, w, s, t, p);
 }
+
+*/
