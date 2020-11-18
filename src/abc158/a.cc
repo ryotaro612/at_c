955cdca353183881adb618954d5efec3d747e183
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2020-11-18 15:43:55 +0900

    158a

---
+++ src/abc158/a.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+string solve(string s) {
+    if(s == "AAA" || s == "BBB")
+        return "No";
+    return "Yes";
+}
+/*
+int main() {
+    string s;
+    cin >> s;
+    cout << solve(s);
+}
+*/
