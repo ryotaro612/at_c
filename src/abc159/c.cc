13c4d6a1041195b76bbace7840806a0f82ce5f6c
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2020-11-18 12:24:02 +0900

    abc159

---
+++ src/abc159/c.cc
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+double solve(double l) { return pow(l, 3) / 27; }
+/*
+int main() {
+    double l;
+    cin >> l;
+    cout << fixed << setprecision(15) << solve(l);
+}
+*/
