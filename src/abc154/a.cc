d5f71c8826131e43642521e2e5bbc401c4cb5349
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2020-11-22 19:02:16 +0900

    154a

---
+++ src/abc154/a.cc
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+pair<int, int> solve(string s, string t, int a, int b, string u) {
+    if(u == s) {
+        return make_pair(a- 1, b);
+    }
+        return make_pair(a, b-1);
+}
+/*
+int main() {
+    string s, t, u;
+    int a, b;
+    cin >> s >> t >> a >> b >> u;
+    pair<int, int> res = solve(s, t, a, b, u);
+    cout << res.first << " " << res.second;
+}
+*/
