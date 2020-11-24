7acac5d99ae926cae760db7bf1c36c8aa122c429
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2020-11-25 00:33:08 +0900

    abc152

---
+++ src/abc152/a.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+string solve(int n, int m) {
+    return n == m ? "Yes" : "No";
+}
+/*
+int main() {
+    int n, m;
+    cin >> n >> m;
+    cout << solve(n, m);
+}
+*/
