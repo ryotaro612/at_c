c54bba329b04c7030fd4e53fc2dd2b0e37cf0928
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2020-11-25 15:10:26 +0900

    151

---
+++ src/abc151/a.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+char solve(char c) {
+    int cc = c;
+
+    return (char)(cc + 1);
+}
+/*
+int main() {
+    char c;
+    cin >> c;
+    cout << solve(c);
+}
+*/
