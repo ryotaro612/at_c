9048b797f8a8ee7ce29cf410bad820894acc7ef4
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2020-11-29 01:51:49 +0900

    150

---
+++ src/abc150/a.cc
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+string solve(int k, int x) { return (500 * k >= x ? "Yes" : "No"); }
+/*
+int main() {
+    int k, x;
+    cin >> k >> x;
+    cout << solve(k, x);
+}
+*/
