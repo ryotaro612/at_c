ee891553f1ee6c234f7e183fe4a1334dce1c3851
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2020-11-29 12:50:47 +0900

    149

---
+++ src/abc149/a.cc
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+string solve(string s, string t) { return t + s; }
+/*
+int main() {
+    string s, t;
+    cin >> s >> t;
+    cout << solve(s, t);
+}
+*/
