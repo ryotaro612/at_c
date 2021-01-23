fb2a7d9e1eabc8578406166b5b3ea458d0ce3b55
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2021-01-24 02:31:38 +0900

    wip

--- src/abc189/d.cc
+++ src/abc189/d.cc
@@ -2,35 +2,23 @@
 using namespace std;
 typedef long long ll;
 
-ll dfs(bool y, int index, int n, vector<string> &s) {
-    if(index == n) {
-        return y ? 1 : 0;
+ll dfs(ll index, vector<string> &s) {
+    if(index == 0) {
+        return 1;
     }
-    if(s[index] == "AND") {
-        if(y) {
-            return dfs(y, index + 1, n, s) + dfs(false, index + 1, n, s);
-        } else {
-            return 2 * dfs(false, index + 1, n, s);
-        }
-    }
-    // s[index] == or
-    if(y) {
-        return 2 * dfs(true, index + 1, n, s);
-    } else {
-        return dfs(true, index + 1, n, s) + dfs(y, index + 1, n, s);
-    }
+    if(s[index - 1] == "AND")
+        return dfs(index - 1, s);
+    return (ll)pow(2ll, index) + dfs(index - 1, s);
 }
-
-ll solve(int n, vector<string> s) {
-    return dfs(true, 0, n, s) + dfs(false, 0, n, s);
-}
-
+ll solve(ll n, vector<string> s) { return dfs(n, s); }
+/*
 int main() {
-    int n;
+    ll n;
     cin >> n;
     vector<string> s(n);
-    for(int i = 0; i < n; i++) {
+    for(ll i = 0; i < n; i++) {
         cin >> s[i];
     }
     cout << solve(n, s) << endl;
 }
+*/
--- tests/abc189/d.cc
+++ tests/abc189/d.cc
@@ -4,3 +4,9 @@
 TEST(abc189d, 1) { EXPECT_EQ(5, solve(2, {"AND", "OR"})); }
 
 TEST(abc189d, 2) { EXPECT_EQ(63, solve(5, {"OR", "OR", "OR", "OR", "OR"})); }
+
+TEST(abc189d, 3) { EXPECT_EQ(3, solve(1, {"OR"})); }
+
+TEST(abc189d, 4) { EXPECT_EQ(1, solve(1, {"AND"})); }
+
+TEST(abc189d, 5) { EXPECT_EQ(5, solve(2, {"AND", "OR"})); }
