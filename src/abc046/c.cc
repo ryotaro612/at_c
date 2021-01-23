c695a0b2dd2051241525fd0b548d069689855825
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2021-01-23 13:48:23 +0900

    0456

--- src/abc046/c.cc
+++ src/abc046/c.cc
@@ -5,40 +5,16 @@ typedef long long ll;
 ll solve(ll n, vector<ll> t, vector<ll> a) {
     ll tt = 1, aa = 1;
     for(ll i = 0; i < n; i++) {
-        if(t[i] >= tt && a[i] >= aa) {
-            tt = t[i];
-            aa = a[i];
-            continue;
-        }
-        if(a[i] * tt > t[i] * aa) {
-            // ｔで計算する
-            if(tt % t[i] == 0) {
-                ll bai = (tt / t[i]);
-                tt = t[i] * bai;
-                aa = bai * a[i];
-                continue;
-            } else {
-                ll bai = (tt / t[i]) + 1;
-                tt = t[i] * bai;
-                aa = bai * a[i];
-            }
-        } else {
-            if(aa % a[i] == 0) {
-                ll bai = (aa / a[i]);
-                aa = a[i] * bai;
-                tt = bai * t[i];
-                continue;
-            } else {
-                ll bai = (aa / a[i]) + 1;
-                aa = a[i] * bai;
-                tt = bai * t[i];
-            }
-        }
+        ll x = tt / t[i] + (tt % t[i] == 0 ? 0 : 1);
+        ll y = aa / a[i] + (aa % a[i] == 0 ? 0 : 1);
+        ll z = max(x, y);
+        tt = t[i] * z;
+        aa = a[i] * z;
     }
 
     return tt + aa;
 }
-
+/*
 int main() {
     ll n;
     cin >> n;
@@ -48,3 +24,4 @@ int main() {
     }
     cout << solve(n, t, a) << endl;
 }
+*/
