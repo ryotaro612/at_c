965c69a86ffb220608f84f10198b10cf6e984c40
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2021-02-11 18:56:48 +0900

    034

--- src/abc034/d.cc
+++ src/abc034/d.cc
@@ -2,45 +2,30 @@
 using namespace std;
 typedef long long ll;
 
-typedef pair<ll, double> salty_water;
+bool check(int n, int k, vector<ll> w, vector<int> p, double threashold) {
+    vector<double> v(n);
+    for(int i = 0; i < n; i++) {
+        v[i] = (double)w[i] * ((double)p[i] - 100 * threashold);
+    }
+    sort(v.begin(), v.end(), greater<double>());
+    double res = 0;
+    for(int i = 0; i < k; i++) {
+        res += v[i];
+    }
+    return res > 0;
+}
 
 double solve(int n, int k, vector<ll> w, vector<int> p) {
-    vector<vector<salty_water>> dp(n + 1, vector<salty_water>(k + 1));
-    dp[0][0] = salty_water(0, 0);
-    for(int i = 0; i <= k; i++) {
-        dp[0][i] = salty_water(0ll, 0);
-    }
-    for(int i = 1; i <= n; i++) {
-        for(int j = 1; j <= k && j <= i; j++) {
-            cout << "i: " << i << " j: " << j << endl;
-            double current_salt =
-                (((double)w[i - 1] * (double)p[i - 1]) / (double)100);
-            if(j == 1) {
-                if(i == 1 || current_salt / w[i - 1] >
-                                 dp[i - 1][j].second / dp[i - 1][j].first) {
-                    dp[i][j] = salty_water(w[i - 1], current_salt);
-                } else {
-                    dp[i][j] = dp[i - 1][j];
-                }
-                continue;
-            }
-
-            double salt = dp[i - 1][j - 1].second + current_salt;
-            ll salty = dp[i - 1][j - 1].first + w[i - 1];
-            cout << "expr: salty_water: " << salty << " salt: " << salt << endl;
-            if(j == i || salt / (double)salty >=
-               (double)dp[i - 1][j].second / (double)dp[i - 1][j].first) {
-                dp[i][j] = salty_water(salty, salt);
-            } else {
-                dp[i][j] = dp[i - 1][j];
-            }
-            cout << "salty_water: " << dp[i][j].first
-                 << " salt: " << dp[i][j].second << endl;
+    double lb = 0.0, ub = 1.01;
+    for(int i = 0; i < 100; i++) {
+        double mid = (lb + ub) / (double)2;
+        if(check(n, k, w, p, mid)) {
+            lb = mid;
+        } else {
+            ub = mid;
         }
     }
-    cout << "##### " << ((double)dp[n][k].second / (double)dp[n][k].first) * 100
-         << endl;
-    return ((double)dp[n][k].second / (double)dp[n][k].first) * 100;
+    return ub*100;
 }
 /*
 int main() {
