6e51240e344ac363afd891570d8a48387fe67f93
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2021-02-13 18:57:51 +0900

    005

--- src/abc005/d.cc
+++ src/abc005/d.cc
@@ -2,7 +2,7 @@
 using namespace std;
 
 vector<vector<int>> calc_area(int n, vector<vector<int>> d) {
-    vector<vector<int>> area(n, vector<int>(n));
+    vector<vector<int>> area(n, vector<int>(n, 0));
     area[0][0] = d[0][0];
     for(int i = 1; i < n; i++) {
         area[0][i] += area[0][i - 1] + d[0][i];
@@ -28,6 +28,7 @@ vector<pair<int, int>> list_rect_patterns(int area) {
 }
 int query_area(int x, int y, int width, int height,
                vector<vector<int>> &areas) {
+
     int res = areas[y + height - 1][x + width - 1];
     if(y != 0) {
         res -= areas[y - 1][x + width - 1];
@@ -42,46 +43,45 @@ int query_area(int x, int y, int width, int height,
 }
 
 vector<int> solve(int n, vector<vector<int>> d, int q, vector<int> p) {
-    vector<vector<int>> areas = calc_area(n, d);
-    /*
-    for(int i = 0; i < n; i++) {
-        for(int j = 0; j < n; j++) {
-            cout << area[i][j] << " ";
-        }
-        cout << endl;
+    vector<vector<int>> areas(n, vector<int>(n, 0));
+    areas[0][0] = d[0][0];
+    for(int i = 1; i < n; i++) {
+        areas[0][i] += areas[0][i - 1] + d[0][i];
+        areas[i][0] += areas[i - 1][0] + d[i][0];
     }
-    */
-    /*
-     cout << query_area(0, 0, 1, 1, areas) << endl;
-     cout << query_area(0, 0, 3, 3, areas) << endl;
-     cout << query_area(0, 0, 1, 3, areas) << endl;
-     cout << query_area(0, 0, 3, 1, areas) << endl;
-     cout << query_area(0, 0, 2, 2, areas) << endl;
-     cout << query_area(1, 1, 1, 2, areas) << endl;
-     */
-    vector<int> cache(n + 1, 0);
-    for(int use = 1; use <= n*n; use++) {
-        vector<pair<int, int>> rect_patterns = list_rect_patterns(use);
-        /*
-        for(auto x: rect_patterns) {
-            cout << x.first << " " << x.second << endl;
+    for(int i = 1; i < n; i++) {
+        for(int j = 1; j < n; j++) {
+            areas[i][j] = (areas[i - 1][j] + areas[i][j - 1] + d[i][j] -
+                           areas[i - 1][j - 1]);
         }
-        cout << "  === " << endl;
-        */
+    }
+    vector<int> cache(n * n + 1, 0);
+    for(int use = 1; use <= n * n; use++) {
+        vector<pair<int, int>> rect_patterns = list_rect_patterns(use);
         int temp = 0;
         for(auto pattern : rect_patterns) {
             int width = pattern.first, height = pattern.second;
             for(int x = 0; x + width - 1 < n; x++) {
                 for(int y = 0; y + height - 1 < n; y++) {
-                    temp = max(query_area(x, y, width, height, areas), temp);
+
+                    int rect = areas[y + height - 1][x + width - 1];
+                    if(y != 0) {
+                        rect -= areas[y - 1][x + width - 1];
+                    }
+                    if(x != 0) {
+                        rect -= areas[y + height - 1][x - 1];
+                    }
+                    if(y != 0 && x != 0) {
+                        rect += areas[y - 1][x - 1];
+                    }
+                    temp = max(rect, temp);
                 }
             }
         }
-        //cout << " temp: " << temp << endl;
         cache[use] = max(temp, cache[use - 1]);
     }
-    vector<int> res(q);
-    for(int i=0;i<q;i++) {
+    vector<int> res(q, 0);
+    for(int i = 0; i < q; i++) {
         res[i] = cache[p[i]];
     }
     return res;
