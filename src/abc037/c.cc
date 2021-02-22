4b4951b497baf1fcd1a54971784a913c2e3412a7
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2021-02-22 12:55:05 +0900

    037

---
+++ src/abc037/c.cc
@@ -0,0 +1,28 @@
+#define _GLIBCXX_DEBUG
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+ll solve(int n, int k, vector<ll> a) {
+    vector<ll> sum(n + 1, 0ll);
+    for(int i = 1; i < n + 1; i++) {
+        sum[i] = a[i - 1] + sum[i - 1];
+    }
+    ll res = 0ll;
+    for(int i = 0; i < n - k + 1; i++) {
+        res += sum[i + k] - sum[i];
+    }
+    return res;
+}
+
+#ifndef _LOCAL
+int main() {
+    int n, k;
+    cin >> n >> k;
+    vector<ll> a(n);
+    for(int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    cout << solve(n, k, a) << endl;
+}
+#endif
