a025229ed0a1fe022ac517641e87e8762af18a11
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2021-02-28 12:03:06 +0900

    b

---
+++ src/abc193/b.cc
@@ -0,0 +1,30 @@
+#ifdef _LOCAL
+#define _GLIBCXX_DEBUG
+#endif
+#include <bits/stdc++.h>
+typedef long long ll;
+using namespace std;
+
+ll solve(int n, vector<ll> a, vector<ll> p, vector<ll> x) {
+    ll res = -1ll;
+    for(int i = 0; i < n; i++) {
+        if(x[i] - a[i] > 0) {
+            if(res < 0 || p[i] < res)
+                res = p[i];
+        }
+    }
+    return res;
+}
+
+#ifndef _LOCAL
+int main() {
+    int n;
+    cin >> n;
+    vector<ll> a(n), p(n), x(n);
+    for(int i = 0; i < n; i++) {
+        cin >> a[i] >> p[i] >> x[i];
+    }
+    cout << solve(n, a, p, x) << endl;
+    return 0;
+}
+#endif
