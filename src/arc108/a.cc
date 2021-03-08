bbe1d2cc20474ac2ecbd928e61b616a7a0541f26
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2021-03-08 10:47:42 +0900

    arc108

---
+++ src/arc108/a.cc
@@ -0,0 +1,23 @@
+#ifdef _LOCAL
+#define _GLIBCXX_DEBUG
+#endif
+#include <bits/stdc++.h>
+typedef long long ll;
+using namespace std;
+
+string solve(ll s, ll p) {
+    for(ll i = 1; i * i <= p; i++) {
+        if(p % i == 0 && p / i + i == s) {
+            return "Yes";
+        }
+    }
+    return "No";
+}
+
+#ifndef _LOCAL
+int main() {
+    ll s, p;
+    cin >> s >> p;
+    cout << solve(s, p) << endl;
+}
+#endif
