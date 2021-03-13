35cd864289dc67f11eb663ff80487f286ab7a98e
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2021-03-13 18:26:59 +0900

    arc109

--- src/arc109/b.cc
+++ src/arc109/b.cc
@@ -5,17 +5,22 @@
 typedef long long ll;
 using namespace std;
 
+ll find(ll v) {
+    ll lb = 0, ub =1414213562ll;
+    while(ub - lb > 1) {
+        ll mid = (ub + lb) / 2ll;
+        if((mid * (mid + 1ll)) / 2ll > v)
+            ub = mid;
+        else
+            lb = mid;
+    }
+    return lb;
+}
+
 ll solve(ll n) {
     if(n == 1ll)
         return 1ll;
-
-    ll c = 1;
-    while(true) {
-        if(((c + 1) * (c + 2)) / 2 < n + 1)
-            c++;
-        else
-            break;
-    }
+    ll c = find(n + 1ll);
     return n - c + 1;
 }
 
