c6f5d44c5f286365e9e447e34dc5cc5a62da546b
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2021-03-20 12:31:16 +0900

    65

---
+++ src/abc065/b.cc
@@ -0,0 +1,32 @@
+#ifdef _LOCAL
+#define _GLIBCXX_DEBUG
+#endif
+#include <bits/stdc++.h>
+typedef long long ll;
+using namespace std;
+
+int solve(int n, vector<int> a) {
+    vector<bool> used(n, false);
+    int count = 0;
+    int index = 0;
+    while(!used[index]) {
+        used[index] = true;
+        index = a[index] - 1;
+        count++;
+        if(index == 1)
+            return count;
+    }
+
+    return -1;
+}
+
+#ifndef _LOCAL
+int main() {
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for(int i = 0; i < n; i++)
+        cin >> a[i];
+    cout << solve(n, a) << endl;
+}
+#endif
