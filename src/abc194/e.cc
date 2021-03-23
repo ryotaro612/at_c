5d7c9314c8515e5f796ac06b0436533604db79b6
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2021-03-23 13:39:33 +0900

    e

--- src/abc194/e.cc
+++ src/abc194/e.cc
@@ -6,32 +6,25 @@ typedef long long ll;
 using namespace std;
 
 int solve(ll n, ll m, vector<ll> a) {
-    vector<ll> sum(n + 1, 0ll);
-    vector<ll> mex(n - m + 1ll);
-    for(ll i = 0ll; i < m; i++) {
+    vector<ll> sum(n + 1, 0ll), mex(n - m + 1ll);
+    for(ll i = 0ll; i < m; i++)
         sum[a[i]]++;
-    }
     ll mv = 0ll;
-    while(sum[mv] > 0ll) {
+    while(sum[mv] > 0ll)
         mv++;
-    }
     mex[0] = mv;
     for(ll i = m; i < n; i++) {
         sum[a[i]]++;
         sum[a[i - m]]--;
         if(sum[a[i - m]] == 0ll && a[i - m] < mv)
             mv = a[i - m];
-
-        while(sum[mv] > 0ll) {
+        while(sum[mv] > 0ll)
             mv++;
-        }
         mex[i - m + 1] = mv;
     }
-
     ll res = n + 1;
-    for(auto m : mex) {
+    for(auto m : mex)
         res = min(res, m);
-    }
     return res;
 }
 
