4b185f4530a48f832b810d3a9b73a511be83caab
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2021-03-23 11:50:46 +0900

    b

---
+++ src/abc186/b.cc
@@ -0,0 +1,35 @@
+#ifdef _LOCAL
+#define _GLIBCXX_DEBUG
+#endif
+#include <bits/stdc++.h>
+typedef long long ll;
+using namespace std;
+
+int solve(int h, int w, vector<vector<int>> a) {
+    int mini = 101;
+    for(int i = 0; i < h; i++) {
+        for(int j = 0; j < w; j++) {
+            mini = min(mini, a[i][j]);
+        }
+    }
+    int res = 0;
+    for(int i = 0; i < h; i++)
+        for(int j = 0; j < w; j++)
+            res += a[i][j] - mini;
+    return res;
+}
+
+#ifndef _LOCAL
+int main() {
+    int h, w;
+    cin >> h >> w;
+    vector<vector<int>> a(h, vector<int>(w));
+    for(int i = 0; i < h; i++) {
+        for(int j = 0; j < w; j++) {
+            cin >> a[i][j];
+        }
+    }
+    cout << solve(h, w, a) << endl;
+    return 0;
+}
+#endif
