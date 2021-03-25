8c0b9a4d322783c51e1e030c4a41eefcd7b629d4
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2021-03-25 11:51:20 +0900

    golf

--- src/abc185/b.cc
+++ src/abc185/b.cc
@@ -9,15 +9,12 @@ string solve(ll n, ll m, ll t, vector<ll> a, vector<ll> b) {
     ll charge = n;
     for(int i = 0; i < m; i++) {
         charge -= a[i] - (i == 0 ? 0ll : b[i - 1]);
-        // cout << "at: " << a[i] << " charge: " << charge << endl;
         if(charge <= 0)
             return "No";
         charge += b[i] - a[i];
         charge = min(charge, n);
-        // cout << "at: " << b[i] << " charge: " << charge << endl;
     }
     charge -= t - b[m - 1];
-    // cout << "at: " << t << " charge: " << charge << endl;
     if(charge <= 0ll)
         return "No";
     return "Yes";
@@ -28,9 +25,8 @@ int main() {
     ll n, m, t;
     cin >> n >> m >> t;
     vector<ll> a(m), b(m);
-    for(int i = 0; i < m; i++) {
+    for(int i = 0; i < m; i++)
         cin >> a[i] >> b[i];
-    }
     cout << solve(n, m, t, a, b) << endl;
 }
 #endif
