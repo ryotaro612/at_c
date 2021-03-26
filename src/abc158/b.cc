1fd25a16c922b053dbd19f61410242ff43b47576
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2021-03-26 14:18:08 +0900

    158b

---
+++ src/abc158/b.cc
@@ -0,0 +1,25 @@
+#ifdef _LOCAL
+#define _GLIBCXX_DEBUG
+#endif
+#include <bits/stdc++.h>
+typedef long long ll;
+using namespace std;
+
+ll solve(ll n, ll a, ll b) {
+    ll div = n / (a + b);
+    if(n % (a + b) == 0ll)
+        return div * a;
+    ll res = div * a;
+    ll remain = n - div * (a + b);
+    remain = min(a, remain);
+    return res + remain;
+}
+
+#ifndef _LOCAL
+int main() {
+    ll n, a, b;
+    cin >> n >> a >> b;
+    cout << solve(n, a, b) << endl;
+    return 0;
+}
+#endif
