0550388b6da5f16da3edd789cc925c03047ddf6f
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2021-03-26 14:24:25 +0900

    082

---
+++ src/abc082/b.cc
@@ -0,0 +1,21 @@
+#ifdef _LOCAL
+#define _GLIBCXX_DEBUG
+#endif
+#include <bits/stdc++.h>
+typedef long long ll;
+using namespace std;
+
+string solve(string s, string t) {
+    sort(s.begin(), s.end());
+    sort(t.begin(), t.end(), greater<char>());
+    return s < t ? "Yes" : "No";
+}
+
+#ifndef _LOCAL
+int main() {
+    string s, t;
+    cin >> s >> t;
+    cout << solve(s, t) << endl;
+    return 0;
+}
+#endif
