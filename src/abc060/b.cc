95f46807062d15e6e2494b5c9c3399280f8d10cd
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2021-03-27 14:11:24 +0900

    650

---
+++ src/abc060/b.cc
@@ -0,0 +1,22 @@
+#ifdef _LOCAL
+#define _GLIBCXX_DEBUG
+#endif
+#include <bits/stdc++.h>
+typedef long long ll;
+using namespace std;
+
+string solve(int a, int b, int c) {
+    for(int i = 1; i <= 101; i++) {
+        if(a * i % b == c)
+            return "YES";
+    }
+    return "NO";
+}
+
+#ifndef _LOCAL
+int main() {
+    int a, b, c;
+    cin >> a >> b >> c;
+    cout << solve(a, b, c) << endl;
+}
+#endif
