e7fe53b68c3c0e04f877a4c942d703db09c96a0b
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2021-03-29 12:21:49 +0900

    116

--- src/arc116/a.cc
+++ src/arc116/a.cc
@@ -5,48 +5,16 @@
 typedef long long ll;
 using namespace std;
 
-vector<ll> calc_divisors(ll v) {
-    vector<ll> res;
-    for(ll i = 1ll; i * i <= v; i++) {
-        if(v % i == 0ll) {
-            res.push_back(i);
-            if(i * i != v)
-                res.push_back(v / i);
-        }
-    }
-    sort(res.begin(), res.end());
-    return res;
-}
-
-string calc(ll v) {
-    map<ll, ll> primes;
-    for(ll i = 2ll; i * i <= v; i++) {
-        while(v % i == 0) {
-            primes[i]++;
-            v /= i;
-        }
-    }
-    if(v != 1)
-        primes[v] = 1;
-
-    ll odd = 1;
-    for(auto pp : primes) {
-        if(pp.first != 2)
-            odd *= pp.second + 1;
-    }
-    ll even = primes[2] * odd;
-    if(even > odd)
-        return "Even";
-    if(even == odd)
-        return "Same";
-    else
-        return "Odd";
-}
-
 vector<string> solve(int t, vector<ll> cases) {
     vector<string> res(t);
-    for(int i = 0; i < t; i++)
-        res[i] = calc(cases[i]);
+    for(int i = 0; i < t; i++) {
+        if(cases[i] % 4 == 0)
+            res[i] = "Even";
+        else if(cases[i] % 2 == 0)
+            res[i] = "Same";
+        else
+            res[i] = "Odd";
+    }
     return res;
 }
 
