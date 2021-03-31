bdff4332e508cb0a9360eab90d7ef78a27bd27cd
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2021-03-31 11:07:25 +0900

    133

---
+++ src/abc133/c.cc
@@ -0,0 +1,26 @@
+#ifdef _LOCAL
+#define _GLIBCXX_DEBUG
+#endif
+#include <bits/stdc++.h>
+typedef long long ll;
+using namespace std;
+
+ll solve(ll l, ll r) {
+    if(r - l >= 2018ll)
+        return 0ll;
+
+    ll res = 100000ll;
+    for(ll i = l; i < r; i++)
+        for(ll j = i + 1ll; j <= r; j++)
+            res = min((i * j) % 2019ll, res);
+
+    return res;
+}
+
+#ifndef _LOCAL
+int main() {
+    ll l, r;
+    cin >> l >> r;
+    cout << solve(l, r);
+}
+#endif
