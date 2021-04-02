0f249f7a69ff8e7293bae5191447b0288e7a10ee
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2021-04-02 13:35:57 +0900

    155b

---
+++ src/abc155/b.cc
@@ -0,0 +1,26 @@
+#ifdef _LOCAL
+#define _GLIBCXX_DEBUG
+#endif
+#include <bits/stdc++.h>
+typedef long long ll;
+using namespace std;
+
+string solve(int n, vector<int> a) {
+    for(auto aa : a)
+        if(aa % 2 == 0)
+            if(aa % 3 != 0 && aa % 5 != 0)
+                return "DENIED";
+
+    return "APPROVED";
+}
+
+#ifndef _LOCAL
+int main() {
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for(int i = 0; i < n; i++)
+        cin >> a[i];
+    cout << solve(n, a) << endl;
+}
+#endif
