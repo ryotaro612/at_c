64720d4917a754fffc9ff49606719991b1c85678
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2021-04-03 01:56:00 +0900

    045

---
+++ src/abc045/b.cc
@@ -0,0 +1,31 @@
+#ifdef _LOCAL
+#define _GLIBCXX_DEBUG
+#endif
+#include <bits/stdc++.h>
+typedef long long ll;
+using namespace std;
+
+char solve(string sa, string sb, string sc) {
+    map<char, string> mp;
+    mp['a'] = sa;
+    mp['b'] = sb;
+    mp['c'] = sc;
+    char turn = 'a';
+    while(true) {
+        if((int)mp[turn].size() == 0)
+            return toupper(turn);
+
+        char temp = mp[turn][0];
+        mp[turn].erase(mp[turn].begin());
+        turn = temp;
+    }
+    return ' ';
+}
+
+#ifndef _LOCAL
+int main() {
+    string sa, sb, sc;
+    cin >> sa >> sb >> sc;
+    cout << solve(sa, sb, sc) << endl;
+}
+#endif
