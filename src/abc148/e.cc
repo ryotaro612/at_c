814ec16eb1edf8d60ff99aa1d4c9e0e0ad837fb4
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2021-04-03 21:58:24 +0900

    148

---
+++ src/abc148/e.cc
@@ -0,0 +1,26 @@
+#ifdef _LOCAL
+#define _GLIBCXX_DEBUG
+#endif
+#include <bits/stdc++.h>
+typedef long long ll;
+using namespace std;
+
+ll solve(ll n) {
+    if(n % 2ll != 0ll)
+        return 0ll;
+    ll x = 5ll;
+    ll res = 0ll;
+    while(n > x) {
+        res += n / (x * 2ll);
+        x *= 5ll;
+    }
+    return res;
+}
+
+#ifndef _LOCAL
+int main() {
+    ll n;
+    cin >> n;
+    cout << solve(n) << endl;
+}
+#endif
