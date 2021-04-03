a0d4434ed72fbee4f6e1fe216ff8c73b380e84a0
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2021-04-03 19:56:52 +0900

    171

---
+++ src/abc171/e.cc
@@ -0,0 +1,33 @@
+#ifdef _LOCAL
+#define _GLIBCXX_DEBUG
+#endif
+#include <bits/stdc++.h>
+typedef long long ll;
+using namespace std;
+
+vector<ll> solve(int n, vector<ll> a) {
+    ll sum = 0ll;
+    for(auto aa : a)
+        sum ^= aa;
+    vector<ll> res(n);
+    for(int i = 0; i < n; i++)
+        res[i] = a[i] ^ sum;
+    return res;
+}
+
+#ifndef _LOCAL
+int main() {
+    int n;
+    cin >> n;
+    vector<ll> a(n);
+    for(int i = 0; i < n; i++)
+        cin >> a[i];
+    vector<ll> res = solve(n, a);
+    for(int i = 0; i < n; i++) {
+        cout << res[i];
+        if(i != n - 1)
+            cout << " ";
+    }
+    cout << endl;
+}
+#endif
