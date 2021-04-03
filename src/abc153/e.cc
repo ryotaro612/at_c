902d1854a2d4f81523450a5dda50a27948025ec5
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2021-04-03 17:29:48 +0900

    hoge

---
+++ src/abc153/e.cc
@@ -0,0 +1,36 @@
+#ifdef _LOCAL
+#define _GLIBCXX_DEBUG
+#endif
+#include <bits/stdc++.h>
+typedef long long ll;
+using namespace std;
+static const ll INF = 10000000000ll;
+int solve(ll h, ll n, vector<ll> a, vector<ll> b) {
+    vector<ll> dp(10010, INF);
+    dp[0] = 0ll;
+    for(ll i = 0ll; i <= h; i++) {
+        if(dp[i] >= INF)
+            continue;
+        for(ll j = 0ll; j < n; j++) {
+            ll dest = min(i + a[j], h);
+            // cout << "dest: " << dest << " " << dp[i] + b[j] << endl;
+            dp[dest] = min(dp[i] + b[j], dp[dest]);
+        }
+    }
+    /*
+        for(int i = 0; i <= h; i++)
+            cout << i << " -> " << dp[i] << endl;
+            */
+    return dp[h];
+}
+
+#ifndef _LOCAL
+int main() {
+    ll h, n;
+    cin >> h >> n;
+    vector<ll> a(n), b(n);
+    for(int i = 0; i < n; i++)
+        cin >> a[i] >> b[i];
+    cout << solve(h, n, a, b) << endl;
+}
+#endif
