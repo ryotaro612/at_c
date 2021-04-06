db65759c6c7ee85d114208c06dc629f2dba40289
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2021-04-06 11:23:24 +0900

    042

---
+++ src/abc042/b.cc
@@ -0,0 +1,25 @@
+#ifdef _LOCAL
+#define _GLIBCXX_DEBUG
+#endif
+#include <bits/stdc++.h>
+typedef long long ll;
+using namespace std;
+
+string solve(int n, int l, vector<string> s) {
+    sort(s.begin(), s.end());
+    string res;
+    for(auto ss : s)
+        res = res + ss;
+    return res;
+}
+
+#ifndef _LOCAL
+int main() {
+    int n, l;
+    cin >> n >> l;
+    vector<string> s(n);
+    for(int i = 0; i < n; i++)
+        cin >> s[i];
+    cout << solve(n, l, s) << endl;
+}
+#endif
