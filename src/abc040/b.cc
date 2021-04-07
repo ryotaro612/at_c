2aeebdcfa924fd954cfb12c9611a17a3fbae50b3
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2021-04-07 10:45:54 +0900

    b

--- src/abc040/b.cc
+++ src/abc040/b.cc
@@ -5,20 +5,20 @@
 typedef long long ll;
 using namespace std;
 
+ll solve(ll n) {
+    ll res = 100000000000ll;
+    for(ll x = 1ll; x <= n; x++) {
+        ll y = n / x;
+        res = min(res, abs(x - y) + (n - x * y));
+    }
+    return res;
+}
+
 #ifndef _LOCAL
 int main() {
-    ll h, n;
-    cin >> h >> n;
-    for(int i = 0; i < n; i++) {
-        ll temp;
-        cin >> temp;
-        h -= temp;
-    }
-    if(h <= 0)
-        cout << "Yes";
-    else
-        cout << "No";
-
+    ll n;
+    cin >> n;
+    cout << solve(n) << endl;
     return 0;
 }
 #endif
