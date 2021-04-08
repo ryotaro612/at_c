d2e015c164a2bb1e87fe65dfc22e8188547287b0
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2021-04-08 09:38:46 +0900

    032

---
+++ src/abc032/b.cc
@@ -0,0 +1,24 @@
+#ifdef _LOCAL
+#define _GLIBCXX_DEBUG
+#endif
+#include <bits/stdc++.h>
+typedef long long ll;
+using namespace std;
+
+ll solve(string s, int k) {
+    if(k > (int)s.size())
+        return 0;
+    set<string> patterns;
+    for(int i = 0; i + k <= (int)s.size(); i++)
+        patterns.insert(s.substr(i, k));
+    return (ll)patterns.size();
+}
+
+#ifndef _LOCAL
+int main() {
+    string s;
+    int k;
+    cin >> s >> k;
+    cout << solve(s, k) << endl;
+}
+#endif
