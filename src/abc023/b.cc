b1ceab5a10f56c1ff5317175217aa0ca18ad94bc
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2021-04-10 02:03:46 +0900

    023

---
+++ src/abc023/b.cc
@@ -0,0 +1,38 @@
+#ifdef _LOCAL
+#define _GLIBCXX_DEBUG
+#endif
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+#ifndef _LOCAL
+int main() {
+    int n, step = 0;
+    string s, a = "b";
+    cin >> n >> s;
+    while(true) {
+        if((int)a.size() > (int)s.size())
+            break;
+        if(s == a) {
+            cout << step << endl;
+            return 0;
+        }
+        a = "a" + a + "c";
+        step++;
+        if(s == a) {
+            cout << step << endl;
+            return 0;
+        }
+        a = "c" + a + "a";
+        step++;
+        if(s == a) {
+            cout << step << endl;
+            return 0;
+        }
+        a = "b" + a + "b";
+        step++;
+    }
+    cout << "-1" << endl;
+    return 0;
+}
+#endif
