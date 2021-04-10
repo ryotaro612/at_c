ff71e61a2fe6174ea54ed2498113a6a1e124404e
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2021-04-10 15:43:31 +0900

    151

---
+++ src/abc151/b.cc
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifndef _LOCAL
+int main() {
+    int n, k, m;
+    cin >> n >> k >> m;
+    int sum = m * n;
+    vector<int> a(n - 1);
+    for(int i = 0; i < n - 1; i++) {
+        cin >> a[i];
+    }
+    for(auto aa : a)
+        sum -= aa;
+
+    int res;
+    if(sum > k)
+        res = -1;
+    else
+        res = max(sum, 0);
+
+    cout << res << endl;
+    return 0;
+}
+#endif
