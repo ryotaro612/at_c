f9fdc95957c64ad7376fb282a931ab5b5b430d99
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2021-04-10 20:32:29 +0900

    022

---
+++ src/abc022/b.cc
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+#ifndef _LOCAL
+int main() {
+    ll n;
+    cin >> n;
+    vector<ll> a(n);
+    set<ll> count;
+    for(int i = 0; i < n; i++) {
+        cin >> a[i];
+        a[i]--;
+    }
+    ll res = 0ll;
+    for(int i = 0; i < n; i++) {
+        if(count.find(a[i]) != count.end()) {
+            res++;
+        } else {
+            count.insert(a[i]);
+        }
+    }
+
+    cout << res << endl;
+    return 0;
+}
+#endif
