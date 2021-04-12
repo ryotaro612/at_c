6a1db3a2e1dfa2343a87e1386a8ac973d3545950
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2021-04-12 11:30:43 +0900

    150b

--- src/abc150/b.cc
+++ src/abc150/b.cc
@@ -6,5 +6,15 @@ using namespace std;
 typedef long long ll;
 
 #ifndef _LOCAL
-int main() {}
+int main() {
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+    int res = 0;
+    for(int i = 0; i + 2 < n; i++)
+        if(s[i] == 'A' && s[i + 1] == 'B' && s[i + 2] == 'C')
+            res++;
+    cout << res << endl;
+}
 #endif
