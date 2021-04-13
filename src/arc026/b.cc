7dd75677168564a16571ebeb10e57d1a9eb13b1f
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2021-04-13 11:17:28 +0900

    arc026

---
+++ src/arc026/b.cc
@@ -0,0 +1,42 @@
+#ifdef _LOCAL
+#define _GLIBCXX_DEBUG
+#endif
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+vector<ll> calc_divisors(ll n) {
+    vector<ll> res;
+    for(ll i = 1ll; i * i <= n; i++) {
+        if(n % i == 0ll) {
+            res.push_back(i);
+            if(i * i != n)
+                res.push_back(n / i);
+        }
+    }
+    sort(res.begin(), res.end());
+    return res;
+}
+
+string solve(ll n) {
+    vector<ll> divisors = calc_divisors(n);
+
+    ll sum = 0ll;
+    for(int i = 0; i < (int)divisors.size() - 1; i++)
+        sum += divisors[i];
+
+    if(sum > n)
+        return "Abundant";
+    if(sum == n)
+        return "Perfect";
+    return "Deficient";
+}
+
+#ifndef _LOCAL
+int main() {
+    ll n;
+    cin >> n;
+    cout << solve(n) << endl;
+    return 0;
+}
+#endif
