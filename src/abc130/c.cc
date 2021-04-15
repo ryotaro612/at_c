d7ca159b9a190915347106e2d211d5575a7d7f38
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2021-04-15 10:45:36 +0900

    abc130

---
+++ src/abc130/c.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+
+#ifndef _LOCAL
+int main() {
+    ld w, h, x, y;
+    cin >> w >> h >> x >> y;
+    int count = 0;
+    if(x * 2 == w && h == y * 2)
+        count = 1;
+    cout << fixed << setprecision(12) << h * w / (ld)2 << " " << count << endl;
+    return 0;
+}
+#endif
