2d174f45337e8bf5970d1107722211c317688de6
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2021-04-15 09:26:41 +0900

    148c

---
+++ src/abc148/c.cc
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+ll gcd(ll a, ll b) {
+    if(b == 0)
+        return a;
+    return gcd(b, a % b);
+}
+
+ll lcm(ll a, ll b) { return a * b / gcd(a, b); }
+
+#ifndef _LOCAL
+int main() {
+    ll a, b;
+    cin >> a >> b;
+    cout << lcm(max(a, b), min(a, b)) << endl;
+}
+#endif
