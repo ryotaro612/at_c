69471eead6ae4802821bec12cb6be716b195c45b
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2021-04-16 20:47:28 +0900

    146b

---
+++ src/abc146/b.cc
@@ -0,0 +1,26 @@
+#ifdef _LOCAL
+#define _GLIBCXX_DEBUG
+#endif
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+string solve(int n, string s) {
+    string res;
+    for(auto c : s) {
+        char cc = ((c - 'A') + n) % 26 + 'A';
+        res.push_back(cc);
+    }
+    return res;
+}
+
+#ifndef _LOCAL
+int main() {
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+    cout << solve(n, s) << endl;
+    return 0;
+}
+#endif
