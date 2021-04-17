927416b891a3bbd76600ff6b6b953b8906ddf9a4
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2021-04-17 19:40:15 +0900

    d

---
+++ src/jsc2021/d.cc
@@ -0,0 +1,38 @@
+#ifdef _LOCAL
+#define _GLIBCXX_DEBUG
+#endif
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+static const ll MOD = 1000000007ll;
+
+ll pow_kai(ll a, ll n) { // aのn乗を計算します。
+    ll x = 1ll;
+    while(n > 0ll) { //全てのbitが捨てられるまで。
+        if(n & 1) {  // 1番右のbitが1のとき。
+            x = x * a;
+            x %= MOD;
+        }
+        a = a * a;
+        a %= MOD;
+        n >>= 1; // bit全体を右に1つシフトして一番右を捨てる。
+    }
+    return x;
+}
+
+ll solve(ll n, ll p) {
+    if(n == 1ll)
+        return p - 1ll;
+
+    return ((p - 1ll) * pow_kai(p - 2ll, n - 1ll)) % MOD;
+}
+
+#ifndef _LOCAL
+int main() {
+    ll n, p;
+    cin >> n >> p;
+    cout << solve(n, p) << endl;
+    return 0;
+}
+#endif
