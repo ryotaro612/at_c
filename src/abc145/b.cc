f1832025e926ccd80b6e957cfb4aa8bdc99a0e96
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2021-04-17 13:27:38 +0900

    145b

---
+++ src/abc145/b.cc
@@ -0,0 +1,28 @@
+#ifdef _LOCAL
+#define _GLIBCXX_DEBUG
+#endif
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+#ifndef _LOCAL
+int main() {
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+    if(n % 2 != 0) {
+        cout << "No" << endl;
+        return 0;
+    }
+    for(int i = 0; i < n / 2; i++) {
+        if(s[i] != s[n / 2 + i]) {
+            cout << "No" << endl;
+            return 0;
+        }
+    }
+    cout << "Yes" << endl;
+
+    return 0;
+}
+#endif
