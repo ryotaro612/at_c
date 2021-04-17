5142066ec563e8470db22f4b3c6f3eeafc35921b
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2021-04-17 19:40:08 +0900

    c

---
+++ src/jsc2021/c.cc
@@ -0,0 +1,28 @@
+#ifdef _LOCAL
+#define _GLIBCXX_DEBUG
+#endif
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+ll solve(ll a, ll b) {
+    ll res;
+    for(ll i = 1ll; i < b; i++) {
+        ll cand = (a / i);
+        if(a > i * cand)
+            cand++;
+
+        if(i * cand >= a && i * (cand + 1) <= b)
+            res = i;
+    }
+    return res;
+}
+
+#ifndef _LOCAL
+int main() {
+    ll a, b;
+    cin >> a >> b;
+    cout << solve(a, b) << endl;
+    return 0;
+}
+#endif
