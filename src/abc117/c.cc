1e76c966dfd2b4c58aaa1019841e61dd36c2e1f4
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2021-04-19 13:41:23 +0900

    abc117

---
+++ src/abc117/c.cc
@@ -0,0 +1,34 @@
+#ifdef _LOCAL
+#define _GLIBCXX_DEBUG
+#endif
+#include <bits/stdc++.h>
+using namespace std;
+
+int solve(int n, int m, vector<int> &x) {
+    if(n >= m)
+        return 0;
+    vector<int> d(m - 1);
+    sort(x.begin(), x.end());
+    for(int i = 0; i < m - 1; i++) {
+        d[i] = x[i + 1] - x[i];
+    }
+    sort(d.begin(), d.end());
+    int res = 0;
+    for(int i = 0; i < m - n; i++) {
+        res += d[i];
+    }
+    return res;
+}
+#ifndef _LOCAL
+int main() {
+
+    int n, m;
+    cin >> n >> m;
+    vector<int> x(m);
+    for(int i = 0; i < m; i++)
+        cin >> x[i];
+
+    cout << solve(n, m, x);
+    return 0;
+}
+#endif
