1a843f4f344e17733b9662616e4284214bba88ab
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2021-04-20 12:57:41 +0900

    abc116

---
+++ src/abc143/a.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+#ifndef _LOCAL
+int main() {
+    int a, b;
+    cin >> a >> b;
+
+    cout << max(a - 2 * b, 0) << endl;
+    return 0;
+}
+#endif
