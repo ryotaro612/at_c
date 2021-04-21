598cfb4ee55ca5d0d9e24f01709e470adb44421b
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2021-04-21 10:40:05 +0900

    143

---
+++ src/abc143/b.cc
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifndef _LOCAL
+int main() {
+    int n;
+    cin >> n;
+    vector<int> d(n);
+    for(int i = 0; i < n; i++)
+        cin >> d[i];
+    int res = 0;
+    for(int i = 0; i < n - 1; i++) {
+        for(int j = i + 1; j < n; j++)
+            res += d[i] * d[j];
+    }
+    cout << res << endl;
+    return 0;
+}
+#endif
