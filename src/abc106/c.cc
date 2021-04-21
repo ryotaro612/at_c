2eb93480f14967c420eb0c63f03c72b9f6e76c64
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2021-04-21 11:24:51 +0900

    abc106

---
+++ src/abc106/c.cc
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+#ifndef _LOCAL
+int main() {
+    string s;
+    cin >> s;
+    ll k;
+    cin >> k;
+
+    ll p1 = -1ll;
+    for(int i = 0; i < (int)s.size(); i++) {
+        if(s[i] == '1')
+            p1 = i;
+        else
+            break;
+    }
+    if(k <= p1 + 1ll) {
+        cout << 1 << endl;
+        return 0;
+    }
+    cout << s[p1 + 1] << endl;
+    return 0;
+}
+
+#endif
+// 5000000000000000
+//
