2f4bf7568d7806370654c8c24a23c276eac4e2e8
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2021-04-22 09:26:24 +0900

    abc143

---
+++ src/abc143/c.cc
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifndef _LOCAL
+int main() {
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+    string res;
+    for(int i = 0; i < n; i++) {
+        if(i < n - 1) {
+            if(s[i] == s[i + 1])
+                continue;
+            else
+                res.push_back(s[i]);
+        } else {
+            res.push_back(s[i]);
+        }
+    }
+    cout << res.size() << endl;
+    return 0;
+}
+#endif
