416d4866b9be6cac315023d60012fe34cffbe337
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2021-04-23 14:22:23 +0900

    142a

---
+++ src/abc142/a.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long double ld;
+#ifndef _LOCAL
+int main() {
+    int n, count = 0, odd = 0;
+    cin >> n;
+
+    for(int i = n; i >= 1; i--) {
+        count++;
+        if(i % 2 == 1)
+            odd++;
+    }
+    cout << fixed << setprecision(12) << (ld)odd / (ld)count << endl;
+    return 0;
+}
+#endif
