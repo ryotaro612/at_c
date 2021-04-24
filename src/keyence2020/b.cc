4aa22416ff15259003971736ed0f475b27ce8050
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2021-04-24 15:10:37 +0900

    2020b

---
+++ src/keyence2020/b.cc
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int solve(int n, vector<ll> x, vector<ll> l) {
+    vector<pair<ll, ll>> lr(n);
+    for(int i = 0; i < n; i++) {
+        lr[i] = make_pair(l[i] + x[i], x[i] - l[i]);
+    }
+    sort(lr.begin(), lr.end());
+
+    int ans = 1, t = lr[0].first;
+    for(int i = 1; i < n; i++) {
+        if(t <= lr[i].second) {
+            ans++;
+            t = lr[i].first;
+        }
+    }
+    return ans;
+}
+
+#ifndef _LOCAL
+int main() {
+    int n;
+    cin >> n;
+    vector<ll> x(n), l(n);
+    for(int i = 0; i < n; i++)
+        cin >> x[i] >> l[i];
+
+    cout << solve(n, x, l) << endl;
+}
+#endif
