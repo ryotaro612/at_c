90a8bb8fc5c71b70b72c46aaf505fd3278b6f22b
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2021-04-25 15:49:13 +0900

    b

---
+++ src/abc199/b.cc
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int solve(int n, vector<int> a, vector<int> b) {
+    int ans = 0;
+    for(int i = 1; i <= 1000; i++) {
+        bool ok = true;
+        for(int j = 0; j < n; j++) {
+            if(!(a[j] <= i && i <= b[j])) {
+                ok = false;
+                break;
+            }
+        }
+        if(ok)
+            ans++;
+    }
+    return ans;
+}
+
+#ifndef _LOCAL
+int main() {
+    int n;
+    cin >> n;
+    vector<int> a(n), b(n);
+    for(int i = 0; i < n; i++)
+        cin >> a[i];
+    for(int i = 0; i < n; i++)
+        cin >> b[i];
+
+    cout << solve(n, a, b) << endl;
+    return 0;
+}
+#endif
