4535af710b2c8220d51a28bc5606f05181076244
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2021-04-27 08:30:45 +0900

    wip

---
+++ src/jsc2019/b.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+static const ll MOD = 1000000007ll;
+#ifndef _LOCAL
+int main() {
+    ll n, k;
+    cin >> n >> k;
+    vector<ll> a(n);
+    for(int i = 0; i < n; i++)
+        cin >> a[i];
+    
+    
+    return 0;
+}
+#endif
