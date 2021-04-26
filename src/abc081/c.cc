1707abda2c3157d4bc3e26f40eae4c293c4834fd
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2021-04-27 08:10:52 +0900

    81

---
+++ src/abc081/c.cc
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+#ifndef _LOCAL
+int main() {
+    int n, k;
+    cin >> n >> k;
+    vector<int> a(n);
+    for(int i = 0; i < n; i++)
+        cin >> a[i];
+    map<int, int> m;
+    for(int i = 0; i < n; i++)
+        m[a[i]]++;
+
+    vector<pair<int, int>> v;
+    for(auto p : m) {
+        v.push_back(make_pair(p.second, p.first));
+    }
+    int siz = v.size();
+    sort(v.begin(), v.end());
+    ll res = 0ll;
+    for(int i = 0; i < siz - k; i++) {
+        res += v[i].first;
+    }
+    cout << res << endl;
+    return 0;
+}
+#endif
