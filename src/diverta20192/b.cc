e7bc4545e51b10c2146864c33b673d26b6be10b7
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2021-04-28 12:39:17 +0900

    20192

---
+++ src/diverta20192/b.cc
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int solve(int n, vector<ll> x, vector<ll> y) {
+    map<pair<ll, ll>, int> mp;
+    for(int i = 0; i < n - 1; i++) {
+        for(int j = i + 1; j < n; j++) {
+            mp[make_pair(x[j] - x[i], y[j] - y[i])]++;
+            mp[make_pair(x[i] - x[j], y[i] - y[j])]++;
+        }
+    }
+    int ans = 0;
+    for(auto p : mp) {
+        //cout << p.second << endl;
+        ans = max(ans, p.second);
+    }
+    return n - ans;
+}
+
+#ifndef _LOCAL
+int main() {
+    int n;
+    cin >> n;
+    vector<ll> x(n), y(n);
+    for(int i = 0; i < n; i++)
+        cin >> x[i] >> y[i];
+    cout << solve(n, x, y) << endl;
+    return 0;
+}
+#endif
