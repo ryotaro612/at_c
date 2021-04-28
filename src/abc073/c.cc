08914b00bd0b3942426d3dde6b383a9b95798347
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2021-04-28 10:51:39 +0900

    073

---
+++ src/abc073/c.cc
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#ifndef _LOCAL
+int main() {
+    int n;
+    cin >> n;
+    vector<ll> a(n);
+    for(int i = 0; i < n; i++)
+        cin >> a[i];
+
+    map<ll, bool> mp;
+    for(int i = 0; i < n; i++) {
+        mp[a[i]] = !mp[a[i]];
+    }
+    ll ans = 0ll;
+    for(auto p : mp) {
+        //cout << p.first << " " << p.second << endl;
+        if(p.second)
+            ans++;
+    }
+    cout << ans << endl;
+    return 0;
+}
+#endif
