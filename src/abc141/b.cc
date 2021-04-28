b0f068caf991413ca322f72db6f2ef3aa2282cf9
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2021-04-28 09:56:17 +0900

    b

---
+++ src/abc141/b.cc
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+#ifndef _LOCAL
+int main() {
+    string s;
+    cin >> s;
+    string ans = "Yes";
+    for(int i = 0; i < (int)s.size(); i++) {
+        if(i % 2 == 0 && s[i] == 'L')
+            ans = "No";
+        if(i % 2 == 1 && s[i] == 'R')
+            ans = "No";
+    }
+    cout << ans << endl;
+    return 0;
+}
+#endif
