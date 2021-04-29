17c695bb9921bf506b01eb8c14f2c1f02324e2e3
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2021-04-29 10:00:30 +0900

    072

---
+++ src/abc072/c.cc
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+typedef long long ll;
+using namespace std;
+
+#ifndef _LOCAL
+int main() {
+    ll n;
+    cin >> n;
+    map<ll, ll> count;
+    vector<ll> a(n);
+    for(int i = 0; i < n; i++)
+        cin >> a[i];
+    for(auto aa : a) {
+        count[aa]++;
+        count[aa + 1]++;
+        count[aa - 1]++;
+    }
+    ll ans = 0ll;
+    for(auto c : count) {
+        ans = max(ans, c.second);
+    }
+    cout << ans << endl;
+    return 0;
+}
+#endif
