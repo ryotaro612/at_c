3b5ba7581b3b58506c24b8969197d0c69ce0c77e
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2021-05-01 11:09:28 +0900

    68

---
+++ src/abc068/c.cc
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+#ifndef _LOCAL
+int main() {
+
+    int n, m;
+    cin >> n >> m;
+    vector<int> a(m), b(m);
+    for(int i = 0; i < m; i++)
+        cin >> a[i] >> b[i];
+
+    set<int> sa, sb;
+    for(int i = 0; i < m; i++) {
+        if(a[i] == 1)
+            sa.insert(b[i]);
+        else if(b[i] == 1)
+            sa.insert(a[i]);
+        if(a[i] == n)
+            sb.insert(b[i]);
+        else if(b[i] == n)
+            sb.insert(a[i]);
+    }
+    set<int> res;
+
+    set_intersection(sa.begin(), sa.end(), sb.begin(), sb.end(),
+                     inserter(res, res.end()));
+    if(res.size() > 0)
+        cout << "POSSIBLE";
+    else
+        cout << "IMPOSSIBLE";
+    cout << endl;
+
+    return 0;
+}
+#endif
