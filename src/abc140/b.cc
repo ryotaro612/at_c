08de5837659378b497ef15acb5122f16bd33c058
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2021-05-01 10:52:11 +0900

    140b

---
+++ src/abc140/b.cc
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+#ifndef _LOCAL
+int main() {
+    int n;
+    cin >> n;
+    vector<int> a(n), b(n), c(n - 1);
+    for(int i = 0; i < n; i++)
+        cin >> a[i];
+    for(int i = 0; i < n; i++)
+        cin >> b[i];
+    for(int i = 0; i < n - 1; i++)
+        cin >> c[i];
+
+    int ans = 0;
+    for(auto bb : b)
+        ans += bb;
+    // cout << ans << endl;
+    for(int i = 0; i < n - 1; i++) {
+        if(a[i] + 1 == a[i + 1])
+            ans += c[a[i] - 1];
+    }
+    cout << ans << endl;
+    return 0;
+}
+#endif
