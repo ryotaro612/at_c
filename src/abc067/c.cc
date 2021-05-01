5b0795f3100e63573bcfc561ee9505328bea8e67
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2021-05-01 11:15:29 +0900

    67

---
+++ src/abc067/c.cc
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+#ifndef _LOCAL
+int main() {
+    int n;
+    cin >> n;
+    vector<ll> a(n);
+    for(int i = 0; i < n; i++)
+        cin >> a[i];
+    vector<ll> sum(n + 1, 0);
+    for(int i = 0; i < n; i++)
+        sum[i + 1] += a[i] + sum[i];
+
+    ll ans = numeric_limits<ll>::max();
+    for(int i = 1; i < n; i++) {
+        ans = min(ans, abs(sum[i] - (sum[n] - sum[i])));
+    }
+    cout << ans << endl;
+    return 0;
+}
+#endif
