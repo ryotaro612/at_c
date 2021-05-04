63628ece3e13a1a46944b7d51ffa082ce14b0b3a
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2021-05-04 10:41:15 +0900

    60

---
+++ src/abc060/c.cc
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+#ifndef _LOCAL
+int main() {
+    ll n, t;
+    cin >> n >> t;
+    vector<ll> tv(n);
+    for(int i = 0; i < n; i++)
+        cin >> tv[i];
+
+    ll ans = 0ll;
+    for(int i = 0; i < n; i++) {
+        if(i < n - 1) {
+            ans += min(t, tv[i + 1] - tv[i]);
+        } else {
+            ans += t;
+        }
+    }
+    cout << ans << endl;
+    return 0;
+}
+#endif
