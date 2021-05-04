a37f116f2060c73b18a21f0aa9238685d7745ca6
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2021-05-04 10:32:20 +0900

    139

---
+++ src/abc139/b.cc
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+#ifndef _LOCAL
+int main() {
+    int a, b;
+    cin >> a >> b;
+    if(b == 1) {
+        cout << 0 << endl;
+        return 0;
+    }
+    for(int i = 1;; i++) {
+        if(a + (i - 1) * (a - 1) >= b) {
+            cout << i << endl;
+            return 0;
+        }
+    }
+    return 0;
+}
+#endif
