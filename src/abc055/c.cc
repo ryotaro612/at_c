c9cb134ab7a38190559b7f164c82ffafbc0bceb7
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2021-05-05 12:22:51 +0900

    055

---
+++ src/abc055/c.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+#ifndef _LOCAL
+int main() {
+    ll n, m;
+    cin >> n >> m;
+    if(n * 2ll >= m) {
+        cout << m / 2ll << endl;
+        return 0;
+    }
+    cout << (2ll * n + m) / 4ll << endl;
+    return 0;
+}
+#endif
