296fd56c0faaba14c61b1a151b352df458a25707
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2021-05-05 12:46:57 +0900

    agc038

---
+++ src/agc038/a.cc
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+#ifndef _LOCAL
+int main() {
+    int h, w, a, b;
+    cin >> h >> w >> a >> b;
+    vector<vector<int>> ans(h, vector<int>(w, 0));
+    for(int i = 0; i < b; i++)
+        for(int j = 0; j < a; j++)
+            ans[i][j] = 1;
+    for(int i = b; i < h; i++)
+        for(int j = a; j < w; j++)
+            ans[i][j] = 1;
+    for(int i = 0; i < h; i++) {
+        for(int j = 0; j < w; j++) {
+            cout << ans[i][j];
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
+#endif
