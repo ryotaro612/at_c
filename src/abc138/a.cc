b6f147fe4dcbeb8eb95694a3858594d4612bc3a0
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2021-05-06 09:14:10 +0900

    138a

---
+++ src/abc138/a.cc
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+string solve(int a, string s) {
+    if(a >= 3200)
+        return s;
+    return "red";
+}
+
+#ifndef _LOCAL
+int main() {
+    int a;
+    string s;
+    cin >> a >> s;
+    cout << solve(a, s) << endl;
+
+    return 0;
+}
+#endif
