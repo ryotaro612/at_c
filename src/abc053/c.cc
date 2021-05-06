0d0cb43fd271f3caaac891f1dbdb909372387756
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2021-05-06 09:59:01 +0900

    053

---
+++ src/abc053/c.cc
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+ll solve(ll x) {
+    if(x <= 6ll)
+        return 1ll;
+
+    ll a = x / 11ll;
+    if(x - a * 11ll <= 0ll)
+        return a * 2ll;
+    if(x - a * 11ll <= 6ll)
+        return a * 2ll + 1ll;
+
+    return a * 2ll + 2ll;
+}
+
+#ifndef _LOCAL
+int main() {
+    ll x;
+    cin >> x;
+    cout << solve(x) << endl;
+    return 0;
+}
+#endif
