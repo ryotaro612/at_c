c3216f7d9fec00f509a4c89848b957bd2010fc6a
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2021-05-06 10:51:52 +0900

    47

---
+++ src/abc047/c.cc
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+ll solve(string s) {
+
+    string a = string(1, s[0]);
+
+    for(int i = 1; i < s.size(); i++) {
+        if(a[a.size() - 1] != s[i])
+            a.push_back(s[i]);
+    }
+    return (int)a.size() - 1;
+}
+
+#ifndef _LOCAL
+int main() {
+    string s;
+    cin >> s;
+    cout << solve(s) << endl;
+    return 0;
+}
+#endif
