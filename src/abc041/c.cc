d339281100379adae9ab1a79830becd860c2b184
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2021-05-07 15:25:13 +0900

    abc041

---
+++ src/abc041/c.cc
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+#ifndef _LOCAL
+int main() {
+    ll n;
+    cin >> n;
+    vector<pair<ll, int>> a(n);
+    for(int i = 0; i < n; i++) {
+        ll temp;
+        cin >> temp;
+        a[i] = make_pair(temp, i + 1);
+    }
+    sort(a.begin(), a.end(), greater<pair<ll, int>>());
+
+    for(int i = 0; i < n; i++) {
+        cout << a[i].second << endl;
+    }
+
+    return 0;
+}
+#endif
