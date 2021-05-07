39598417a4eeda3fb44c69e1a03cd6727a3eed00
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2021-05-07 15:14:06 +0900

    043

---
+++ src/abc043/c.cc
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+#ifndef _LOCAL
+int main() {
+    int n;
+    cin >> n;
+    vector<ll> a(n);
+    for(int i = 0; i < n; i++)
+        cin >> a[i];
+    ll ans = numeric_limits<ll>::max();
+    for(ll i = -100; i <= 100; i++) {
+        ll temp = 0ll;
+        for(auto aa : a) {
+            temp += (i - aa) * (i - aa);
+        }
+        ans = min(ans, temp);
+    }
+    cout << ans << endl;
+    return 0;
+}
+#endif
