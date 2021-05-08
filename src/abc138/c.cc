c1bb6c311cb0afc69679f189afd64580c6b797fa
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2021-05-08 12:59:30 +0900

    138

---
+++ src/abc138/c.cc
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+typedef long double ld;
+using namespace std;
+
+#ifndef _LOCAL
+int main() {
+    int n;
+    cin >> n;
+    vector<ld> v(n);
+    for(int i = 0; i < n; i++)
+        cin >> v[i];
+
+    priority_queue<ld, vector<ld>, greater<ld>> que;
+    for(int i = 0; i < n; i++) {
+        que.push(v[i]);
+    }
+
+    while(que.size() > 1) {
+        ld temp = que.top();
+        que.pop();
+        temp = (temp + que.top()) / (ld)2;
+        que.pop();
+        que.push(temp);
+    }
+    cout << que.top() << endl;
+    return 0;
+}
+#endif
