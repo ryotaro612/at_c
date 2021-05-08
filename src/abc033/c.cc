e5b94ad498f7ff8df832f93ebf78559ac1f57d5b
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2021-05-08 13:32:57 +0900

    033

---
+++ src/abc033/c.cc
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifndef _LOCAL
+int main() {
+    string s;
+    cin >> s;
+    vector<string> chunk;
+    string temp = "";
+    for(int i = 0; i < (int)s.size(); i++) {
+        if(s[i] == '+') {
+            if(temp != "") {
+                chunk.push_back(temp);
+                temp = "";
+            }
+        } else if(s[i] == '*') {
+            continue;
+        } else {
+            temp.push_back(s[i]);
+        }
+    }
+    if(temp != "")
+        chunk.push_back(temp);
+        /*
+    for(auto c: chunk)
+        cout << c << endl;
+        */
+    int ans = 0;
+    for(auto s : chunk) {
+        bool ok = false;
+        for(auto c : s) {
+            if(c == '0') {
+                ok = true;
+                break;
+            }
+        }
+        if(!ok)
+            ans++;
+    }
+    cout << ans << endl;
+
+    return 0;
+}
+#endif
