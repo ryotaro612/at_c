41e51234fde6b2cc69a4cfb135bbb1d76ded1919
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2021-05-09 10:55:59 +0900

    b

---
+++ src/abc200/b.cc
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+ll solve(ll n, ll k) {
+    ll ans = n;
+    for(int i = 0; i < k; i++) {
+        if(ans % 200ll == 0) {
+            ans /= 200ll;
+            continue;
+        }
+        ans *= 1000;
+        ans += 200;
+    }
+    return ans;
+}
+
+#ifndef _LOCAL
+int main() {
+    ll n, k;
+    cin >> n >> k;
+    cout << solve(n, k) << endl;
+}
+#endif
