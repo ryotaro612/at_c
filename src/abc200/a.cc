9694a7dc551fca8856d11f99cc313e59e4f4a923
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2021-05-09 10:55:32 +0900

    a.cc

---
+++ src/abc200/a.cc
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+#ifndef _LOCAL
+int main() {
+    int n;
+    cin >> n;
+    cout << (n - 1) / 100 + 1 << endl;
+    return 0;
+}
+#endif
