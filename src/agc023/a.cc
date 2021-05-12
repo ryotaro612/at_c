410f7373c9ef7ace38ecd62d9277b4c68379b62f
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2021-05-12 12:49:50 +0900

    agc023

---
+++ src/agc023/a.cc
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+#ifndef _LOCAL
+int main() {
+    ll n;
+    cin >> n;
+    vector<ll> a(n);
+    for(int i = 0; i < n; i++)
+        cin >> a[i];
+
+    map<ll, ll> mp;
+    mp[0ll]++;
+    ll sum = 0ll;
+    for(int i = 0; i < n; i++) {
+        sum += a[i];
+        mp[sum]++;
+    }
+    ll ans = 0ll;
+    for(auto p : mp) {
+        if(p.second >= 2) {
+            ans += (p.second * (p.second - 1) / 2ll);
+        }
+    }
+    cout << ans << endl;
+    return 0;
+}
+#endif
