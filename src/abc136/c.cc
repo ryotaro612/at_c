9e329b8ca4c5245296d6c2c38be20cce4935e5a1
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2021-05-14 12:40:29 +0900

    doge

---
+++ src/abc136/c.cc
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+string solve(ll n, vector<ll> h) {
+
+    for(int i = 0; i < n; i++) {
+        if(i == 0) {
+            h[i]--;
+            continue;
+        }
+
+        if(h[i - 1] < h[i])
+            h[i]--;
+        else if(h[i - 1] > h[i])
+            return "No";
+    }
+    return "Yes";
+}
+
+#ifndef _LOCAL
+int main() {
+    ll n;
+    cin >> n;
+    vector<ll> h(n);
+    for(int i = 0; i < n; i++)
+        cin >> h[i];
+
+    cout << solve(n, h) << endl;
+
+    return 0;
+}
+#endif
