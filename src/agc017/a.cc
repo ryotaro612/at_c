5799c9ab8b26d742ec8c3d114507a79a67ace569
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2021-05-14 16:07:24 +0900

    doge

---
+++ src/agc017/a.cc
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+ll pow2(ll n) {
+    ll ans = 1ll;
+    while(n-- != 0ll) {
+        ans *= 2ll;
+    }
+    return ans;
+}
+
+#ifndef _LOCAL
+int main() {
+    ll n, p;
+    cin >> n >> p;
+    vector<ll> a(n);
+    for(int i = 0; i < n; i++)
+        cin >> a[i];
+
+    ll all = pow2(n);
+    ll even = 0ll, odd = 0ll;
+    for(int i = 0; i < n; i++) {
+        if(a[i] % 2ll == 0ll)
+            even++;
+        else
+            odd++;
+    }
+    if(odd == 0) {
+        if(p == 1) {
+            cout << 0 << endl;
+        } else
+            cout << all << endl;
+        return 0;
+    }
+    ll even_pattern = pow2(even);
+    ll odd_pattern = pow2(odd - 1ll);
+    if(p == 1ll) {
+        cout << even_pattern * odd_pattern << endl;
+    } else
+        cout << all - (even_pattern * odd_pattern) << endl;
+
+    return 0;
+}
+#endif
