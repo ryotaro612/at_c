2e60bbf8b02ff1052ac710f18878820273f12037
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2021-05-15 14:15:30 +0900

    arup

---
+++ src/arc054/a.cc
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long double ld;
+
+#ifndef _LOCAL
+int main() {
+    ld l, x, y, s, d, cw, ccw;
+    cin >> l >> x >> y >> s >> d;
+    if(d == s) {
+        cout << 0 << endl;
+        return 0;
+    }
+
+    if(d >= s) {
+        cw = abs(d - s);
+        ccw = s + (l - d);
+    } else {
+        cw = l - s + d;
+        ccw = s - d;
+    }
+    if(x >= y) {
+        cout << fixed << setprecision(12) << cw / (x + y) << endl;
+        return 0;
+    }
+    ld ans = min(cw / (x + y), ccw / (y - x));
+    cout << fixed << setprecision(12) << ans << endl;
+
+    return 0;
+}
+#endif
