0f3304964997491e1bb432363547341388e5077f
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2021-05-15 13:38:15 +0900

    056

---
+++ src/arc056/a.cc
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+#ifndef _LOCAL
+int main() {
+    ll a, b, k, l;
+    cin >> a >> b >> k >> l;
+
+    if(b >= l * a) {
+        cout << a * k << endl;
+        return 0;
+    }
+    ll n_set_money = k / l * b;
+    cout << "b money " << n_set_money << endl;
+    ll ans = a * (k - (k / l * l)) + n_set_money;
+
+    cout << min(ans, (k / l + 1) * b) << endl;
+    return 0;
+}
+#endif
