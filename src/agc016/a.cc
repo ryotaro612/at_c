d05be183dc4e8671a84d92f2d1f66a301d2af76f
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2021-05-15 14:58:52 +0900

    agc016

---
+++ src/agc016/a.cc
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+typedef long long ll;
+using namespace std;
+
+#ifndef _LOCAL
+int main() {
+    string s;
+    cin >> s;
+    ll ans = numeric_limits<ll>::max();
+    set<char> cs;
+    for(auto c : s)
+        cs.insert(c);
+    for(auto c : cs) {
+        string temp = s;
+        ll temp_ans = 0ll;
+        // cout << c << endl;
+        while(true) {
+            set<char> temp_cs;
+            for(auto a : temp)
+                temp_cs.insert(a);
+            if((int)temp_cs.size() == 1) {
+                ans = min(ans, temp_ans);
+                break;
+            }
+
+            string next = string(temp.size() - 1, ' ');
+            for(int i = 0; i < (int)next.size(); i++) {
+                if(temp[i] == c || temp[i + 1] == c)
+                    next[i] = c;
+                else
+                    next[i] = temp[i];
+            }
+            temp = next;
+            temp_ans++;
+        }
+    }
+    cout << ans << endl;
+    return 0;
+}
+#endif
