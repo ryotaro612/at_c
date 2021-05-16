727ccdcb52476880a6b71612df9815f2fec99b82
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2021-05-16 12:01:24 +0900

    a

---
+++ src/abc201/a.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+#ifndef _LOCAL
+int main() {
+    vector<int> a(3);
+    cin >> a[0] >> a[1] >> a[2];
+    sort(a.begin(), a.end());
+    if(a[2] - a[1] == a[1] - a[0]) {
+        cout << "Yes" << endl;
+    } else
+        cout << "No" << endl;
+
+    return 0;
+}
+#endif
