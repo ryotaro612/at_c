5e4db8e80725a33909f1c2d4a68a2dde35d3b3bb
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2021-05-17 11:13:42 +0900

    doge

---
+++ src/abc135/b.cc
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+#ifndef _LOCAL
+int main() {
+    int n;
+    cin >> n;
+    vector<int> p(n);
+    for(int i = 0; i < n; i++)
+        cin >> p[i];
+    int count = 0;
+    for(int i = 1; i <= n; i++) {
+        if(i != p[i - 1])
+            count++;
+    }
+    if(count <= 2)
+        cout << "YES" << endl;
+    else
+        cout << "NO" << endl;
+
+    return 0;
+}
+#endif
