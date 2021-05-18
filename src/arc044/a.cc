28402c92e860f5a6cb0428c1db6bf15962dbe73a
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2021-05-18 10:49:40 +0900

    044

---
+++ src/arc044/a.cc
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+typedef long long ll;
+using namespace std;
+
+bool is_prime(ll n) {
+    vector<ll> ans;
+    for(ll i = 2ll; i * i <= n; i++) {
+        if(n % i == 0ll)
+            return false;
+    }
+    return true;
+}
+
+#ifndef _LOCAL
+int main() {
+    ll n;
+    cin >> n;
+    if(n == 1ll) {
+        cout << "Not Prime" << endl;
+        return 0;
+    }
+
+    if(is_prime(n)) {
+        cout << "Prime" << endl;
+        return 0;
+    }
+
+    if(n % 2ll != 0ll && n % 5ll != 0ll && n % 3 != 0ll) {
+        cout << "Prime" << endl;
+        return 0;
+    }
+
+    cout << "Not Prime" << endl;
+
+    return 0;
+}
+#endif
