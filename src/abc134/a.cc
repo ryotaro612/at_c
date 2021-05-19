767bb4ce9db1c521a6cadaa3107f034602d3567b
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2021-05-19 09:29:09 +0900

    134

---
+++ src/abc134/a.cc
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+typedef long long ll;
+using namespace std;
+
+#ifndef _LOCAL
+int main() {
+    int r;
+    cin >> r;
+    cout << 3 * r * r << endl;
+    return 0;
+}
+#endif
