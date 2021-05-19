590804c12e972be21cf0056ac2309788b7e8f1ce
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2021-05-19 10:15:21 +0900

    arc030

---
+++ src/arc030/a.cc
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+#ifndef _LOCAL
+int main() {
+    ll n, k;
+    cin >> n >> k;
+
+    ll mx = (n - 3) / 2 + (n - 3) % 2 + 1;
+
+    if(mx >= k) {
+        cout << "YES" << endl;
+    } else {
+        cout << "NO" << endl;
+    }
+}
+#endif
