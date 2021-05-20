a62329d1b6164dbb4d26289dc074aa44f4da2be7
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2021-05-20 09:43:47 +0900

    wip

---
+++ src/abc134/b.cc
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifndef _LOCAL
+int main() {
+    int n, d;
+    cin >> n >> d;
+
+    int a = 2 * d + 1;
+
+    if(n % a == 0)
+        cout << n / a << endl;
+    else
+        cout << n / a + 1 << endl;
+
+    return 0;
+}
+#endif
