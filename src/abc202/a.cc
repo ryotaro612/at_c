b5ddc1c23fe467b4233f39ec7a30f202fec058b8
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2021-05-23 10:30:44 +0900

    a

---
+++ src/abc202/a.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+typedef long long ll;
+using namespace std;
+
+#ifndef _LOCAL
+int main() {
+    int a, b, c;
+    cin >> a >> b >> c;
+
+    cout << (7 - a) + (7 - b) + (7 - c) << endl;
+    return 0;
+}
+#endif
