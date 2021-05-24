b4e1b489b851bba10a6c7db86dc16f3e43555fc0
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2021-05-24 13:19:00 +0900

    134c

---
+++ src/abc134/c.cc
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+#ifndef _LOCAL
+int main() {
+    ll n;
+    cin >> n;
+    vector<ll> a(n);
+    for(ll i = 0; i < n; i++)
+        cin >> a[i];
+
+    vector<ll> b(a.begin(), a.end());
+    sort(b.begin(), b.end(), greater<ll>());
+    vector<ll> ans(n, b[0]);
+    /*
+    for(int i = 0; i < n; i++)
+        cout << a[i] << endl;
+    cout << " ---- " << endl;
+    for(int i = 0; i < n; i++)
+        cout << b[i] << endl;
+    cout << " ---- " << endl;
+    */
+    if(b[0] != b[1]) {
+        for(int i = 0; i < n; i++) {
+            if(a[i] == b[0])
+                ans[i] = b[1];
+        }
+    }
+
+    for(auto e : ans)
+        cout << e << endl;
+    return 0;
+}
+#endif
