6e3a7ce251a40e5fe2e955161b15a91e9bf18286
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2021-05-29 12:01:48 +0900

    cf17quala

---
+++ src/cf17quala/b.cc
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+#ifndef _LOCAL
+int main() {
+    int n, m, k;
+    cin >> n >> m >> k;
+
+    for(int i = 0; i <= n; i++) {
+        for(int j = 0; j <= m; j++) {
+            if((i * (m - j) + j * (n - i)) == k) {
+                cout << "Yes" << endl;
+                return 0;
+            }
+        }
+    }
+    cout << "No" << endl;
+    return 0;
+}
+#endif
