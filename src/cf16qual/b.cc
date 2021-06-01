d0f16bb4f97af4d75377bd831e447edf2936a51f
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2021-06-01 11:43:28 +0900

    b

---
+++ src/cf16qual/b.cc
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+#ifndef _LOCAL
+int main() {
+    int k, t;
+    cin >> k >> t;
+    vector<int> a(t);
+    for(int i = 0; i < t; i++)
+        cin >> a[i];
+    if(t == 1) {
+        cout << a[0] - 1 << endl;
+        return 0;
+    }
+
+    sort(a.begin(), a.end(), greater<int>());
+    if(a[0] <= k - a[0] + 1) {
+        cout << 0 << endl;
+        return 0;
+    } else {
+        cout << a[0] - (k - a[0] + 1) << endl;
+        return 0;
+    }
+
+    return 0;
+}
+#endif
