b988c584a33ea0e1a666ba9d9112e644cdae25dc
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2021-06-02 13:21:46 +0900

    133

---
+++ src/abc133/a.cc
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+typedef long long ll;
+using namespace std;
+
+#ifndef _LOCAL
+int main() {
+    int n, a, b;
+    cin >> n >> a >> b;
+    cout << min(a * n, b) << endl;
+    return 0;
+}
+#endif
