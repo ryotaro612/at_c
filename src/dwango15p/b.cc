43df1e14666b2a18730ebbad3839ca2169a68234
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2021-06-06 14:32:28 +0900

    dwango

---
+++ src/dwango15p/b.cc
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+typedef long long ll;
+using namespace std;
+
+ll solve(string s) {
+    int n = (int)s.size();
+    ll ans = 0ll, c = 0ll;
+    for(int i = 0; i < n;) {
+        // cout << c << endl;
+        if(i < n - 1 && s[i] == '2' && s[i + 1] == '5') {
+            c++;
+            i += 2;
+        } else {
+            i++;
+            ans += (c * (c + 1ll)) / 2ll;
+            c = 0;
+        }
+    }
+    ans += (c * (c + 1ll)) / 2ll;
+    return ans;
+}
+
+#ifndef _LOCAL
+int main() {
+    string s;
+    cin >> s;
+    cout << solve(s) << endl;
+
+    return 0;
+}
+#endif
