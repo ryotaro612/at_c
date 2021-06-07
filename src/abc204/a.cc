e234da4119cccba5bb463594766ea5a8b77c93c8
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2021-06-07 09:56:15 +0900

    a

---
+++ src/abc204/a.cc
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+#ifndef _LOCAL
+int main() {
+    int x, y;
+    cin >> x >> y;
+    vector<bool> a(3, false);
+    a[x] = true;
+    a[y] = true;
+    if(x == y) {
+        cout << y << endl;
+        return 0;
+    }
+    for(int i = 0; i < 3; i++) {
+        if(a[i] == false) {
+            cout << i << endl;
+            return 0;
+        }
+    }
+
+    return 0;
+}
+#endif
