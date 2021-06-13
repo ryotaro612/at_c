65ac87ad78744e452ea9a0bb5d1c821fc7e7f804
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2021-06-13 23:36:15 +0900

    b

---
+++ src/abc205/b.cc
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+#ifndef _LOCAL
+int main() {
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for(int i = 0; i < n; i++)
+        cin >> a[i];
+    sort(a.begin(), a.end());
+    string ans = "Yes";
+
+    for(int i = 1; i <= n; i++) {
+        if(i != a[i - 1])
+            ans = "No";
+    }
+    cout << ans << endl;
+    return 0;
+}
+#endif
