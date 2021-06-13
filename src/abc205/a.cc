6048a9df8225b8be49b8dac72331cf76d9f7504a
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2021-06-13 23:36:02 +0900

    a

---
+++ src/abc205/a.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long double ld;
+
+#ifndef _LOCAL
+int main() {
+    ld a, b;
+    cin >> a >> b;
+
+    cout << fixed << setprecision(12) << a * b / (ld)100 << endl;
+    return 0;
+}
+#endif
