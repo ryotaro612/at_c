3cb65033ac67b86be692b865f2b88e657daf212b
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2021-06-13 23:36:22 +0900

    c

---
+++ src/abc205/c.cc
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+#ifndef _LOCAL
+int main() {
+    ll a, b, c;
+    cin >> a >> b >> c;
+
+    if(a == b) {
+        cout << "=" << endl;
+        return 0;
+    }
+    if(c % 2ll == 0ll) {
+        if(abs(a) == abs(b)) {
+            cout << "=" << endl;
+        } else if(abs(a) < abs(b)) {
+            cout << "<" << endl;
+        } else {
+            cout << ">" << endl;
+        }
+        return 0;
+    }
+    if(0 <= a && 0 <= b) {
+        if(a < b) {
+            cout << "<" << endl;
+        } else {
+            cout << ">" << endl;
+        }
+        return 0;
+    }
+    if(a < 0 && b < 0) {
+        if(a < b) {
+            cout << "<" << endl;
+        } else { // a > b
+            cout << ">" << endl;
+        }
+    }
+    if(a >= 0 && b < 0) {
+        cout << ">" << endl;
+        return 0;
+    } else {
+        cout << "<" << endl;
+        return 0;
+    }
+
+    return 0;
+}
+#endif
---
+++ src/abc205/d.cc
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+#ifndef _LOCAL
+int main() {
+    int n, q;
+    cin >> n >> q;
+    vector<ll> a(n), k(q);
+    for(int i = 0; i < n; i++)
+        cin >> a[i];
+    for(int i = 0; i < q; i++)
+        cin >> k[i];
+
+    for(auto kk : k) {
+
+        ll lb = 0ll, ub = numeric_limits<ll>::max();
+        while(ub - lb > 1) {
+            ll mid = (ub + lb) / 2ll;
+            auto it = lower_bound(a.begin(), a.end(), mid);
+            ll index = it - a.begin();
+            if(kk < mid - index) {
+                ub = mid;
+            } else {
+                lb = mid;
+            }
+        }
+        cout << lb << endl;
+    }
+    return 0;
+}
+#endif
