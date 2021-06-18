f9723774baab7fe63dda57d4ccfd21cfa2546f32
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2021-06-18 13:59:36 +0900

    fix

---
+++ src/abc043/d.cc
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+string solve(string s) {
+    for(int i = 0; i < (int)s.size() - 1; i++) {
+        if(s[i] == s[i + 1])
+            return to_string(i + 1) + " " + to_string(i + 2);
+    }
+    for(int i = 0; i < (int)s.size() - 2; i++) {
+        if(s[i] == s[i + 2])
+            return to_string(i + 1) + " " + to_string(i + 3);
+    }
+
+    return "-1 -1";
+}
+
+#ifndef _LOCAL
+int main() {
+    string s;
+    cin >> s;
+    cout << solve(s);
+    return 0;
+}
+#endif
