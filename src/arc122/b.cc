17239d533fbaf9af732148a4c72130fd39f4468b
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2021-06-22 09:16:56 +0900

    develop

---
+++ src/arc122/b.cc
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long double ld;
+
+#ifndef _LOCAL
+int main() {
+    int n;
+    cin >> n;
+    vector<ld> a(n);
+    for(int i = 0; i < n; i++)
+        cin >> a[i];
+    sort(a.begin(), a.end());
+
+    ld avg = 0;
+    for(auto aa : a)
+        avg += aa;
+    avg /= (ld)n;
+
+    vector<ld> sum(n + 1, 0);
+    for(int i = 1; i <= n; i++)
+        sum[i] = sum[i - 1] + a[i - 1];
+
+    ld ans = 10000000000;
+    for(int i = 0; i < n; i++) {
+        ld x = a[i] / (ld)2;
+
+        ld expected = x + avg - (a[i] * (n - i) + sum[i]) / (ld)n;
+
+        // cout << i << " -> " << x << " expected " << expected << endl;
+        ans = min(expected, ans);
+    }
+    cout << fixed << setprecision(12) << ans << endl;
+
+    return 0;
+}
+#endif
