ea00ce47969662d72a3a3b688f0a57d4e74d6236
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2021-06-23 10:37:37 +0900

    b

---
+++ src/diverta2019/b.cc
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+#ifndef _LOCAL
+int main() {
+    ll r, g, b, n, ans = 0ll;
+    cin >> r >> g >> b >> n;
+
+    for(ll x = 0ll; x <= 3000ll; x++) {
+        for(ll y = 0ll; y <= 3000ll; y++) {
+            ll z_cand = n - x * r - g * y;
+            if(z_cand < 0ll || z_cand % b != 0ll)
+                continue;
+            ans++;
+        }
+    }
+
+    cout << ans << endl;
+    return 0;
+}
+#endif
