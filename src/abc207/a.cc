de2bc4c239d172045ecba1b9ac35fb35ce5edf51
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2021-06-27 09:33:04 +0900

    a

---
+++ src/abc207/a.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+#ifndef _LOCAL
+int main() {
+    vector<int> a(3);
+    cin >> a[0] >> a[1] >> a[2];
+    sort(a.begin(), a.end());
+
+    cout << a[1] + a[2] << endl;
+
+    return 0;
+}
+#endif
