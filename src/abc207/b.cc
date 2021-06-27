c4094b58b74ab5a15c6bd71fcfa601aed8e1f797
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2021-06-27 09:33:09 +0900

    b

---
+++ src/abc207/b.cc
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+#ifndef _LOCAL
+int main() {
+    ll a, b, c, d;
+    cin >> a >> b >> c >> d;
+
+    ll cond = c * d - b;
+    if(cond <= 0) {
+        cout << "-1" << endl;
+        return 0;
+    }
+
+    for(int i = 0;; i++) {
+        if(a <= i * cond) {
+            cout << i << endl;
+            return 0;
+        }
+    }
+
+    return 0;
+}
+#endif
