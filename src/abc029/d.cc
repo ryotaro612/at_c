92c51f5ad3e6e70a97e2d910ec3d88f6ec3159fe
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2021-06-29 12:51:50 +0900

    d

---
+++ src/abc029/d.cc
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+ll ten(int i) {
+    ll ans = 1ll;
+    for(int j = 0; j < i - 1; j++)
+        ans *= 10ll;
+    return ans;
+}
+
+ll count(string s, int offset) {
+    ll a = 1ll, ans = 0ll;
+    for(int i = 0; i < offset; i++) {
+        ll digit = s[i] - '0';
+        ans += digit * a;
+        a *= 10ll;
+    }
+    return ans + 1ll;
+}
+
+#ifndef _LOCAL
+int main() {
+    string s;
+    cin >> s;
+    reverse(s.begin(), s.end());
+    ll ans = 0ll;
+    for(int i = 0; i < (int)s.size(); i++) {
+        for(int j = i + 1; j < (int)s.size(); j++) {
+            ll d = s[j] - '0';
+            ans += ten(j) * d;
+        }
+        ll digit = s[i] - '0';
+        if(digit > 1ll)
+            ans += ten(i + 1);
+        else if(digit == 1ll)
+            ans += count(s, i);
+    }
+    cout << ans << endl;
+    return 0;
+}
+#endif
