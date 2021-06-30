66187e86d75232e36b31903e55bce4cfcccb3fed
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2021-06-30 17:54:58 +0900

    132

---
+++ src/abc132/b.cc
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+#ifndef _LOCAL
+int main() {
+    int n;
+    cin >> n;
+    vector<int> p(n);
+    for(int i = 0; i < n; i++)
+        cin >> p[i];
+
+    int ans = 0;
+    for(int i = 1; i < n - 1; i++) {
+        vector<int> a(3);
+        a[0] = p[i - 1];
+        a[1] = p[i];
+        a[2] = p[i + 1];
+        sort(a.begin(), a.end());
+        if(p[i] == a[1])
+            ans++;
+    }
+    cout << ans << endl;
+    return 0;
+}
+#endif
