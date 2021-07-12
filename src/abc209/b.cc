274878cb325c3cba4da75f6e9b75e6292b4e87a7
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2021-07-12 11:42:36 +0900

    b

---
+++ src/abc209/b.cc
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+typedef long long ll;
+using namespace std;
+
+#ifndef _LOCAL
+int main() {
+    int n;
+    ll x;
+    cin >> n >> x;
+    vector<ll> a(n);
+    for(int i = 0; i < n; i++)
+        cin >> a[i];
+    ll acc = 0ll;
+    for(int i = 0; i < n; i++) {
+        if(i % 2 == 1)
+            acc += a[i] - 1;
+        else
+            acc += a[i];
+    }
+    if(acc <= x)
+        cout << "Yes";
+    else
+        cout << "No";
+    cout << endl;
+
+    return 0;
+}
+#endif
