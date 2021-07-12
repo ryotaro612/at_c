633c2b98b973098e0d4aea0ba29acb690f7057a1
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2021-07-12 13:22:19 +0900

    131

---
+++ src/abc131/b.cc
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+typedef long long ll;
+using namespace std;
+
+#ifndef _LOCAL
+int main() {
+
+    int n, l;
+    cin >> n >> l;
+
+    ll all = 0ll;
+    for(int i = 1; i <= n; i++) {
+        all += l + i - 1;
+    }
+    int ans;
+    for(int i = 1; i <= n; i++) {
+        ll temp = 0ll;
+        for(int j = 1; j <= n; j++) {
+            if(i == j)
+                continue;
+            temp += l + j - 1;
+        }
+        if(i == 1) {
+            ans = temp;
+        } else {
+            if(abs(all - temp) < abs(all - ans))
+                ans = temp;
+        }
+    }
+    cout << ans << endl;
+    return 0;
+}
+#endif
