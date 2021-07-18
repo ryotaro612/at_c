c5867502dd4a8e4b61aa22cb3db428ee8fca16e8
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2021-07-18 15:39:52 +0900

    210

---
+++ src/abc210/a.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+typedef long long ll;
+using namespace std;
+
+#ifndef _LOCAL
+int main() {
+    ll n, a, x, y;
+    cin >> n >> a >> x >> y;
+
+    if(n < a) {
+        cout << x * n << endl;
+        return 0;
+    }
+    cout << x * a + y * (n - a) << endl;
+    return 0;
+}
+#endif
