e581379c8d4d0bc5b7618405b839c7e716129cdd
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2021-07-19 12:50:09 +0900

    abc210b

---
+++ src/abc210/b.cc
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+typedef long long ll;
+using namespace std;
+
+#ifndef _LOCAL
+int main() {
+    int n;
+    string s;
+    cin >> n >> s;
+
+    bool t = true;
+    for(int i = 0; i < n; i++) {
+        if(s[i] == '1') {
+            if(t) {
+                cout << "Takahashi" << endl;
+                return 0;
+            } else {
+                cout << "Aoki" << endl;
+                return 0;
+            }
+        }
+        t = !t;
+    }
+    return 0;
+}
+#endif
