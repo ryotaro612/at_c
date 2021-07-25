82ef28fbe64030ba7fac3fe5d45b70467c075427
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2021-07-25 10:26:14 +0900

    b

---
+++ src/abc211/b.cc
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+typedef long long ll;
+using namespace std;
+
+#ifndef _LOCAL
+int main() {
+    vector<string> s(4);
+    for(int i = 0; i < 4; i++)
+        cin >> s[i];
+
+    set<string> ss;
+    for(int i = 0; i < 4; i++)
+        ss.insert(s[i]);
+    if((int)ss.size() == 4) {
+        cout << "Yes";
+    } else {
+        cout << "No";
+    }
+    cout << endl;
+    return 0;
+}
+#endif
