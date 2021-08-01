4bfe8d8ac88dce11285cfae226b96bb94304d9b7
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2021-08-01 09:30:49 +0900

    c

---
+++ src/abc212/d.cc
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+ll const INF = 10000000000000ll;
+
+#ifndef _LOCAL
+int main() {
+    int q;
+    cin >> q;
+
+    priority_queue<ll, vector<ll>, greater<ll>> que;
+    ll acc = 0ll;
+    for(int i = 0; i < q; i++) {
+        int o;
+        cin >> o;
+        if(o == 1) {
+            ll x;
+            cin >> x;
+            x -= acc;
+            que.push(x);
+        } else if(o == 2) {
+            ll x;
+            cin >> x;
+            acc += x;
+        } else {
+            ll item = que.top();
+            cout << item + acc << endl;
+            que.pop();
+        }
+    }
+    return 0;
+}
+#endif
