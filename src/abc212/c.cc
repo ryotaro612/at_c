55f264c8e0364a8600f4db59e5484db2bcccf023
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2021-08-01 09:30:45 +0900

    c

---
+++ src/abc212/c.cc
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+#ifndef _LOCAL
+int main() {
+    int n, m;
+    cin >> n >> m;
+    vector<ll> a(n), b(m);
+    for(int i = 0; i < n; i++)
+        cin >> a[i];
+    for(int i = 0; i < m; i++)
+        cin >> b[i];
+    sort(a.begin(), a.end());
+    sort(b.begin(), b.end());
+    ll ans = 1000000000000000ll;
+    for(int i = 0; i < n; i++) {
+        auto c = lower_bound(b.begin(), b.end(), a[i]);
+        if(c == b.end()) {
+            ans = min(abs(b[m - 1] - a[i]), ans);
+        } else {
+            ans = min(abs((*c) - a[i]), ans);
+            if(c != b.begin()) {
+                // auto f = *(c - 1);
+                c--;
+                ans = min(abs((*c) - a[i]), ans);
+            }
+        }
+    }
+    cout << ans << endl;
+    return 0;
+}
+#endif
