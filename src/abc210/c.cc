fd3c96b369bcb1b20f5bfc597bd0371322ae0202
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2021-08-02 15:06:22 +0900

    210

---
+++ src/abc210/c.cc
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+typedef long long ll;
+using namespace std;
+
+#ifndef _LOCAL
+int main() {
+    int n, k;
+    cin >> n >> k;
+    vector<ll> c(n);
+    for(int i = 0; i < n; i++)
+        cin >> c[i];
+
+    map<ll, int> mp;
+    set<ll> a;
+    for(int i = 0; i < k; i++) {
+        mp[c[i]]++;
+        a.insert(c[i]);
+    }
+    int ans = (int)a.size();
+    for(int i = k; i < n; i++) {
+        mp[c[i - k]]--;
+        if(mp[c[i - k]] == 0ll) {
+            a.erase(c[i - k]);
+        }
+        mp[c[i]]++;
+        a.insert(c[i]);
+        ans = max(ans, (int)a.size());
+    }
+    cout << ans << endl;
+    return 0;
+}
+#endif
