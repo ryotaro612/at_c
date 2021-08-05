f3ea01eb6039ac16998d74a872088a60c826d386
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2021-08-05 13:41:24 +0900

    cmake

---
+++ src/abc129/a.cc
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+typedef long long ll;
+using namespace std;
+
+#ifndef _LOCAL
+int main() {
+    int p, q, r;
+    cin >> p >> q >> r;
+    cout << min(p + q, min(q + r, p + r)) << endl;
+    return 0;
+}
+#endif
