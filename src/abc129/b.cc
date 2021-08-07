206943a2b16e0614674115496b5b4a8f5ab357d9
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2021-08-07 12:57:51 +0900

    129

---
+++ src/abc129/b.cc
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+typedef long long ll;
+using namespace std;
+
+#ifndef _LOCAL
+int main() {
+    int n;
+    cin >> n;
+    vector<int> w(n);
+    for(int i = 0; i < n; i++)
+        cin >> w[i];
+    int ans = 100000000;
+    for(int i = 0; i < n - 1; i++) {
+        int s1 = 0;
+        for(int j = 0; j <= i; j++) {
+            s1 += w[j];
+        }
+        int s2 = 0;
+        for(int j = i + 1; j < n; j++) {
+            s2 += w[j];
+        }
+        ans = min(ans, abs(s1 - s2));
+    }
+    cout << ans << endl;
+
+    return 0;
+}
+#endif
