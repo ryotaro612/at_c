f0d1d7bc0cddef859e6af6d32bd59ce27c9a24b5
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2021-08-09 09:11:17 +0900

    c

---
+++ src/abc213/c.cc
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+typedef long long ll;
+using namespace std;
+
+#ifndef _LOCAL
+int main() {
+    ll h, w, n;
+    cin >> h >> w >> n;
+    vector<ll> a(n), b(n);
+    for(int i = 0; i < n; i++) {
+        cin >> a[i] >> b[i];
+    }
+    set<ll> r, c;
+    for(int i = 0; i < n; i++) {
+        r.insert(a[i]);
+        c.insert(b[i]);
+    }
+    map<ll, ll> mp_a, mp_b;
+    int i = 1;
+    for(auto aa : r) {
+        mp_a[aa] = i++;
+    }
+    i = 1;
+    for(auto aa : c) {
+        mp_b[aa] = i++;
+    }
+    for(int i = 0; i < n; i++) {
+        cout << mp_a[a[i]] << " " << mp_b[b[i]] << endl;
+    }
+
+    return 0;
+}
+#endif
