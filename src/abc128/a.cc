2e2b31d4533bff284597a9c3c6bcb989b759faf8
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2021-08-16 13:09:26 +0900

    abc128

---
+++ src/abc128/a.cc
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+typedef long long ll;
+using namespace std;
+
+#ifndef _LOCAL
+int main() {
+    int a, p;
+    cin >> a >> p;
+    cout << (a * 3 + p) / 2 << endl;
+    return 0;
+}
+#endif
