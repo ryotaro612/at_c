570bb5a79e4b04a0bb87f510804cf8f2494c0e77
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2021-08-19 09:17:47 +0900

    127c

---
+++ src/abc127/a.cc
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+#ifndef _LOCAL
+int main() {
+    int a, b;
+    cin >> a >> b;
+
+    if(a >= 13)
+        cout << b << endl;
+    else if(a >= 6)
+        cout << b / 2 << endl;
+    else
+        cout << 0 << endl;
+    return 0;
+}
+#endif
