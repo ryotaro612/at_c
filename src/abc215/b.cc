a7f6b0f2183a67cd7f9755fe8defd6b6cfe5438b
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2021-08-22 10:34:49 +0900

    b

---
+++ src/abc215/b.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+#ifndef _LOCAL
+int main() {
+    ll n;
+    cin >> n;
+    for(ll ans = 0ll;; ans++) {
+        if((((ll)1ll << (ans + 1ll))) > n) {
+            cout << ans << endl;
+            return 0;
+        }
+    }
+    return 0;
+}
+#endif
