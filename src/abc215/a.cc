f366e849297a548e00ba32336a432e7bbda19f2b
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2021-08-22 10:34:39 +0900

    a

---
+++ src/abc215/a.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+typedef long long ll;
+using namespace std;
+
+#ifndef _LOCAL
+int main() {
+    string s;
+    cin >> s;
+    if(s == "Hello,World!") {
+        cout << "AC" << endl;
+    } else {
+        cout << "WA" << endl;
+    }
+    return 0;
+}
+#endif
