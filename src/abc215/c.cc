41df6005dfdf63df8ec1d505c73798c8dedcfa48
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2021-08-22 10:34:54 +0900

    c

---
+++ src/abc215/c.cc
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+typedef long long ll;
+using namespace std;
+
+#ifndef _LOCAL
+int main() {
+    string s;
+    cin >> s;
+    int k;
+    cin >> k;
+	sort(s.begin(), s.end());
+    do {
+		//cout << s << endl;
+        k--;
+        if(k == 0) {
+            cout << s << endl;
+        }
+
+    } while(next_permutation(s.begin(), s.end()));
+    return 0;
+}
+#endif
