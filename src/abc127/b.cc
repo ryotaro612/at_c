f74853a1b23c216111acf7004b0a08c0eb8ba07f
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2021-08-24 12:08:01 +0900

    127

---
+++ src/abc127/b.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+#ifndef _LOCAL
+int main() {
+    int r, d, x;
+    cin >> r >> d >> x;
+    for(int i = 0; i < 10; i++) {
+        x = r * x - d;
+        cout << x << endl;
+    }
+    return 0;
+}
+#endif
