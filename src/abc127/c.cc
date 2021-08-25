c7edd46ac01f0bd39510e48d7e45e3dfd39c5082
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2021-08-25 15:49:07 +0900

    leetcode

---
+++ src/abc127/c.cc
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+typedef long long ll;
+using namespace std;
+
+#ifndef _LOCAL
+int main() {
+    int n, m;
+    cin >> n >> m;
+    vector<int> a(n + 1, 0);
+    for(int i = 0; i < m; i++) {
+        int l, r;
+        cin >> l >> r;
+        l--;
+        r--;
+        a[l] += 1;
+        a[r + 1] -= 1;
+    }
+    for(int i = 1; i < n + 1; i++) {
+        a[i] += a[i - 1];
+    }
+    int ans = 0;
+    for(auto aa : a)
+        if(aa == m)
+            ans++;
+    cout << ans << endl;
+
+    return 0;
+}
+#endif
