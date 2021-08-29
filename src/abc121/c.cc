6317b177e967704dd47f67d7b92f6fd8eba5807a
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2021-08-29 12:35:58 +0900

    121c

---
+++ src/abc121/c.cc
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+#ifndef _LOCAL
+int main() {
+    int n, m;
+    cin >> n >> m;
+    vector<pair<ll, int>> ab(n);
+
+    for(int i = 0; i < n; i++) {
+        ll a;
+        int b;
+        cin >> a >> b;
+        ab[i] = {a, b};
+    }
+    sort(ab.begin(), ab.end());
+    ll ans = 0;
+    int c = 0;
+    for(int i = 0; i < (int)ab.size(); i++) {
+        int d = min(m - c, ab[i].second);
+        ans += ab[i].first * d;
+        c += d;
+    }
+    cout << ans << endl;
+    return 0;
+}
+#endif
