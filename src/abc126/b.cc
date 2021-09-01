46ca00968fb823930154829d5781f4e6c0c33194
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2021-09-01 10:10:34 +0900

    126

---
+++ src/abc126/b.cc
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+bool is_mm(string s) {
+    if(s[0] == '0') {
+        if(s[1] != '0')
+            return true;
+        else
+            return false;
+    } else if(s[0] == '1') {
+        if(s[1] - '0' <= 2)
+            return true;
+        else
+            return false;
+    }
+    return false;
+}
+
+#ifndef _LOCAL
+int main() {
+    string s;
+    cin >> s;
+
+    string a = s.substr(0, 2), b = s.substr(2, 4);
+
+    if(is_mm(a)) {
+        if(is_mm(b)) {
+            cout << "AMBIGUOUS" << endl;
+        } else {
+            cout << "MMYY" << endl;
+        }
+    } else {
+        if(is_mm(b)) {
+            cout << "YYMM" << endl;
+        } else {
+            cout << "NA" << endl;
+        }
+    }
+
+    return 0;
+}
+#endif
