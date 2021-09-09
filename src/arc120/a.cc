a2fb9c92ccff7cfc5fe0d82cd0f6a01383dab898
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2021-09-09 10:48:59 +0900

    arc120a

---
+++ src/arc120/a.cc
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define rep(i, n) for(int i = 0; i < (int)(n); i++)
+
+vector<ll> solve(int n, vector<ll> &a) {
+
+    vector<ll> sums(n, 0ll), max_value(n);
+    rep(i, n) {
+        if(i == 0) {
+            sums[0] = a[i];
+            max_value[0] = a[i];
+        } else {
+            sums[i] = sums[i - 1] + a[i];
+            max_value[i] = max(max_value[i - 1], a[i]);
+        }
+    }
+    vector<ll> ans(n);
+    ll acc = 0ll;
+    rep(i, n) {
+        ans[i] = acc + sums[i] + max_value[i] * (i + 1);
+        acc += sums[i];
+    }
+	return ans;
+}
+
+#ifndef _debug
+int main() {
+    int n;
+    cin >> n;
+    vector<ll> a(n);
+    rep(i, n) cin >> a[i];
+    auto ans = solve(n, a);
+    for(auto e : ans)
+        cout << e << endl;
+    return 0;
+}
+#endif
