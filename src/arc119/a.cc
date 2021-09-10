75819947aab5d4b764d013b020327d27c550743c
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2021-09-10 15:09:02 +0900

    119

---
+++ src/arc119/a.cc
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define rep(i, n) for(int i = 0; i < (int)(n); i++)
+
+ll pow(ll x, ll n) {
+    ll res = 1ll;
+    while(n > 0) {
+        if(n & 1) {
+            res *= x;
+        }
+        n >>= 1;
+        x *= x;
+    }
+    return res;
+}
+
+ll solve(ll n) {
+
+    ll b = 0ll;
+    while(true) {
+        if(pow(2, b + 1) > n)
+            break;
+        b++;
+    }
+    ll ans = numeric_limits<ll>::max();
+    for(ll i = 0; i <= b; i++) {
+        ll a = n / pow(2ll, i);
+        ll c = n - pow(2ll, i) * a;
+        //cout << a << " " << i << " " << c << endl;
+        ans = min(ans, a + i + c);
+    }
+    return ans;
+}
+
+#ifndef _debug
+int main() {
+    ll n;
+    cin >> n;
+    cout << solve(n) << endl;
+    return 0;
+}
+#endif
