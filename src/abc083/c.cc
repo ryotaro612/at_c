37f7ecf320b952777a974954b8a3147636a7c5ef
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2021-09-17 16:28:58 +0900

    083

---
+++ src/abc083/c.cc
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+typedef long long ll;
+using namespace std;
+#define rep(i, n) for(int i = 0; i < (int)(n); i++)
+
+ll solve(ll x, ll y) {
+    ll current = x, res = 1;
+
+    while(current * 2ll <= y) {
+        res++;
+        current *= 2ll;
+        //cout << current << endl;
+    }
+    return res;
+}
+
+#ifndef _debug
+int main() {
+    ll x, y;
+    cin >> x >> y;
+    cout << solve(x, y) << endl;
+    return 0;
+}
+#endif
