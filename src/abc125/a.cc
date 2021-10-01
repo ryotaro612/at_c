f7d1e9c50020e1773f4c61323b75cfddf83ca466
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2021-10-01 21:58:02 +0900

    abc125

---
+++ src/abc125/a.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+typedef long long ll;
+using namespace std;
+#define rep(i, n) for(int i = 0; i < (int)(n); i++)
+
+#ifndef _debug
+int main() {
+    int a, b, t;
+    cin >> a >> b >> t;
+    int ans = 0;
+    for(int i = a; i <= t; i += a) {
+        ans += b;
+    }
+    cout << ans << endl;
+    return 0;
+}
+#endif
