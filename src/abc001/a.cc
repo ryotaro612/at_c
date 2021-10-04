67dfa58c68c25526d7f9d4db09043a37ed721182
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2021-10-04 16:27:23 +0900

    123

---
+++ src/abc001/a.cc
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+
+#ifndef _debug
+int main() {
+	int h1, h2;
+	cin >> h1 >> h2;
+	cout << h1 - h2	<< endl;
+	return 0;
+}
+#endif
---
+++ src/abc124/a.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int solve(int a, int b) {
+    vector<int> ans = {a + a - 1, b + a, b + b - 1};
+    return *max_element(ans.begin(), ans.end());
+}
+
+#ifndef _debug
+int main() {
+    int a, b;
+    cin >> a >> b;
+    cout << solve(a, b) << endl;
+    return 0;
+}
+#endif
