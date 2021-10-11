95d6d3d005a4fa5e696a41ada41eaea37743dae7
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2021-10-11 14:37:26 +0900

    arc0043

---
+++ src/arc003/a.cc
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+typedef long long ll;
+using namespace std;
+#define rep(i, n) for(int i = 0; i < (int)(n); i++)
+
+#ifndef _debug
+int main() {
+    int n;
+    cin >> n;
+    string r;
+    cin >> r;
+    int sum = 0;
+    rep(i, r.size()) { sum += max(0, 4 + ('A' - r[i])); }
+    cout << fixed << setprecision(12) << (double)sum / (double)r.size();
+
+    return 0;
+}
+#endif
