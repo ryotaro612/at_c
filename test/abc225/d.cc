5d9979f3c410210f4054a51ddd9ab626b796bd0b
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2021-11-01 13:05:50 +0900

    forbatr

--- test/abc225/d.cc
+++ test/abc225/d.cc
@@ -2,29 +2,12 @@
 #include <gtest/gtest.h>
 
 TEST(abc225d, 1) {
-	vector<vector<int>> queries = {
-{1,6,3},
-{1,4,1},
-{1,5,2},
-{1,2,7},
-{1,3,5},
-{3,2},
-{3,4},
-{3,6},
-{2,3,5},
-{2,4,1},
-{1,1,5},
-{3,2},
-{3,4},
-{3,6}
-	};
-	vector<vector<int>> res = solve(7, 14, queries);
-   vector<vector<int>> ans = {{5,6,3,5,2,7},
-{2,4,1},
-{5,6,3,5,2,7},
-{4,1,5,2,7},
-{1,4},
-  {2,6,3}};
-  EXPECT_EQ(ans, res);
+    vector<vector<int>> queries = {
+        {1, 6, 3}, {1, 4, 1}, {1, 5, 2}, {1, 2, 7}, {1, 3, 5}, {3, 2}, {3, 4},
+        {3, 6},    {2, 3, 5}, {2, 4, 1}, {1, 1, 5}, {3, 2},    {3, 4}, {3, 6}};
+    vector<vector<int>> res = solve(7, 14, queries);
+    vector<vector<int>> ans = {
+        {5, 6, 3, 5, 2, 7}, {2, 4, 1}, {5, 6, 3, 5, 2, 7},
+        {4, 1, 5, 2, 7},    {1, 4},    {2, 6, 3}};
+    EXPECT_EQ(ans, res);
 }
-
