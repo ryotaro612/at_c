2859f9fea99d7caad469af0e3176812052224d6e
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2021-11-04 12:21:45 +0900

    124b

--- src/abc124/b.cc
+++ src/abc124/b.cc
@@ -1,40 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
+#define rep(i, n) for(int i = 0; i < (int)(n); i++)
+
+int solve(int n, vector<int> &h) {
+    int mx = 0, res = 0;
+    rep(i, n) {
+        if(mx <= h[i])
+            res++;
+        mx = max(mx, h[i]);
+    }
+    return res;
+}
 
 #ifndef _debug
 int main() {
     int n;
     cin >> n;
-    vector<ll> a(n), b(n);
-    for(int i = 0; i < n; i++)
-        cin >> a[i];
-    for(int i = 0; i < n; i++)
-        cin >> b[i];
-
-    vector<ll> cand(n);
-    for(int i = 0; i < n; i++) {
-        cand[i] = a[0] ^ b[i];
-    }
-    set<ll> ans;
-    sort(b.begin(), b.end());
-    for(int i = 0; i < n; i++) {
-        vector<ll> temp(n);
-        for(int j = 0; j < n; j++) {
-            temp[j] = a[j] ^ cand[i];
-        }
-        sort(temp.begin(), temp.end());
-        bool ok = true;
-        for(int j = 0; j < n; j++) {
-            if(b[j] != temp[j])
-                ok = false;
-        }
-        if(ok)
-            ans.insert(cand[i]);
-    }
-    cout << (int)ans.size() << endl;
-    for(auto aa : ans)
-        cout << aa << endl;
+    vector<int> h(n);
+    rep(i, n) cin >> h[i];
+    cout << solve(n, h) << endl;
     return 0;
 }
 #endif
---
+++ src/arc124/b.cc
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+#ifndef _debug
+int main() {
+    int n;
+    cin >> n;
+    vector<ll> a(n), b(n);
+    for(int i = 0; i < n; i++)
+        cin >> a[i];
+    for(int i = 0; i < n; i++)
+        cin >> b[i];
+
+    vector<ll> cand(n);
+    for(int i = 0; i < n; i++) {
+        cand[i] = a[0] ^ b[i];
+    }
+    set<ll> ans;
+    sort(b.begin(), b.end());
+    for(int i = 0; i < n; i++) {
+        vector<ll> temp(n);
+        for(int j = 0; j < n; j++) {
+            temp[j] = a[j] ^ cand[i];
+        }
+        sort(temp.begin(), temp.end());
+        bool ok = true;
+        for(int j = 0; j < n; j++) {
+            if(b[j] != temp[j])
+                ok = false;
+        }
+        if(ok)
+            ans.insert(cand[i]);
+    }
+    cout << (int)ans.size() << endl;
+    for(auto aa : ans)
+        cout << aa << endl;
+    return 0;
+}
+#endif
