6fb4623be972f57e9e2b92969c8034077d873f82
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2021-11-21 14:40:24 +0900

    sumitturst2019

--- src/sumitrust2019/f.cc
+++ src/sumitrust2019/f.cc
@@ -3,31 +3,36 @@ typedef long long ll;
 using namespace std;
 #define rep(i, n) for(int i = 0; i < (int)(n); i++)
 
-ll solve(ll t1, ll t2, ll a1, ll a2, ll b1, ll b2) {
+ll solve(__int128_t t1, __int128_t t2, __int128_t a1, __int128_t a2,
+         __int128_t b1, __int128_t b2) {
     if(t1 * a1 + t2 * a2 == b1 * t1 + b2 * t2)
         return -1ll;
+    if((a1 * t1 < b1 * t1 && a1 * t1 + a2 * t2 < b1 * t1 + b2 * t2) ||
+       (b1 * t1 < a1 * t1 && b1 * t1 + b2 * t2 < a1 * t1 + a2 * t2)) {
+        return 0ll;
+    }
     if(b1 < a1 && a2 < b2) {
         swap(a1, b1);
         swap(a2, b2);
-    } else if(!(a1 < b1 && b2 < a2)) {
-        return 0ll;
     }
-    // ll unit_dist = a1 * t1 + a2 * t2 - b1 * t1 - b2 * t2;
-    // x * unit_dist <= (b1 - a1) * t1;
-    ll ub = numeric_limits<ll>::max() / (2ll * (b1 * t1 + b2 * t2)), lb = -1ll;
+    // a1 < b1 && b2 < a2
+    __int128_t ub = numeric_limits<ll>::max(), lb = -1;
     while(ub - lb > 1ll) {
-        ll mid = (ub + lb) / 2ll;
-        ll dist_a = (a1 * t1 + a2 * t2) * mid,
-           dist_b = (b1 * t1 + b2 * t2) * mid;
-        ll dist = dist_a - dist_b;
-        //cout << "mid: " << mid << " dist " << dist << endl;
-        if(dist <= (b1 - a1) * t1 ||
-           (0 <= (b1 - a1) * t1 - dist &&
-            (b1 - a1) * t1 - dist <= t2 * (a2 - b2))) {
+        __int128_t mid = (__int128_t)(ub + lb) / (__int128_t)2;
+        __int128_t dist_a = (a1 * t1 + a2 * t2) * mid,
+                   dist_b = (b1 * t1 + b2 * t2) * mid;
+        __int128_t dist = dist_a - dist_b;
+        if(dist <= (b1 - a1) * t1) {
             lb = mid;
         } else {
             ub = mid;
         }
+    }
+    __int128_t dist_a = (a1 * t1 + a2 * t2) * lb,
+               dist_b = (b1 * t1 + b2 * t2) * lb;
+    __int128_t dist = dist_a - dist_b;
+    if(dist == (b1 - a1) * t1) {
+        return lb * 2ll;
     }
     return lb * 2ll + 1ll;
 }
--- tests/sumitrust2019/f.cc
+++ tests/sumitrust2019/f.cc
@@ -4,11 +4,22 @@
 TEST(sumitrust2019f, 1) { EXPECT_EQ(1ll, solve(1, 2, 10, 10, 12, 4)); }
 
 TEST(sumitrust2019f, 2) {
-    EXPECT_EQ(-1ll, solve(100ll, 1ll, 101ll, 101ll, 102ll, 1ll));
+    EXPECT_EQ(-1ll, 
+    solve(100ll, 1ll, 101ll, 101ll, 102ll, 1ll));
 }
 
 TEST(sumitrust2019f, 3) {
     ll t1 = 12000ll, t2 = 15700ll, a1 = 3390000000ll, a2 = 3810000000ll,
        b1 = 5550000000ll, b2 = 2130000000ll;
     EXPECT_EQ(113ll, solve(t1, t2, a1, a2, b1, b2));
+}
+
+TEST(sumitrust2019f, 4) { 
+    EXPECT_EQ(0ll, solve(1, 2, 10, 10, 2, 2)); }
+
+TEST(sumitrust2019f, 5) { 
+    EXPECT_EQ(0ll, solve(1, 2, 2, 2, 10, 10)); }
+
+TEST(sumitrust2019f, 6) {
+    EXPECT_EQ(0ll, solve(1, 1, 1, 2, 10, 1));
 }
