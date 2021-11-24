b36c30846672d0aeeb3f2295dd908c6f30c35dd0
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2021-11-24 20:39:56 +0900

    abc228

---
+++ src/abc228/e.cc
@@ -0,0 +1,37 @@
+#define _GLIBCXX_DEBUG
+#include <bits/stdc++.h>
+typedef long long ll;
+using namespace std;
+#define rep(i, n) for(int i = 0; i < (int)(n); i++)
+ll const MOD = 998244353ll;
+
+ll pow(ll x, ll n, ll mod) {
+    x %= mod;
+    ll res = 1ll;
+    while(n) {
+        if(n & 1) {
+            res *= x;
+            res %= mod;
+        }
+        x *= x;
+        x %= mod;
+        n >>= 1;
+    }
+    return res;
+}
+
+ll solve(ll n, ll k, ll m) {
+    if(m % MOD == 0)
+        return 0ll;
+    ll b = pow(k, n, MOD - 1ll);
+    return pow(m, b, MOD);
+}
+
+#ifndef _debug
+int main() {
+    ll n, k, m;
+    cin >> n >> k >> m;
+    cout << solve(n, k, m) << endl;
+    return 0;
+}
+#endif
