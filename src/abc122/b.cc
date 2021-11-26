a9341cd327448cd12bda61743ef0f89535a3d93d
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2021-11-26 21:55:50 +0900

    arc127b

---
+++ src/abc122/b.cc
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+typedef long long ll;
+using namespace std;
+#define rep(i, n) for(int i = 0; i < (int)(n); i++)
+
+#ifndef _debug
+int main() {
+    string s;
+    cin >> s;
+    int n = s.size(), res = 0;
+    for(int i=0;i<n;i++) {
+        for(int j=i+1;j<=n;j++) {
+            string temp = string(s.begin() + i, s.begin() + j);
+            //cout << temp << endl;
+            bool ok = true;
+            for(auto c: temp) {
+                if(!(c == 'A' || c == 'C' || c == 'G' || c == 'T'))
+                    ok = false;
+            }
+            if(ok)
+                res = max(res, (int) temp.size());
+
+        }
+    }
+    cout << res << endl;
+    return 0;
+}
+#endif
---
+++ src/arc127/b.cc
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define rep(i, n) for(int i=0;i<(int)(n);i++)
+
+vector<string> solve(int n, int l) {
+
+}
+
+#ifndef _debug
+int main() {
+	int n, l;
+	cin >> n >> l;
+	vector<string> res = solve(n, l);
+	for(auto e: res)
+		cout << e << endl;
+	return 0;
+}
+#endif
