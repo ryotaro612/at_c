0786805d8a699859bf4741dbaf8854e647d57ed4
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2021-12-05 19:07:52 +0900

    230a

---
+++ src/abc230/a.cc
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+#ifndef _debug
+int main() {
+    int n;
+    cin >> n;
+    int number;
+    if(n >= 42)
+        number = n + 1;
+    else
+        number = n;
+    char res[7];
+    sprintf(res, "AGC%03d", number);
+    cout << res << endl;
+}
+#endif
