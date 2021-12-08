4e11c1bbdbeeadbd236feeca76dc180159f589fe
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2021-12-08 12:33:14 +0900

    arc131

---
+++ src/arc131/c.cc
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define rep(i, n) for(int i = 0; i < (int)(n); i++)
+
+string solve(int n, vector<ll> &a) {
+    if(n % 2 == 1)
+        return "Win";
+    ll x = 0ll;
+    rep(i, n) x ^= a[i];
+    rep(i, n) {
+        if(a[i] == x)
+            return "Win";
+    }
+    return "Lose";
+}
+
+#ifndef _debug
+int main() {
+    int n;
+    cin >> n;
+    vector<ll> av(n);
+    rep(i, n) cin >> av[i];
+    cout << solve(n, av) << endl;
+    return 0;
+}
+#endif
