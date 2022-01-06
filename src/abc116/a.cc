7fd9ad3728b6a7a06bd74bb509b9785747d84183
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2022-01-06 10:36:05 +0900

    abc116a

--- src/abc116/a.cc
+++ src/abc116/a.cc
@@ -1,3 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+#ifdef ONLINE_JUDGE
+int main() {
+  int ab, bc, ca;
+  cin >> ab >> bc >> ca;
+  cout << ab * bc / 2 << endl;
+  return 0;
+}
+#endif
