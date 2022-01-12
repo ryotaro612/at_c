e3bf6f8205ad4a62583edb90455d2392a6ed211a
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2022-01-12 10:57:29 +0900

    abc114

---
+++ src/abc114/a.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef ONLINE_JUDGE
+int main() {
+  int x;
+  cin >> x;
+
+  if(x == 7 || x == 5 || x == 3)
+    cout << "YES";
+  else
+    cout << "NO";
+  cout << endl;
+  return 0;
+}
+#endif
