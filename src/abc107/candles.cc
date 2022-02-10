b73d51bcba86ef5cef8ec01f32f312026e654de9
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2022-02-10 10:46:28 +0900

    format.

--- src/abc107/candles.cc
+++ src/abc107/candles.cc
@@ -1,21 +1,22 @@
-#include<vector>
-#include<cmath>
-#include<iostream>
+#include <cmath>
+#include <iostream>
+#include <vector>
 
 using namespace std;
 
 long long candles(int n, int k, std::vector<long long> x) {
   long long answer = 400000000;
-  for(int start_index = 0; start_index+k<=x.size(); start_index++) {
-    long long start = x[start_index];
-    long long end = x[start_index+k-1];
 
-    if(end <=0) {
+  for (int start_index = 0; start_index + k <= x.size(); start_index++) {
+    long long start = x[start_index];
+    long long end = x[start_index + k - 1];
+
+    if (end <= 0) {
       answer = min(answer, -start);
       continue;
     }
-    if(start <0 && end > 0) {
-      long long temp = min(2*(-start) + end, 2*end - start);
+    if (start < 0 && end > 0) {
+      long long temp = min(2 * (-start) + end, 2 * end - start);
       answer = min(temp, answer);
       continue;
     }
