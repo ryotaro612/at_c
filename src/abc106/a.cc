5da104e4b6ac3fa722deb3919b110121cb597ec5
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2022-02-11 13:03:32 +0900

    10c6

---
+++ src/abc106/a.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+
+#ifdef ONLINE_JUDGE
+int main() {
+  int a, b;
+  cin >> a >> b;
+  cout << a * b + 1 - a - b << endl;
+  return 0;
+}
+#endif
