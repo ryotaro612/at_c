0038efd1ea74939242a54bfc80c6e46907e21092
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2022-02-21 11:01:43 +0900

    105b

---
+++ src/abc105/b.cc
@@ -0,0 +1,25 @@
+#ifndef ONLINE_JUDGE
+#define _GLIBCXX_DEBUG
+#endif
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+
+//#ifdef ONLINE_JUDGE
+int main() {
+  int n;
+  cin >> n;
+  rep(i, 26) {
+    rep(j, 16) {
+      if (i * 4 + j * 7 == n) {
+        cout << "Yes" << endl;
+        return 0;
+      }
+    }
+  }
+  cout << "No" << endl;
+  return 0;
+}
+//#endif
