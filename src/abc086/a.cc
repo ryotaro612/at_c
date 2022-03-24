9bf849cde173e951d492e65618b2f85ffb9c9a50
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2022-03-24 14:32:10 +0900

    086.

---
+++ src/abc086/a.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// int main() {
+//   int a, b;
+//   cin >> a >> b;
+//   int is_odd = a * b % 2;
+//   if (is_odd)
+//     cout << "Odd";
+//   else
+//     cout << "Even";
+//   cout << endl;
+//   return 0;
+// }
