8d6c5e9ceaea39797e0dfa9580366e0e14ded2bd
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2022-03-25 12:44:47 +0900

    086b.

---
+++ src/abc086/b.cc
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// int main() {
+//   int a, b;
+//   cin >> a >> b;
+//   string s = to_string(a) + to_string(b);
+//   int n = stoi(s);
+//   for(int i = 1;i*i <= n;i++) {
+//     if(i*i == n) {
+//        cout << "Yes"  << endl;
+//       return 0;
+//     }
+
+//   }
+//   cout << "No" << endl;
+//   return 0;
+// }
