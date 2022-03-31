92fd46cc037654e9edefdae705c385599979a168
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2022-03-31 12:11:49 +0900

    084b

--- src/abc084/a.cc
+++ src/abc084/a.cc
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
-// int main() {
+/// int main() {
 //   int n;
 //   cin >> n;
 //   cout << 24 - n + 24 << endl;
---
+++ src/abc084/b.cc
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+// int main() {
+//   int a, b;
+//   cin >> a >> b;
+//   string s;
+//   cin >> s;
+//   for(int i = 0;i<a;i++) {
+//     if(!(0 <= s[i] && s[i] <= 9)) {
+//       cout << "No" << endl;
+//       return 0;
+//     }
+//   }
+//   if(s[a] != '-') {
+//     cout << "No" << endl;
+//     return 0;
+//   }
+//   for(int i = 0;i<b;i++) {
+//     if(!(0 <= s[a+ 1 + i] && s[a + 1 + i] <= 9)) {
+//       cout << "No" << endl;
+//       return 0;
+//     }
+//   }
+//   cout << "Yes" << endl;
+//   return 0;
+// }
