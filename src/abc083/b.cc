c614c5628dac32c161fb9aa6a1f205666f191261
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2022-04-04 11:28:57 +0900

    083b

---
+++ src/abc083/b.cc
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+// int main() {
+//   int n, a, b;
+//   cin >> n >> a >> b;
+//   int res = 0;
+//   for(int i = 1;i<=n;i++) {
+//     string s = to_string(i);
+//     int temp = 0;
+//     for(int j = 0;j<s.size();j++) {
+//       temp += s[j] - '0';
+//     }
+//     if(a <= temp && temp <= b) {
+//       res += i;
+//     }
+//   }
+//   cout << res << endl;
+//   return 0;
+// }
