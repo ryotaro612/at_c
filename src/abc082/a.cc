8ad7e7e3e2e1c294011cd9d97d78f4edab1b8202
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2022-04-05 12:00:14 +0900

    082a

---
+++ src/abc082/a.cc
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+// int main() {
+//   int a, b;
+//   cin >> a >> b;
+//   if((a + b) % 2 == 0) {
+//     cout << (a + b) / 2 << endl;
+//     return 0;
+//   }
+//   cout << (a + b + 1) / 2 << endl;
+//   return 0;
+// }
