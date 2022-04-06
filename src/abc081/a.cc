de6869f0a514e617518101f8cf351c3b000e5667
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2022-04-06 10:36:29 +0900

    081a

---
+++ src/abc081/a.cc
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+// int main() {
+//   string s;
+//   cin >> s;
+//   int res = 0;
+//   for(int i = 0;i<s.size();i++) {
+//     if(s[i] == '1')
+//       res++;
+//   }
+//   cout << res << endl;
+// }
