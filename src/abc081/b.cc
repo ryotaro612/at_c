6d0f41f767af472a080df54cf072361b75a2546a
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2022-04-07 13:02:54 +0900

    081b

---
+++ src/abc081/b.cc
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+// int main() {
+//   int n;
+//   cin >> n;
+//   vector<ll> av(n);
+//   for(int i =0;i<n;i++)
+//     cin >> av[i];
+//   int result = 0;
+//   while(true) {
+//     bool ok = true;
+//     for(int i = 0;i<n;i++) {
+//       if(av[i] % 2ll == 0) {
+//         av[i] /= 2ll;
+//       } else
+//         ok = false;
+//     }
+//     if(ok)
+//       result++;
+//     else
+//       break;
+//   }
+//   cout << result << endl;
+//   return 0;
+// }
