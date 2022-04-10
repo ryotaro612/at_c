cb3c249ce2ffa8e51dae852fcc26139168c29b92
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2022-04-10 13:38:28 +0900

    138b

--- src/arc138/b.cc
+++ src/arc138/b.cc
@@ -7,6 +7,39 @@ using ll = long long;
 using ld = long double;
 #define rep(i, n) for (int i = 0; i < (int)(n); i++)
 
-//#ifdef ONLINE_JUDGE
-int main() { return 0; }
-//#endif
+#ifdef ONLINE_JUDGE
+int main() {
+  int n;
+  cin >> n;
+  vector<int> av(n);
+  rep(i, n) cin >> av[i];
+
+  deque<int> que;
+  for (auto e : av)
+    que.push_back(e);
+
+  int flip = 0;
+  while (!que.empty()) {
+    // 0 0 -> 0, 1 0 -> 1, 0 1 -> 1 , 1 1 -> 0
+    if ((que.back() ^ flip) == 0) {
+      que.pop_back();
+      continue;
+    } else if ((que.front() ^ flip) == 0) {
+      que.pop_front();
+      if (flip)
+        flip = 0;
+      else
+        flip = 1;
+    } else {
+      break;
+    }
+  }
+  if (que.empty())
+    cout << "Yes";
+  else
+    cout << "No";
+
+  cout << endl;
+  return 0;
+}
+#endif
