468e91b67774332392f5e0b7bb1f19d82fa12c81
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2022-04-13 10:54:25 +0900

    79b

---
+++ src/abc079/b.cc
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+// ll solve(int n, vector<ll> &dp) {
+//   if(n == 0)
+//     return 2;
+//   if(n == 1)
+//     return 1;
+//   if(dp[n] == -1ll) {
+//     return dp[n] = solve(n-1, dp) + solve(n-2, dp);
+//   }
+//   return dp[n];
+// }
+
+// int main() {
+//   int n;
+//   cin >> n;
+//   vector<ll> dp(n+1, -1);
+//   cout << solve(n, dp) << endl;
+//   return 0;
+// }
