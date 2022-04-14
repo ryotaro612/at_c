c6a427474b5524903c0ba7a61f52f8cce2214082
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2022-04-14 10:27:54 +0900

    078a

---
+++ src/abc078/a.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+// int main() {
+//   char x, y;
+//   cin >> x >> y;
+//   if (x < y) {
+//     cout << "<";
+//   } else if (x == y)
+//     cout << "=";
+//   else
+//     cout << ">";
+//   cout << endl;
+//   return 0;
+// }
