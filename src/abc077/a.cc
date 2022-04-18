d795aa40ac6a3d718c4b3337e3f070fa775d8f41
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2022-04-18 11:08:23 +0900

    077a

---
+++ src/abc077/a.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// int main() {
+//   string s, s1;
+//   cin >> s >> s1;
+//   string rev_s(s), rev_s1(s1);
+//   reverse(rev_s.begin(), rev_s.end());
+//   reverse(rev_s1.begin(), rev_s1.end());
+//   if(s == rev_s1 && rev_s == s1)
+//     cout << "YES";
+//   else
+//     cout << "NO";
+//   cout << endl;
+//   return 0;
+// }
