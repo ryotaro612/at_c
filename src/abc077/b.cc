506fc06c60556387e84001c790d62217d98cd425
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2022-04-19 16:18:44 +0900

    077b

---
+++ src/abc077/b.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+// int main() {
+//   ll n;
+//   cin >> n;
+//   for (ll i = 0ll; i <= n; i++) {
+//     if (n < (i + 1ll) * (i + 1ll)) {
+//       cout << i * i << endl;
+//       return 0;
+//     }
+//   }
+//   return 0;
+// }
