c51704d0bbda7f64326d611a20dad351839ce1ec
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2022-04-21 11:34:11 +0900

    b.cc

---
+++ src/abc076/b.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+// int main() {
+//   int n, k;
+//   cin >> n >> k;
+//   int val = 1;
+//   for(int i = 0;i <n;i++) {
+//     if(val + k < val * 2) {
+//       val += k;
+//     } else {
+//       val *= 2;
+//     }
+//   }
+//   cout << val << endl;
+//   return 0;
+// }
