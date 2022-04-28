1657fcd0eae473dc51365b455b9ad6bbecd84ce7
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2022-04-28 12:50:52 +0900

    074b

---
+++ src/abc074/b.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+// int main() {
+//   int n, k;
+//   cin >> n >> k;
+
+//   int res = 0;
+//   for (int i = 0; i < n; i++) {
+//     int x;
+//     cin >> x;
+//     res += 2 * min(x, abs(x - k));
+//   }
+//   cout << res << endl;
+
+//   return 0;
+// }
