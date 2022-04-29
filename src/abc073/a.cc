dd1895516ec7f994807a020cef366749d4f53b97
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2022-04-29 11:20:27 +0900

    073

---
+++ src/abc073/a.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+// int main() {
+//   string s;
+//   cin >> s;
+//   for(char c: s) {
+//     if(c == '9') {
+//        cout << "Yes" << endl;
+//       return 0;
+//     }
+//   }
+//   cout << "No" << endl;
+//   return 0;
+// }
