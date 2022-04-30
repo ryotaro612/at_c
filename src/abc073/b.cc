afce0b0c9d05def8de4823098fc4ca2e03d04890
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2022-04-30 15:01:37 +0900

    073b

---
+++ src/abc073/b.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// int main() {
+//   int n;
+//   int res = 0;
+//   cin >> n;
+//   for(int i = 0;i<n;i++) {
+//     int l, r;
+//     cin >> l >> r;
+//     res += r - l + 1;
+//   }
+//   cout << res << endl;
+//   return 0;
+// }
