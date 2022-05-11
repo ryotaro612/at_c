992b66562d9856c303eae854758eefce58a48e5c
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2022-05-11 15:52:17 +0900

    cmake.

---
+++ src/abc072/a.cc
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+// int main() {
+// 	ll x, t;
+//   cin >> x >> t;
+//   cout << max(x - t, 0ll) << endl;
+//   return 0;
+// }
