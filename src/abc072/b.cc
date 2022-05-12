3ff1842ea04b419fdc137f1a046ac16b66094ca4
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2022-05-12 10:57:01 +0900

    072b

---
+++ src/abc072/b.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+// int main() {
+//   string s;
+//   cin >> s;
+//   string res;
+//   for(int i = 0;i<static_cast<int>(s.size());i++) {
+//     if (i %2 == 0)
+//       res.push_back(s[i]);
+//   }
+//   cout << res << endl;
+// }
