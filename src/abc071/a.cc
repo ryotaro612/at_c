4233d7e90889acd1a9b1d24aabb7754063701f4e
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2022-05-13 13:29:23 +0900

    071a

---
+++ src/abc071/a.cc
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+// int main() {
+//   int x, a, b;
+//   cin >> x >> a >> b;
+//   if (abs(a - x) < abs(b - x))
+//     cout << "A" << endl;
+//   else
+//     cout << "B" << endl;
+//   return 0;
+// }
