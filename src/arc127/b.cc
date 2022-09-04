7ef875c98a357142cfa70b5d8c9aedacf2cbb749
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2022-09-04 16:30:13 +0900

    arc127b

--- src/arc127/b.cc
+++ src/arc127/b.cc
@@ -4,36 +4,60 @@ typedef long long ll;
 #define rep(i, n) for (int i = 0; i < (int)(n); i++)
 
 ll pow3(int n) {
-  ll result = 1ll;
-  rep(i, n) result *= 3ll;
-
-  return result;
+  ll res = 1ll;
+  while (n-- > 0ll)
+    res *= 3ll;
+  return res;
 }
 
-vector<string> solve(int n, int l) {
-
-  vector<string> sv(3 * n);
-
-  rep(i, l) {
-    sort(sv.begin(), sv.end(), greater<string>());
-    vector<int> dv(3, n);
-    int j = 0;
-    int digit = 0;
-    while (j < 3 * n) {
-      for (int k = 0; k < pow3(l - 1 - i); k++) {
-        sv[j++].append(to_string(digit));
-        dv[digit]--;
-        if (dv[digit] == 0 || 3 * n <= j)
-          break;
-      }
-      digit = (digit + 1) % 3;
-    }
+string tri(ll n, int l) {
+  string s;
+  for (int i = l - 1; i >= 0; i--) {
+    ll base = pow3(i);
+    if (n >= 2ll * base) {
+      n -= 2ll * base;
+      s.push_back('2');
+    } else if (n >= base) {
+      n -= base;
+      s.push_back('1');
+    } else
+      s.push_back('0');
   }
-  sort(sv.begin(), sv.end());
-  return sv;
+  assert(n == 0ll);
+  return s;
 }
 
-#ifdef ONLINE_JUDGE
+vector<string> solve(ll n, ll l) {
+  vector<string> sv;
+  for (ll i = 0ll; i < n; i++) {
+    sv.push_back(tri(2ll * pow3(l - 1ll) + i, l));
+  }
+  // cout << "doge" << sv.size() << endl;
+  //  for (auto s : sv)
+  //    cout << s << endl;
+  //  cout << "---" << endl;
+  vector<string> res(sv.begin(), sv.end());
+  for (string s : sv) {
+    map<char, char> mp;
+    mp['0'] = '1';
+    mp['1'] = '2';
+    mp['2'] = '0';
+    string new_s;
+    for (auto c : s)
+      new_s.push_back(mp[c]);
+    res.push_back(new_s);
+    mp['0'] = '2';
+    mp['1'] = '0';
+    mp['2'] = '1';
+    new_s = "";
+    for (auto c : s)
+      new_s.push_back(mp[c]);
+    res.push_back(new_s);
+  }
+  return res;
+}
+
+//#ifdef ONLINE_JUDGE
 int main() {
   int n, l;
   cin >> n >> l;
@@ -42,4 +66,4 @@ int main() {
     cout << e << endl;
   return 0;
 }
-#endif
+//#endif
