82fbea7de13500572452a659635717ed7a60e65c
Ryotaro Nakamura <nakamura.ryotaro.kzs@gmail.com>
2023-02-11 12:31:52 -0500

    arc00c

--- src/arc005/c.cc
+++ src/arc005/c.cc
@@ -1,15 +1,9 @@
 #ifndef ONLINE_JUDGE
 #define _GLIBCXX_DEBUG
-#include <functional>
-#include <queue>
 #endif
 #include <bits/stdc++.h>
 using namespace std;
-using ll = long long;
-using ld = long double;
-using ull = unsigned long long;
 #define rep(i, n) for (int i = 0; i < (int)(n); i++)
-#define as_int(a) static_cast<int>(a)
 
 #ifdef ONLINE_JUDGE
 int main() {
