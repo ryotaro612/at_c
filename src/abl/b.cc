4ba2c1cd4469eef83c757e8e3c7301e258ecf8ec
Ryotaro Nakamura <nakamura.ryotaro.kzs@gmail.com>
2023-03-14 12:57:27 -0400

    ablb

---
+++ src/abl/b.cc
@@ -0,0 +1,23 @@
+#ifndef ONLINE_JUDGE
+#define _GLIBCXX_DEBUG
+#endif
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ld = long double;
+using ull = unsigned long long;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define as_int(a) static_cast<int>(a)
+
+#ifdef ONLINE_JUDGE
+int main() {
+  ll a, b, c, d;
+  cin >> a >> b >> c >> d;
+  if (b < c || d < a) {
+    cout << "No";
+  } else
+    cout << "Yes";
+  cout << endl;
+  return 0;
+}
+#endif
