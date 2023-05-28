221bd0cac912502a370c5b65eaab76d097a643a2
Ryotaro Nakamura <nakamura.ryotaro.kzs@gmail.com>
2023-05-28 13:10:30 -0400

    a

---
+++ src/arc160/b.cc
@@ -0,0 +1,136 @@
+#ifndef ONLINE_JUDGE
+#define _GLIBCXX_DEBUG
+#endif
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ld = long double;
+using ull = unsigned long long;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+template <ll P> class Mod {
+public:
+  ll v;
+  Mod<P>() : v(0) {}
+  Mod<P>(ll v) : v(v % P) {}
+  ll get() { return v; }
+  Mod<P> operator*(Mod<P> &a) { return Mod<P>(v * a.v % P); }
+  Mod<P> operator*(ll &b) { return Mod<P>(v * b % P); }
+  friend Mod<P> operator+(const Mod<P> &lhs, const ll &rhs) {
+    return Mod(lhs.v + rhs % P);
+  }
+  friend Mod<P> operator-(const Mod<P> &lhs, const Mod<P> &rhs) {
+    ll res = lhs.v - rhs.v;
+    if (res < 0)
+      res += P;
+    return Mod<P>(res);
+  }
+  friend Mod<P> operator*(const Mod<P> &lhs, const Mod<P> &rhs) {
+    return Mod<P>(lhs.v * rhs.v % P);
+  }
+  friend Mod<P> operator/(const Mod<P> &lhs, const Mod<P> &rhs) {
+    ll b = P, u = 1, v = 0, a = rhs.v;
+    while (b) {
+      ll t = a / b;
+      a -= t * b;
+      swap(a, b);
+      u -= t * v;
+      swap(u, v);
+    }
+    u %= P;
+    if (u < 0)
+      u += P;
+    return lhs.v * u % P;
+  }
+  Mod<P> &operator=(const ll &number) {
+    v = number;
+    return *this;
+  }
+  Mod<P> &operator+=(const Mod &rhs) {
+    v += rhs.v;
+    v %= P;
+    return *this;
+  }
+  Mod<P> &operator-=(const Mod &rhs) {
+    v -= rhs.v;
+    if (v < 0)
+      v += P;
+    return *this;
+  }
+  Mod<P> &operator*=(const Mod &rhs) {
+    v *= rhs.v;
+    v %= P;
+    return *this;
+  }
+  Mod<P> &operator++() {
+    v++;
+    v %= P;
+    return *this;
+  }
+  friend bool operator==(const Mod<P> &lhs, const int &rhs) {
+    return lhs.v == rhs;
+  }
+  friend ostream &operator<<(ostream &os, Mod<P> &mod) {
+    os << mod.v;
+    return os;
+  }
+  friend istream &operator>>(istream &is, Mod<P> &mod) {
+    is >> mod.v;
+    return is;
+  }
+
+  Mod<P> pow(ll n) {
+    ll res = 1ll;
+    ll x = v;
+    while (n) {
+      if (n & 1) {
+        res *= x;
+        res %= P;
+      }
+      n >>= 1;
+      x *= x;
+      x %= P;
+    }
+    return res;
+  }
+};
+
+vector<ll> compute_divisors(ll n) {
+  vector<ll> res;
+  for (ll i = 1; i * i <= n; i++) {
+    if (n % i == 0) {
+      res.push_back(i);
+      if (n / i != i)
+        res.push_back(n / i);
+    }
+  }
+  sort(res.begin(), res.end());
+  return res;
+}
+const ll mod = 998244353ll;
+Mod<mod> solve(ll n) {
+  ll m = 1ll;
+
+  while ((m + 1ll) * (m + 1ll) <= n)
+    m++;
+  Mod<mod> res = Mod<mod>(m) * m * m;
+  // cout << "doge" << m << endl;
+  for (ll i = 1; n / i > m; i++) {
+    Mod<mod> temp = n / i - max(n / (i + 1ll), m);
+    res += Mod<mod>(3) * temp * Mod<mod>(i) * Mod<mod>(i);
+  }
+  return res;
+}
+
+#ifdef ONLINE_JUDGE
+int main() {
+  int t;
+  cin >> t;
+
+  rep(_, t) {
+    ll n;
+    cin >> n;
+    cout << solve(n).v << endl;
+  }
+  return 0;
+}
+#endif
