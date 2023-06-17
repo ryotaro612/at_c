bfe928aa52eb5cd2c59f2189188043c61b541da8
Ryotaro Nakamura <nakamura.ryotaro.kzs@gmail.com>
2023-06-17 16:44:04 -0400

    a.cc

---
+++ src/abc306/a.cc
@@ -0,0 +1,40 @@
+#ifndef ONLINE_JUDGE
+#define _GLIBCXX_DEBUG
+#endif
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ld = long double;
+using ull = unsigned long long;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+void debug_() { cout << endl; }
+template <typename Head, typename... Tail> void debug_(Head H, Tail... T) {
+  cout << H << " ";
+  debug_(T...);
+}
+#ifndef ONLINE_JUDGE
+#define debug(...) debug_(__VA_ARGS__)
+#else
+#define debug(...)
+#endif
+
+template <typename T> ostream &operator<<(ostream &os, vector<T> &v) {
+  cout << "[";
+  string s[2] = {", ", "]"};
+  rep(i, v.size()) { cout << v[i] << s[i == int(v.size()) - 1]; }
+  return os;
+}
+
+#ifdef ONLINE_JUDGE
+int main() {
+  int n;
+  string s, res = "";
+  cin >> n >> s;
+  rep(i, n) {
+    res.push_back(s[i]);
+    res.push_back(s[i]);
+  }
+  cout << res << endl;
+  return 0;
+}
+#endif
