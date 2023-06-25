04f7549dfea884c9b3ce97e9981916d75a3edb70
Ryotaro Nakamura <nakamura.ryotaro.kzs@gmail.com>
2023-06-25 01:19:15 -0400

    a

---
+++ src/abc307/a.cc
@@ -0,0 +1,58 @@
+#ifndef ONLINE_JUDGE
+#define _GLIBCXX_DEBUG
+#endif
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ld = long double;
+using ull = unsigned long long;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+template <typename T> ostream &operator<<(ostream &os, vector<T> &v) {
+  os << "[";
+  string s[2] = {", ", "]"};
+  rep(i, v.size()) os << v[i] << s[i == int(v.size()) - 1];
+  return os;
+}
+template <typename T, typename S>
+ostream &operator<<(ostream &os, pair<S, T> a) {
+  os << "{" << a.first << ", " << a.second << "}";
+  return os;
+}
+template <typename T> ostream &operator<<(ostream &os, set<T> &v) {
+  os << "{";
+  string s[2] = {", ", "}"};
+  int size = v.size(), i = 0;
+  for (auto e : v) {
+    os << e << s[i == (size - 1)];
+    i++;
+  }
+  return os;
+}
+void debug_() { cout << endl; }
+
+template <typename Head, typename... Tail> void debug_(Head H, Tail... T) {
+  cout << H << " ";
+  debug_(T...);
+}
+#ifndef ONLINE_JUDGE
+#define debug(...) debug_(__VA_ARGS__)
+#else
+#define debug(...)
+#endif
+
+#ifdef ONLINE_JUDGE
+int main() {
+  int n;
+  cin >> n;
+  rep(i, n) {
+    int res = 0;
+    rep(j, 7) {
+      int a;
+      cin >> a;
+      res += a;
+    }
+    cout << res << " \n"[i == n - 1];
+  }
+  return 0;
+}
+#endif
