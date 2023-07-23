010ca359fe7164a7f832f34b7933b99b4e12fc66
Ryotaro Nakamura <nakamura.ryotaro.kzs@gmail.com>
2023-07-23 09:42:19 -0400

    a

---
+++ src/arc164/a.cc
@@ -0,0 +1,82 @@
+#ifndef ONLINE_JUDGE
+#define _GLIBCXX_DEBUG
+#endif
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ld = long double;
+using ull = unsigned long long;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+template <typename T, typename S>
+ostream &operator<<(ostream &os, const pair<S, T> a) {
+  os << "{" << a.first << ", " << a.second << "}";
+  return os;
+}
+template <typename T> ostream &operator<<(ostream &os, const set<T> &v) {
+  os << "{";
+  string s[2] = {", ", "}"};
+  int size = v.size(), i = 0;
+  for (auto e : v) {
+    os << e << s[i == (size - 1)];
+    i++;
+  }
+  return os;
+}
+
+template <typename T> ostream &operator<<(ostream &os, const vector<T> &v) {
+  os << "[";
+  if (v.size() == 0) {
+    os << "]";
+    return os;
+  }
+  string s[2] = {", ", "]"};
+  rep(i, v.size()) os << v[i] << s[i == int(v.size()) - 1];
+  return os;
+}
+
+void debug_() { cout << endl; }
+
+template <typename Head, typename... Tail> void debug_(Head H, Tail... T) {
+  cout << H << " ";
+  debug_(T...);
+}
+#ifndef ONLINE_JUDGE
+#define debug(...) debug_(__VA_ARGS__)
+#else
+#define debug(...)
+#endif
+
+bool solve(ll n, ll k) {
+  if(n < k)
+    return false;
+  int count = 0;
+  while(n) {
+    ll offset = 1ll;
+    while(offset * 3ll <=n) {
+      offset *= 3ll;
+    }
+    n -= offset;
+    count++;
+  }
+  //cout << "a" << count << endl;
+  if(k < count)
+    return false;
+  // k >= count
+  return (k-count) % 2ll==0;
+}
+
+// #ifdef ONLINE_JUDGE
+int main() {
+  int t;
+  cin >> t;
+  rep(_, t) {
+    ll n, k;
+    cin >> n >> k;
+    if(solve(n, k))
+      cout << "Yes\n";
+    else
+      cout << "No\n";
+  }
+  return 0;
+}
+// #endif
