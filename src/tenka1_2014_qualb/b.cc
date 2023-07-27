3d0b49461bd61c4f3bbfe85fe63cf265cfa65401
Ryotaro Nakamura <nakamura.ryotaro.kzs@gmail.com>
2023-07-27 13:42:32 -0400

    b

---
+++ src/tenka1_2014_qualb/b.cc
@@ -0,0 +1,160 @@
+#ifndef ONLINE_JUDGE
+#define _GLIBCXX_DEBUG
+#endif
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ld = long double;
+using ull = unsigned long long;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+template <typename T, typename S>
+ostream &operator<<(ostream &os, const pair<S, T> a) {
+  os << "{" << a.first << ", " << a.second << "}";
+  return os;
+}
+template <typename T> ostream &operator<<(ostream &os, const set<T> &v) {
+  os << "{";
+  string s[2] = {", ", "}"};
+  int size = v.size(), i = 0;
+  for (auto e : v) {
+    os << e << s[i == (size - 1)];
+    i++;
+  }
+  return os;
+}
+
+template <typename T> ostream &operator<<(ostream &os, const vector<T> &v) {
+  os << "[";
+  if (v.size() == 0) {
+    os << "]";
+    return os;
+  }
+  string s[2] = {", ", "]"};
+  rep(i, v.size()) os << v[i] << s[i == int(v.size()) - 1];
+  return os;
+}
+
+void debug_() { cout << endl; }
+
+template <typename Head, typename... Tail> void debug_(Head H, Tail... T) {
+  cout << H << " ";
+  debug_(T...);
+}
+#ifndef ONLINE_JUDGE
+#define debug(...) debug_(__VA_ARGS__)
+#else
+#define debug(...)
+#endif
+
+template <ll P> class Mod {
+public:
+  ll v;
+  Mod<P>() : v(0) {}
+  Mod<P>(ll v) : v(v % P) {}
+  ll get() { return v; }
+  Mod<P> operator*(Mod<P> &a) { return Mod<P>(v * a.v % P); }
+  Mod<P> operator*(ll &b) { return Mod<P>(v * b % P); }
+  friend Mod<P> operator+(const Mod<P> &lhs, const ll &rhs) {
+    return Mod((lhs.v + rhs) % P);
+  }
+  friend Mod<P> operator+(const Mod<P> &lhs, const Mod<P> &rhs) {
+    return Mod((lhs.v + rhs.v) % P);
+  }
+  friend Mod<P> operator-(const Mod<P> &lhs, const Mod<P> &rhs) {
+    ll res = lhs.v - rhs.v;
+    if (res < 0)
+      res += P;
+    return Mod<P>(res);
+  }
+  friend Mod<P> operator*(const Mod<P> &lhs, const Mod<P> &rhs) {
+    return Mod<P>(lhs.v * rhs.v % P);
+  }
+  friend Mod<P> operator/(const Mod<P> &lhs, const Mod<P> &rhs) {
+    ll b = P, u = 1, v = 0, a = rhs.v;
+    while (b) {
+      ll t = a / b;
+      a -= t * b;
+      swap(a, b);
+      u -= t * v;
+      swap(u, v);
+    }
+    u %= P;
+    if (u < 0)
+      u += P;
+    return lhs.v * u % P;
+  }
+  Mod<P> &operator=(const ll &number) {
+    v = number;
+    return *this;
+  }
+  Mod<P> &operator+=(const Mod &rhs) {
+    v += rhs.v;
+    v %= P;
+    return *this;
+  }
+  Mod<P> &operator-=(const Mod &rhs) {
+    v -= rhs.v;
+    if (v < 0)
+      v += P;
+    return *this;
+  }
+  Mod<P> &operator*=(const Mod &rhs) {
+    v *= rhs.v;
+    v %= P;
+    return *this;
+  }
+  Mod<P> &operator++() {
+    v++;
+    v %= P;
+    return *this;
+  }
+  friend bool operator==(const Mod<P> &lhs, const int &rhs) {
+    return lhs.v == rhs;
+  }
+  friend ostream &operator<<(ostream &os, const Mod<P> &mod) {
+    os << mod.v;
+    return os;
+  }
+  friend istream &operator>>(istream &is,  Mod<P> &mod) {
+    is >> mod.v;
+    return is;
+  }
+
+  Mod<P> pow(ll n) {
+    ll res = 1ll;
+    ll x = v;
+    while (n) {
+      if (n & 1) {
+        res *= x;
+        res %= P;
+      }
+      n >>= 1;
+      x *= x;
+      x %= P;
+    }
+    return res;
+  }
+};
+// g++ -fdiagnostics-color=always -g -O0 --std=c++17 -Wall
+// -fsanitize=undefined,address
+ll const MOD = 1000000007ll;
+int main() {
+  int n;
+  string s;
+  cin >> n >> s;
+  vector<string> tv(n);
+  rep(i, n) cin >> tv[i];
+  vector<Mod<MOD>> dp(size(s)+1, 0);
+  dp[0] = 1;
+  rep(i, size(s)) {
+    for(string &t: tv) {
+      if(size(t) <= size(s) - i) {
+	if(t == string(begin(s)+i, begin(s) + i + size(t))) {
+	  dp[i+size(t)] += dp[i];
+	}
+      }
+    }
+  }
+  cout << dp[size(s)]<< endl;
+  return 0;
+}
