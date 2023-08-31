a862c82673b71047d9cf076d12d9a76b20d146a6
Ryotaro Nakamura <nakamura.ryotaro.kzs@gmail.com>
2023-08-30 20:15:58 -0400

    donumts2015c

---
+++ src/donuts2015/c.cc
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ld = long double;
+using ull = unsigned long long;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#ifndef ONLINE_JUDGE
+#define dbg(...)                                                               \
+  cerr << "\e[91m" << __func__ << ":" << __LINE__ << " " << #__VA_ARGS__       \
+       << " = ";                                                               \
+  debug_(__VA_ARGS__);
+#else
+#define dbg(...)
+#endif
+template <typename Os, typename... Ts>
+Os &operator<<(Os &os, const pair<Ts...> &p) {
+  return os << "{" << p.first << ", " << p.second << "}";
+}
+template <typename Os, typename T>
+typename enable_if<is_same<Os, ostream>::value, Os &>::type
+operator<<(Os &os, const T &v) {
+  os << "[";
+  string sep = "";
+  for (auto &x : v) {
+    os << sep << x;
+    sep = ", ";
+  };
+  return os << "]";
+}
+
+void debug_() { cerr << "\e[39m" << endl; }
+
+template <typename Head, typename... Tail> void debug_(Head H, Tail... T) {
+  cerr << H << " ";
+  debug_(T...);
+}
+int main() {
+  int n;
+  cin >> n;
+  vector<int> hv(n);
+  rep(i, n) cin >> hv[i];
+  vector<int>stk;
+  rep(i, n) {
+    cout << stk.size() <<endl;
+    while(stk.size() && stk.back() < hv[i])
+      stk.pop_back();
+    stk.push_back(hv[i]);
+  }
+  return 0;
+}
