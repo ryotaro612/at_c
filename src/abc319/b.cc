16d6c9ebead160b0cf2ca285ff45726b9fa7418f
Ryotaro Nakamura <nakamura.ryotaro.kzs@gmail.com>
2023-09-10 16:38:33 -0400

    b

---
+++ src/abc319/b.cc
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ld = long double;
+using ull = unsigned long long;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#ifndef ONLINE_JUDGE
+#define dbg(...)                                                               \
+  cerr << "\e[91m" << __func__ << ":" << __LINE__ << " " << #__VA_ARGS__       \
+       << " = ";                                                               \
+  debug_(__VA_ARGS__);
+#else
+#define dbg(...)
+#endif
+template <typename Os, typename... Ts>
+Os &operator<<(Os &os, const pair<Ts...> &p) {
+  return os << "{" << p.first << ", " << p.second << "}";
+}
+template <typename Os, typename T>
+typename enable_if<is_same<Os, ostream>::value, Os &>::type
+operator<<(Os &os, const T &v) {
+  os << "[";
+  string sep = "";
+  for (auto &x : v) {
+    os << sep << x;
+    sep = ", ";
+  };
+  return os << "]";
+}
+
+void debug_() { cerr << "\e[39m" << endl; }
+
+template <typename Head, typename... Tail> void debug_(Head H, Tail... T) {
+  cerr << H << " ";
+  debug_(T...);
+}
+int main() {
+  int n;
+  cin >> n;
+  for (int i = 0; i <= n; i++) {
+    for (int j = 1; j <= 9; j++) {
+      if (n % j == 0 && (i % (n / j) == 0)) {
+        cout << j;
+        break;
+      }
+      if (j == 9)
+        cout << "-";
+    }
+  }
+  cout << endl;
+  return 0;
+}
