679a57c412285e711d1797bda7434a7ed575fd55
Ryotaro Nakamura <nakamura.ryotaro.kzs@gmail.com>
2023-09-10 16:38:28 -0400

    a

---
+++ src/abc319/a.cc
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ld = long double;
+using ull = unsigned long long;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#ifndef ONLINE_JUDGE
+#define dbg(...)                                                               \
+  cerr << "\e[91m" << __func__ << ":" << __LINE__ << " " << #__VA_ARGS__       \
+       << " = ";                                                               \
+  debug_(__VA_ARGS__);
+#else
+#define dbg(...)
+#endif
+template <typename Os, typename... Ts>
+Os &operator<<(Os &os, const pair<Ts...> &p) {
+  return os << "{" << p.first << ", " << p.second << "}";
+}
+template <typename Os, typename T>
+typename enable_if<is_same<Os, ostream>::value, Os &>::type
+operator<<(Os &os, const T &v) {
+  os << "[";
+  string sep = "";
+  for (auto &x : v) {
+    os << sep << x;
+    sep = ", ";
+  };
+  return os << "]";
+}
+
+void debug_() { cerr << "\e[39m" << endl; }
+
+template <typename Head, typename... Tail> void debug_(Head H, Tail... T) {
+  cerr << H << " ";
+  debug_(T...);
+}
+int main() {
+  map<string, int> mp;
+  mp["tourist"] = 3858;
+  mp["ksun48"] = 3679;
+  mp["Benq"] = 3658;
+  mp["Um_nik"] = 3648;
+  mp["apiad"] = 3638;
+  mp["Stonefeang"] = 3630;
+  mp["ecnerwala"] = 3613;
+  mp["mnbvmar"] = 3555;
+  mp["newbiedmy"] = 3516;
+  mp["semiexp"] = 3481;
+  string s;
+  cin >> s;
+  cout << mp[s] << endl;
+  return 0;
+}
