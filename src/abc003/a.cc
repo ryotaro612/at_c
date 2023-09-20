235609dacd3a1d9d9bed7d059d20e1169ab044c0
Ryotaro Nakamura <nakamura.ryotaro.kzs@gmail.com>
2023-09-20 14:15:59 -0400

    a

---
+++ src/abc003/a.cc
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+#include <cstdio>
+#include <iomanip>
+using namespace std;
+using ll = long long;
+using ld = long double;
+using ull = unsigned long long;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#ifndef ONLINE_JUDGE
+#define dbg(...)							\
+  cerr << "\e[91m" << __func__ << ":" << __LINE__ << " " << #__VA_ARGS__ \
+  << " = ";								\
+  debug_(__VA_ARGS__);
+#else
+#define dbg(...)
+#endif
+template <typename Os, typename... Ts>
+Os &operator<<(Os &os, const pair<Ts...> &p) {
+  return os << "{" << p.first << ", " << p.second << "}";
+}
+template <typename Os, typename T>
+typename enable_if<is_same<Os, ostream>::value, Os &>::type
+operator<<(Os &os, const T &v) {
+  os << "[";
+  string sep = "";
+  for (auto &x : v) {
+    os << sep << x;
+    sep = ", ";
+  };
+  return os << "]";
+}
+
+void debug_() { cerr << "\e[39m" << endl; }
+
+template <typename Head, typename... Tail> void debug_(Head H, Tail... T) {
+  cerr << H << " ";
+  debug_(T...);
+}
+int main() {
+  int n;
+  cin >> n;
+  ld temp = 0;
+  for(int i = 1;i<=n;i++) {
+    temp += (ld)1/(ld)n * (ld) i;
+  }
+  cout << fixed << setprecision(12) << temp * 10000.0 << endl;
+  return 0;
+}
