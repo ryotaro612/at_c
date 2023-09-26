c075546d3326ade0deb5dab80374a1971bc48a15
Ryotaro Nakamura <nakamura.ryotaro.kzs@gmail.com>
2023-09-26 12:19:21 -0400

    036

---
+++ src/abc036/d.cc
@@ -0,0 +1,163 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ld = long double;
+using ull = unsigned long long;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#ifndef ONLINE_JUDGE
+#define dbg(...)                                                               \
+  cerr << "\e[91m" << __func__ << ":" << __LINE__ << " " << #__VA_ARGS__       \
+       << " = ";                                                               \
+  debug_(__VA_ARGS__);
+#else
+#define dbg(...)
+#endif
+template <typename Os, typename... Ts>
+Os &operator<<(Os &os, const pair<Ts...> &p) {
+  return os << "{" << p.first << ", " << p.second << "}";
+}
+template <typename Os, typename T>
+typename enable_if<is_same<Os, ostream>::value, Os &>::type
+operator<<(Os &os, const T &v) {
+  os << "[";
+  string sep = "";
+  for (auto &x : v) {
+    os << sep << x;
+    sep = ", ";
+  };
+  return os << "]";
+}
+
+void debug_() { cerr << "\e[39m" << endl; }
+
+template <typename Head, typename... Tail> void debug_(Head H, Tail... T) {
+  cerr << H << " ";
+  debug_(T...);
+}
+
+template <ll P> class Mod {
+public:
+  Mod() : v(0) {}
+  Mod(ll v_) : v((v_ % P + P) % P) {}
+  ll get() { return v; }
+  Mod<P> operator*(Mod<P> &a) { return Mod<P>(v * a.v % P); }
+  Mod<P> operator*(ll &b) { return Mod<P>(v * b % P); }
+  friend Mod<P> operator+(const Mod<P> &lhs, const ll &rhs) {
+    return Mod((lhs.v + rhs) % P);
+  }
+  friend Mod<P> operator+(const Mod<P> &lhs, const Mod<P> &rhs) {
+    return Mod((lhs.v + rhs.v) % P);
+  }
+  friend Mod<P> operator-(const Mod<P> &lhs, const Mod<P> &rhs) {
+    ll res = lhs.v - rhs.v;
+    if (res < 0)
+      res += P;
+    return Mod<P>(res);
+  }
+  friend Mod<P> operator*(const Mod<P> &lhs, const Mod<P> &rhs) {
+    return Mod<P>(lhs.v * rhs.v % P);
+  }
+  friend Mod<P> operator/(const Mod<P> &lhs, const Mod<P> &rhs) {
+    ll b = P, u = 1, vv = 0, a = rhs.v;
+    while (b) {
+      ll t = a / b;
+      a -= t * b;
+      swap(a, b);
+      u -= t * vv;
+      swap(u, vv);
+    }
+    u %= P;
+    if (u < 0)
+      u += P;
+    return lhs.v * u % P;
+  }
+  Mod<P> &operator=(const ll &number) {
+    v = number;
+    return *this;
+  }
+  Mod<P> &operator+=(const Mod &rhs) {
+    v += rhs.v;
+    v %= P;
+    return *this;
+  }
+  Mod<P> &operator-=(const Mod &rhs) {
+    v -= rhs.v;
+    if (v < 0)
+      v += P;
+    return *this;
+  }
+  Mod<P> &operator*=(const Mod &rhs) {
+    v *= rhs.v;
+    v %= P;
+    return *this;
+  }
+  Mod<P> &operator++() {
+    v++;
+    v %= P;
+    return *this;
+  }
+  friend bool operator==(const Mod<P> &lhs, const int &rhs) {
+    return lhs.v == rhs;
+  }
+  friend ostream &operator<<(ostream &os, const Mod<P> &mod) {
+    os << mod.v;
+    return os;
+  }
+  friend istream &operator>>(istream &is, Mod<P> &mod) {
+    is >> mod.v;
+    return is;
+  }
+
+  Mod<P> pow(ll n) {
+    ll res = 1ll;
+    ll x = v;
+    while (n) {
+      if (n & 1) {
+        res *= x;
+        res %= P;
+      }
+      n >>= 1;
+      x *= x;
+      x %= P;
+    }
+    return res;
+  }
+
+private:
+  ll v;
+};
+
+const ll mod = 1000000007ll;
+void rec(int node, int parent, vector<vector<int>> &g,
+         vector<vector<Mod<mod>>> &dp) {
+  if (g[node].size() == 1 && g[node][0] == parent) {
+    dp[node][0] = dp[node][1] = 1;
+    return;
+  }
+  Mod<mod> black = 1, white = 1;
+  for (auto child : g[node]) {
+    if (child == parent)
+      continue;
+    rec(child, node, g, dp);
+    black *= dp[child][0];
+    white *= (dp[child][0] + dp[child][1]);
+  }
+  dp[node][0] = white;
+  dp[node][1] = black;
+}
+
+int main() {
+  int n;
+  cin >> n;
+  vector<vector<int>> g(n);
+
+  rep(i, n - 1) {
+    int a, b;
+    cin >> a >> b;
+    g[--a].push_back(--b);
+    g[b].push_back(a);
+  }
+  vector<vector<Mod<mod>>> dp(n, vector<Mod<mod>>(2));
+  rec(0, -1, g, dp);
+  cout << dp[0][0] + dp[0][1] << endl;
+}
