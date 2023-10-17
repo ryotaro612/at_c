aaf60dcf0d009a35f753805852b194f3a81347d2
Ryotaro Nakamura <nakamura.ryotaro.kzs@gmail.com>
2023-10-17 15:05:09 -0400

    b

---
+++ src/abc323/b.cc
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ld = long double;
+using ull = unsigned long long;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#ifndef ONLINE_JUDGE
+#define dbg(...)                                                               \
+  cerr << "\e[91m" << __func__ << ":" << __LINE__ << " " << #__VA_ARGS__       \
+       << " = ";                                                               \
+  debug_(__VA_ARGS__);
+#else
+#define dbg(...)
+#endif
+template <typename Os, typename... Ts>
+Os &operator<<(Os &os, const pair<Ts...> &p) {
+  return os << "{" << p.first << ", " << p.second << "}";
+}
+template <typename Os, typename T>
+typename enable_if<is_same<Os, ostream>::value, Os &>::type
+operator<<(Os &os, const T &v) {
+  os << "[";
+  string sep = "";
+  for (auto &x : v) {
+    os << sep << x;
+    sep = ", ";
+  };
+  return os << "]";
+}
+
+void debug_() { cerr << "\e[39m" << endl; }
+
+template <typename Head, typename... Tail> void debug_(Head H, Tail... T) {
+  cerr << H << " ";
+  debug_(T...);
+}
+int main() {
+  int n;
+  cin >> n;
+  vector<string> sv(n);
+  vector<pair<int, int>> a(n);
+  rep(i, n) {
+    string s;
+    cin >> s;
+    int cnt = 0;
+    rep(j, n) {
+      if (j != i) {
+        if (s[j] == 'o')
+          cnt--;
+      }
+    }
+    a[i] = {cnt, i};
+  }
+  sort(begin(a), end(a));
+  rep(i, n) {
+    cout << a[i].second + 1 << " \n"[i == n-1];
+  }
+
+  return 0;
+}
