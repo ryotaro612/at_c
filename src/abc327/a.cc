6b0ba3579e292df76d5ee86ca5007edd49e3d1d7
Ryotaro Nakamura <nakamura.ryotaro.kzs@gmail.com>
2023-11-04 21:50:09 -0400

    a

---
+++ src/abc327/a.cc
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ld = long double;
+using ull = unsigned long long;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#ifndef ONLINE_JUDGE
+#define dbg(...)							\
+  cerr << "\e[91m" << __func__ << ":" << __LINE__ << " " << #__VA_ARGS__ \
+  << " = ";								\
+  debug_(__VA_ARGS__);
+#else
+#define dbg(...)
+#endif
+template <typename Os, typename... Ts>
+Os &operator<<(Os &os, const pair<Ts...> &p) {
+  return os << "{" << p.first << ", " << p.second << "}";
+}
+template <typename Os, typename T>
+typename enable_if<is_same<Os, ostream>::value, Os &>::type
+operator<<(Os &os, const T &v) {
+  os << "[";
+  string sep = "";
+  for (auto &x : v) {
+    os << sep << x;
+    sep = ", ";
+  };
+  return os << "]";
+}
+
+void debug_() { cerr << "\e[39m" << endl; }
+
+template <typename Head, typename... Tail> void debug_(Head H, Tail... T) {
+  cerr << H << " ";
+  debug_(T...);
+}
+int main() {
+  int n;
+  string s;
+  cin >> n >> s;
+  rep(i, s.size() -1){
+    if((s[i] == 'a' && s[i+1] == 'b' )|| (s[i] == 'b' && s[i+1] == 'a' )) {
+      cout << "Yes\n";
+      return 0;
+    }
+  }
+  cout << "No\n";
+  
+  return 0;
+}
