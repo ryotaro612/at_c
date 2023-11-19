4ee4518343a7dd9d94f4578a87df639772e8baae
Ryotaro Nakamura <nakamura.ryotaro.kzs@gmail.com>
2023-11-19 16:53:39 -0500

    e

---
+++ src/abc329/e.cc
@@ -0,0 +1,76 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ld = long double;
+using ull = unsigned long long;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#ifndef ONLINE_JUDGE
+#define dbg(...)                                                               \
+  cerr << "\e[91m" << __func__ << ":" << __LINE__ << " " << #__VA_ARGS__       \
+       << " = ";                                                               \
+  debug_(__VA_ARGS__);
+#else
+#define dbg(...)
+#endif
+template <typename Os, typename... Ts>
+Os &operator<<(Os &os, const pair<Ts...> &p) {
+  return os << "{" << p.first << ", " << p.second << "}";
+}
+template <typename Os, typename T>
+typename enable_if<is_same<Os, ostream>::value, Os &>::type
+operator<<(Os &os, const T &v) {
+  os << "[";
+  string sep = "";
+  for (auto &x : v) {
+    os << sep << x;
+    sep = ", ";
+  };
+  return os << "]";
+}
+
+void debug_() { cerr << "\e[39m" << endl; }
+
+template <typename Head, typename... Tail> void debug_(Head H, Tail... T) {
+  cerr << H << " ";
+  debug_(T...);
+}
+int main() {
+  int n, m;
+  cin >> n >> m;
+  string s, t;
+  cin >> s >> t;
+
+  vector<vector<bool>> dp(n, vector<bool>(m, false));
+
+  rep(i, n) {
+    rep(j, m) {
+      if (s[i] == t[j]) {
+        if (i && j && dp[i - 1][j - 1]) {
+          dp[i][j] = true;
+        } else if (i && dp[i - 1][m - 1]) {
+          dp[i][j] = true;
+        } else if (j == 0) {
+          if (i) {
+            bool ok = false;
+            rep(k, m) { ok = ok || dp[i - 1][k]; }
+            if (ok)
+              dp[i][j] = true;
+          } else {
+            dp[i][j] = true;
+          }
+        }
+      }
+    }
+  }
+  // rep(i, n) {
+  //   rep(j, m) cout << (dp[i][j] == true) << " ";
+  //   cout << endl;
+  // }
+
+  if (dp[n - 1][m - 1])
+    cout << "Yes";
+  else
+    cout << "No";
+  cout << endl;
+  return 0;
+}
