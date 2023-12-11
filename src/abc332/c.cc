593cd03f3bf6c95cac40f9100e2c1df2fb0fd6a9
Ryotaro Nakamura <nakamura.ryotaro.kzs@gmail.com>
2023-12-11 12:35:19 -0500

    c

---
+++ src/abc332/c.cc
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ld = long double;
+using ull = unsigned long long;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#ifndef ONLINE_JUDGE
+#define dbg(...)                                                               \
+  cerr << "\e[91m" << __func__ << ":" << __LINE__ << " " << #__VA_ARGS__       \
+       << " = ";                                                               \
+  debug_(__VA_ARGS__);
+#else
+#define dbg(...)
+#endif
+template <typename Os, typename... Ts>
+Os &operator<<(Os &os, const pair<Ts...> &p) {
+  return os << "{" << p.first << ", " << p.second << "}";
+}
+template <typename Os, typename T>
+typename enable_if<is_same<Os, ostream>::value, Os &>::type
+operator<<(Os &os, const T &v) {
+  os << "[";
+  string sep = "";
+  for (auto &x : v) {
+    os << sep << x;
+    sep = ", ";
+  };
+  return os << "]";
+}
+
+void debug_() { cerr << "\e[39m" << endl; }
+
+template <typename Head, typename... Tail> void debug_(Head H, Tail... T) {
+  cerr << H << " ";
+  debug_(T...);
+}
+int main() {
+  int n, m;
+  string s;
+  cin >> n >> m >> s;
+
+  for (int i = 0;; i++) {
+    bool ok = true;
+    int c_muji = m, c_logo = i;
+    for (auto c : s) {
+      if (c == '0') {
+        c_muji = m;
+        c_logo = i;
+      } else if (c == '1') {
+        if (c_muji)
+          c_muji--;
+        else if (c_logo) {
+          c_logo--;
+        } else {
+          ok = false;
+          break;
+        }
+      } else {
+        if (c_logo) {
+          c_logo--;
+        } else {
+          ok = false;
+          break;
+        }
+      }
+    }
+    if (ok) {
+      cout << i << endl;
+      return 0;
+    }
+  }
+  return 0;
+}
