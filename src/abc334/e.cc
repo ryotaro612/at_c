fdaea33771fb3186170e97d60e8f46b48d890787
Ryotaro Nakamura <nakamura.ryotaro.kzs@gmail.com>
2023-12-26 21:57:54 -0500

    e

---
+++ src/abc334/e.cc
@@ -0,0 +1,236 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ld = long double;
+using ull = unsigned long long;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#ifndef ONLINE_JUDGE
+#define dbg(...)                                                               \
+  cerr << "\e[91m" << __func__ << ":" << __LINE__ << " " << #__VA_ARGS__       \
+       << " = ";                                                               \
+  debug_(__VA_ARGS__);
+#else
+#define dbg(...)
+#endif
+template <typename Os, typename... Ts>
+Os &operator<<(Os &os, const pair<Ts...> &p) {
+  return os << "{" << p.first << ", " << p.second << "}";
+}
+template <typename Os, typename T>
+typename enable_if<is_same<Os, ostream>::value, Os &>::type
+operator<<(Os &os, const T &v) {
+  os << "[";
+  string sep = "";
+  for (auto &x : v) {
+    os << sep << x;
+    sep = ", ";
+  };
+  return os << "]";
+}
+
+void debug_() { cerr << "\e[39m" << endl; }
+
+template <typename Head, typename... Tail> void debug_(Head H, Tail... T) {
+  cerr << H << " ";
+  debug_(T...);
+}
+class UnionFind {
+public:
+  vector<int> par;
+  /**
+   *  グループの総数
+   */
+  UnionFind(int n) { par = vector<int>(n, -1); }
+
+  int find_root(int a) {
+    if (par[a] < 0)
+      return a;
+    return par[a] = find_root(par[a]);
+  }
+
+  int is_same_group(int a, int b) { return find_root(a) == find_root(b); }
+
+  void unite(int a, int b) {
+    if (is_same_group(a, b))
+      return;
+    int root_a = find_root(a), root_b = find_root(b);
+    if (group_size(root_a) > group_size(b)) {
+      par[root_a] += par[root_b];
+      par[root_b] = root_a;
+    } else {
+      par[root_b] += par[root_a];
+      par[root_a] = root_b;
+    }
+  }
+  int group_size(int a) { return -par[find_root(a)]; }
+  /**
+   * グループの総数を返す。
+   */
+  int count_groups() {
+    unordered_set<int> groups;
+    rep(i, par.size()) { groups.insert(find_root(i)); }
+    return groups.size();
+  }
+};
+
+template <ll P> class Mod {
+public:
+  Mod() : v(0) {}
+  Mod(ll v_) : v((v_ % P + P) % P) {}
+  ll get() { return v; }
+  Mod<P> operator*(Mod<P> &a) { return Mod<P>(v * a.v % P); }
+  Mod<P> operator*(ll &b) { return Mod<P>(v * b % P); }
+  friend Mod<P> operator+(const Mod<P> &lhs, const ll &rhs) {
+    return Mod((lhs.v + rhs) % P);
+  }
+  friend Mod<P> operator+(const Mod<P> &lhs, const Mod<P> &rhs) {
+    return Mod((lhs.v + rhs.v) % P);
+  }
+  friend Mod<P> operator-(const Mod<P> &lhs, const Mod<P> &rhs) {
+    ll res = lhs.v - rhs.v;
+    if (res < 0)
+      res += P;
+    return Mod<P>(res);
+  }
+  Mod<P> operator-() { return (-v + P) % P; }
+  friend Mod<P> operator*(const Mod<P> &lhs, const Mod<P> &rhs) {
+    return Mod<P>(lhs.v * rhs.v % P);
+  }
+  friend Mod<P> operator/(const Mod<P> &lhs, const Mod<P> &rhs) {
+    ll b = P, u = 1, vv = 0, a = rhs.v;
+    while (b) {
+      ll t = a / b;
+      a -= t * b;
+      swap(a, b);
+      u -= t * vv;
+      swap(u, vv);
+    }
+    u %= P;
+    if (u < 0)
+      u += P;
+    return lhs.v * u % P;
+  }
+  Mod<P> &operator=(const ll &number) {
+    v = number;
+    return *this;
+  }
+  Mod<P> &operator+=(const Mod &rhs) {
+    v += rhs.v;
+    v %= P;
+    return *this;
+  }
+  Mod<P> &operator-=(const Mod &rhs) {
+    v -= rhs.v;
+    if (v < 0)
+      v += P;
+    return *this;
+  }
+  Mod<P> &operator*=(const Mod &rhs) {
+    v *= rhs.v;
+    v %= P;
+    return *this;
+  }
+  Mod<P> &operator++() {
+    v++;
+    v %= P;
+    return *this;
+  }
+  friend bool operator==(const Mod<P> &lhs, const int &rhs) {
+    return lhs.v == rhs;
+  }
+  friend bool operator==(const Mod<P> &lhs, const Mod<P> &rhs) {
+    return lhs.v == rhs.v;
+  }
+  friend ostream &operator<<(ostream &os, const Mod<P> &mod) {
+    os << mod.v;
+    return os;
+  }
+  friend istream &operator>>(istream &is, Mod<P> &mod) {
+    is >> mod.v;
+    return is;
+  }
+
+  Mod<P> pow(ll n) {
+    ll res = 1ll;
+    ll x = v;
+    while (n) {
+      if (n & 1) {
+        res *= x;
+        res %= P;
+      }
+      n >>= 1;
+      x *= x;
+      x %= P;
+    }
+    return res;
+  }
+
+private:
+  ll v;
+};
+const ll MOD = 998244353ll;
+int main() {
+  int h, w;
+  cin >> h >> w;
+  vector<string> grid(h);
+  UnionFind uf(h * w);
+  rep(i, h) cin >> grid[i];
+  int count = 0;
+  rep(r, h) {
+    rep(c, w) {
+      if (grid[r][c] == '.') {
+        count++;
+        continue;
+      }
+
+      int cur = r * w + c;
+      if (r && grid[r - 1][c] == '#') {
+        uf.unite((r - 1) * w + c, cur);
+      }
+      if (c < w - 1 && grid[r][c + 1] == '#') {
+        uf.unite(r * w + c + 1, cur);
+      }
+      if (r < h - 1 && grid[r + 1][c] == '#') {
+        uf.unite((r + 1) * w + c, cur);
+      }
+      if (c && grid[r][c - 1] == '#') {
+        uf.unite(r * w + c - 1, cur);
+      }
+    }
+  }
+
+  Mod<MOD> prob(1);
+  prob = prob / count;
+  Mod<MOD> total = uf.count_groups() - count;
+  Mod<MOD> res = 0;
+  dbg(total, count);
+  rep(r, h) {
+    rep(c, w) {
+      if (grid[r][c] == '#')
+        continue;
+      set<int> groups;
+      if (r && grid[r - 1][c] == '#') {
+        groups.insert(uf.find_root((r - 1) * w + c));
+      }
+      if (c < w - 1 && grid[r][c + 1] == '#') {
+        groups.insert(uf.find_root(r * w + c + 1));
+      }
+      if (r < h - 1 && grid[r + 1][c] == '#') {
+        groups.insert(uf.find_root((r + 1) * w + c));
+      }
+      if (c && grid[r][c - 1] == '#') {
+        groups.insert(uf.find_root(r * w + c - 1));
+      }
+      if (groups.size()) {
+        Mod<MOD> a = total - (groups.size() - 1);
+	dbg(r, c, a);
+        res += a * prob;
+      } else {
+	dbg(r, c, total+1);	
+	res += (total + 1) * prob;
+      }
+    }
+  }
+  cout << res << endl;
+  return 0;
+}
