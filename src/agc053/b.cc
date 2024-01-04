34af16f9c3500d31dbd0a502b6027f011c3c2c90
Ryotaro Nakamura <nakamura.ryotaro.kzs@gmail.com>
2024-01-04 12:53:30 -0500

    b

---
+++ src/agc053/b.cc
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ld = long double;
+using ull = unsigned long long;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#ifndef ONLINE_JUDGE
+#define dbg(...)                                                               \
+  cerr << "\e[91m" << __func__ << ":" << __LINE__ << " " << #__VA_ARGS__       \
+       << " = ";                                                               \
+  debug_(__VA_ARGS__);
+#else
+#define dbg(...)
+#endif
+template <typename Os, typename... Ts>
+Os &operator<<(Os &os, const pair<Ts...> &p) {
+  return os << "{" << p.first << ", " << p.second << "}";
+}
+template <typename Os, typename T>
+typename enable_if<is_same<Os, ostream>::value, Os &>::type
+operator<<(Os &os, const T &v) {
+  os << "[";
+  string sep = "";
+  for (auto &x : v) {
+    os << sep << x;
+    sep = ", ";
+  };
+  return os << "]";
+}
+
+void debug_() { cerr << "\e[39m" << endl; }
+
+template <typename Head, typename... Tail> void debug_(Head H, Tail... T) {
+  cerr << H << " ";
+  debug_(T...);
+}
+int main() {
+  int n;
+  cin >> n;
+  vector<ll> vv(2 * n);
+  ll res = 0;  
+  rep(i, 2 * n){
+    cin >> vv[i];
+    res += vv[i];
+  } 
+  multiset<ll> st;
+
+  for (int i = 0; i < n; i++) {
+    st.insert(vv[n - 1 - i]);
+    st.insert(vv[n + i]);
+    res -= *begin(st);
+    st.erase(begin(st));
+  }
+
+  cout << res << endl;
+  return 0;
+}
