e901fb7dd2727239a1d9714f65964b20d6e4bcc6
Ryotaro Nakamura <nakamura.ryotaro.kzs@gmail.com>
2024-01-06 17:23:59 -0500

    b

---
+++ src/abc335/b.cc
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ld = long double;
+using ull = unsigned long long;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#ifndef ONLINE_JUDGE
+#define dbg(...)                                                               \
+  cerr << "\e[91m" << __func__ << ":" << __LINE__ << " " << #__VA_ARGS__       \
+       << " = ";                                                               \
+  debug_(__VA_ARGS__);
+#else
+#define dbg(...)
+#endif
+template <typename Os, typename... Ts>
+Os &operator<<(Os &os, const pair<Ts...> &p) {
+  return os << "{" << p.first << ", " << p.second << "}";
+}
+template <typename Os, typename T>
+typename enable_if<is_same<Os, ostream>::value, Os &>::type
+operator<<(Os &os, const T &v) {
+  os << "[";
+  string sep = "";
+  for (auto &x : v) {
+    os << sep << x;
+    sep = ", ";
+  };
+  return os << "]";
+}
+
+void debug_() { cerr << "\e[39m" << endl; }
+
+template <typename Head, typename... Tail> void debug_(Head H, Tail... T) {
+  cerr << H << " ";
+  debug_(T...);
+}
+int main() {
+  int n;
+  cin >> n;
+  vector<tuple<int, int, int>> res;
+  rep(i, n + 1) {
+    rep(j, n + 1) {
+      rep(k, n + 1) {
+        if (i + j + k <= n)
+          res.push_back({i, j, k});
+      }
+    }
+  }
+  sort(begin(res), end(res));
+  for (auto [a, b, c] : res) {
+    cout << a << " " << b << " " << c << endl;
+  }
+  return 0;
+}
