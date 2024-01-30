f8fa8c1fccbdee419c110d8893d0c45221dbe914
Ryotaro Nakamura <nakamura.ryotaro.kzs@gmail.com>
2024-01-30 12:49:18 -0500

    a

---
+++ src/abc338/a.cc
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ld = long double;
+using ull = unsigned long long;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#ifndef ONLINE_JUDGE
+#define dbg(...)							\
+  cerr << "\e[91m" << __func__ << ":" << __LINE__ << " " << #__VA_ARGS__ \
+  << " = ";								\
+  debug_(__VA_ARGS__);
+#else
+#define dbg(...)
+#endif
+template <typename Os, typename... Ts>
+Os &operator<<(Os &os, const pair<Ts...> &p) {
+  return os << "{" << p.first << ", " << p.second << "}";
+}
+template <typename Os, typename T>
+typename enable_if<is_same<Os, ostream>::value, Os &>::type
+operator<<(Os &os, const T &v) {
+  os << "[";
+  string sep = "";
+  for (auto &x : v) {
+    os << sep << x;
+    sep = ", ";
+  };
+  return os << "]";
+}
+
+void debug_() { cerr << "\e[39m" << endl; }
+
+template <typename Head, typename... Tail> void debug_(Head H, Tail... T) {
+  cerr << H << " ";
+  debug_(T...);
+}
+int main() {
+  string s;
+  cin >> s;
+  if('A' <= s[0] && s[0] <= 'Z') {
+    for(int i = 1;i<(int)s.size();i++) {
+      if(!('a' <=s[i] && s[i] <= 'z')) {
+	cout << "No\n";
+	return 0;
+      }
+    }
+    cout << "Yes\n";
+  } else {
+    cout << "No\n";
+    return 0;
+  }
+  return 0;
+}
