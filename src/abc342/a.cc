6ddee56a28d3871ce7575bdc3f563c48ba0ff287
Ryotaro Nakamura <ryotaro612.ooo@gmail.com>
2024-02-28 23:34:29 +0900

    a

---
+++ src/abc342/a.cc
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ld = long double;
+using ull = unsigned long long;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#ifndef ONLINE_JUDGE
+#define dbg(...)                                                               \
+  cerr << "\e[91m" << __func__ << ":" << __LINE__ << " " << #__VA_ARGS__       \
+       << " = ";                                                               \
+  debug_(__VA_ARGS__);
+#else
+#define dbg(...)
+#endif
+template <typename Os, typename... Ts>
+Os &operator<<(Os &os, const pair<Ts...> &p) {
+  return os << "{" << p.first << ", " << p.second << "}";
+}
+template <typename Os, typename T>
+typename enable_if<is_same<Os, ostream>::value, Os &>::type
+operator<<(Os &os, const T &v) {
+  os << "[";
+  string sep = "";
+  for (auto &x : v) {
+    os << sep << x;
+    sep = ", ";
+  };
+  return os << "]";
+}
+
+void debug_() { cerr << "\e[39m" << endl; }
+
+template <typename Head, typename... Tail> void debug_(Head H, Tail... T) {
+  cerr << H << " ";
+  debug_(T...);
+}
+int main() {
+  string s;
+  char freq[26] = {0};
+  cin >> s;
+  rep(i, s.size()) { freq[s[i] - 'a']++; }
+  char c;
+  rep(i, 26) {
+    if (freq[i] == 1)
+      c = i + 'a';
+  }
+  rep(i, s.size()) {
+    if (s[i] == c) {
+      cout << i + 1 << endl;
+    }
+  }
+
+  return 0;
+}
