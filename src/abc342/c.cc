3257481f97ff2f1c89580e97f7ecfe90ab88f84b
Ryotaro Nakamura <ryotaro612.ooo@gmail.com>
2024-03-06 01:04:03 +0900

    c

---
+++ src/abc342/c.cc
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ld = long double;
+using ull = unsigned long long;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#ifndef ONLINE_JUDGE
+#define dbg(...)                                                               \
+  cerr << "\e[91m" << __func__ << ":" << __LINE__ << " " << #__VA_ARGS__       \
+       << " = ";                                                               \
+  debug_(__VA_ARGS__);
+#else
+#define dbg(...)
+#endif
+template <typename Os, typename... Ts>
+Os &operator<<(Os &os, const pair<Ts...> &p) {
+  return os << "{" << p.first << ", " << p.second << "}";
+}
+template <typename Os, typename T>
+typename enable_if<is_same<Os, ostream>::value, Os &>::type
+operator<<(Os &os, const T &v) {
+  os << "[";
+  string sep = "";
+  for (auto &x : v) {
+    os << sep << x;
+    sep = ", ";
+  };
+  return os << "]";
+}
+
+void debug_() { cerr << "\e[39m" << endl; }
+
+template <typename Head, typename... Tail> void debug_(Head H, Tail... T) {
+  cerr << H << " ";
+  debug_(T...);
+}
+int main() {
+  int n, q;
+  string s;
+  cin >> n >> s >> q;
+  char mp[26];
+  rep(i, 26) { mp[i] = i + 'a'; }
+  rep(_, q) {
+    char c, d;
+    cin >> c >> d;
+    rep(i, 26) {
+      if (mp[i] == c)
+        mp[i] = d;
+    }
+  }
+  rep(i, s.size()) { s[i] = mp[s[i] - 'a']; }
+  cout << s << endl;
+  return 0;
+}
