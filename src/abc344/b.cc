137eb4945e2e48752699cdcd9b790e2209f01705
Ryotaro Nakamura <ryotaro612.ooo@gmail.com>
2024-03-11 23:44:01 +0900

    b

---
+++ src/abc344/b.cc
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ld = long double;
+using ull = unsigned long long;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#ifndef ONLINE_JUDGE
+#define dbg(...)                                                               \
+  cerr << "\e[91m" << __func__ << ":" << __LINE__ << " " << #__VA_ARGS__       \
+       << " = ";                                                               \
+  debug_(__VA_ARGS__);
+#else
+#define dbg(...)
+#endif
+template <typename Os, typename... Ts>
+Os &operator<<(Os &os, const pair<Ts...> &p) {
+  return os << "{" << p.first << ", " << p.second << "}";
+}
+template <typename Os, typename T>
+typename enable_if<is_same<Os, ostream>::value, Os &>::type
+operator<<(Os &os, const T &v) {
+  os << "[";
+  string sep = "";
+  for (auto &x : v) {
+    os << sep << x;
+    sep = ", ";
+  };
+  return os << "]";
+}
+
+void debug_() { cerr << "\e[39m" << endl; }
+
+template <typename Head, typename... Tail> void debug_(Head H, Tail... T) {
+  cerr << H << " ";
+  debug_(T...);
+}
+int main() {
+  vector<ll> res;
+  ll a = -1;
+  while (a != 0) {
+    cin >> a;
+    res.push_back(a);
+  }
+  for (int i = res.size() - 1; i >= 0; i--) {
+    cout << res[i] << endl;
+  }
+  return 0;
+}
