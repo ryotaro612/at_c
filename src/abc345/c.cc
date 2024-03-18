d16d09c1b18e1a448c2006400902058b82002944
Ryotaro Nakamura <ryotaro612.ooo@gmail.com>
2024-03-18 17:39:22 +0900

    c

---
+++ src/abc345/c.cc
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ld = long double;
+using ull = unsigned long long;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#ifndef ONLINE_JUDGE
+#define dbg(...)                                                               \
+  cerr << "\e[91m" << __func__ << ":" << __LINE__ << " " << #__VA_ARGS__       \
+       << " = ";                                                               \
+  debug_(__VA_ARGS__);
+#else
+#define dbg(...)
+#endif
+template <typename Os, typename... Ts>
+Os &operator<<(Os &os, const pair<Ts...> &p) {
+  return os << "{" << p.first << ", " << p.second << "}";
+}
+template <typename Os, typename T>
+typename enable_if<is_same<Os, ostream>::value, Os &>::type
+operator<<(Os &os, const T &v) {
+  os << "[";
+  string sep = "";
+  for (auto &x : v) {
+    os << sep << x;
+    sep = ", ";
+  };
+  return os << "]";
+}
+
+void debug_() { cerr << "\e[39m" << endl; }
+
+template <typename Head, typename... Tail> void debug_(Head H, Tail... T) {
+  cerr << H << " ";
+  debug_(T...);
+}
+int main() {
+  string s;
+  cin >> s;
+  vector<ll> freq(26, 0);
+  for (auto c : s) {
+    freq[c - 'a']++;
+  }
+
+  ll res = 0;
+  rep(i, 26) {
+    if (freq[i] > 1)
+      res = 1;
+  }
+  freq = vector<ll>(26, 0);
+
+  rep(i, s.size()) {
+    rep(j, 26) {
+      if (s[i] - 'a' != j && freq[j]) {
+        res += freq[j];
+      }
+    }
+    freq[s[i] - 'a']++;
+  }
+  cout << res << endl;
+  return 0;
+}
