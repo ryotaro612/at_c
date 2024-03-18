5e6dffccfdcf3d4d4af9ccca7588a0ce264e50ad
Ryotaro Nakamura <ryotaro612.ooo@gmail.com>
2024-03-18 17:07:24 +0900

    b

---
+++ src/abc345/b.cc
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ld = long double;
+using ull = unsigned long long;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#ifndef ONLINE_JUDGE
+#define dbg(...)                                                               \
+  cerr << "\e[91m" << __func__ << ":" << __LINE__ << " " << #__VA_ARGS__       \
+       << " = ";                                                               \
+  debug_(__VA_ARGS__);
+#else
+#define dbg(...)
+#endif
+template <typename Os, typename... Ts>
+Os &operator<<(Os &os, const pair<Ts...> &p) {
+  return os << "{" << p.first << ", " << p.second << "}";
+}
+template <typename Os, typename T>
+typename enable_if<is_same<Os, ostream>::value, Os &>::type
+operator<<(Os &os, const T &v) {
+  os << "[";
+  string sep = "";
+  for (auto &x : v) {
+    os << sep << x;
+    sep = ", ";
+  };
+  return os << "]";
+}
+
+void debug_() { cerr << "\e[39m" << endl; }
+
+template <typename Head, typename... Tail> void debug_(Head H, Tail... T) {
+  cerr << H << " ";
+  debug_(T...);
+}
+int main() {
+  ll x;
+  cin >> x;
+  if (x >= 0) {
+    if (x % 10ll == 0) {
+      cout << x / 10ll << endl;
+      return 0;
+    } else {
+      cout << x / 10ll + 1ll << endl;
+      return 0;
+    }
+  } else {
+      cout << x / 10ll << endl;    
+  }
+  return 0;
+}
