be4328a9b784e5633b10ca29a8cc8fdfdd489bef
Ryotaro Nakamura <ryotaro612.ooo@gmail.com>
2024-03-24 14:51:26 +0900

    c

---
+++ src/abc346/c.cc
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ld = long double;
+using ull = unsigned long long;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#ifndef ONLINE_JUDGE
+#define dbg(...)                                                               \
+  cerr << "\e[91m" << __func__ << ":" << __LINE__ << " " << #__VA_ARGS__       \
+       << " = ";                                                               \
+  debug_(__VA_ARGS__);
+#else
+#define dbg(...)
+#endif
+template <typename Os, typename... Ts>
+Os &operator<<(Os &os, const pair<Ts...> &p) {
+  return os << "{" << p.first << ", " << p.second << "}";
+}
+template <typename Os, typename T>
+typename enable_if<is_same<Os, ostream>::value, Os &>::type
+operator<<(Os &os, const T &v) {
+  os << "[";
+  string sep = "";
+  for (auto &x : v) {
+    os << sep << x;
+    sep = ", ";
+  };
+  return os << "]";
+}
+
+void debug_() { cerr << "\e[39m" << endl; }
+
+template <typename Head, typename... Tail> void debug_(Head H, Tail... T) {
+  cerr << H << " ";
+  debug_(T...);
+}
+int main() {
+  ll n, k, total;
+  cin >> n >> k;
+  total = (k + 1ll) * k / 2ll;
+  set<ll> as;
+  rep(i, n) {
+    ll a;
+    cin >> a;
+    as.insert(a);
+  }
+  for (auto a : as) {
+    if(a<=k)
+      total -= a;
+  }
+  cout << total << endl;
+  return 0;
+}
