d632d8e58eb904c71972aa74c772fa8613555292
Ryotaro <ryotaro612.ooo@gmail.com>
2024-05-18 12:25:46 +0900

    e

---
+++ src/abc352/e.cc
@@ -0,0 +1,111 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ld = long double;
+using ull = unsigned long long;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#ifndef ONLINE_JUDGE
+#define dbg(...)                                                               \
+  cerr << "\e[91m" << __func__ << ":" << __LINE__ << " " << #__VA_ARGS__       \
+       << " = ";                                                               \
+  debug_(__VA_ARGS__);
+#else
+#define dbg(...)
+#endif
+template <typename Os, typename... Ts>
+Os &operator<<(Os &os, const pair<Ts...> &p) {
+  return os << "{" << p.first << ", " << p.second << "}";
+}
+template <typename Os, typename T>
+typename enable_if<is_same<Os, ostream>::value, Os &>::type
+operator<<(Os &os, const T &v) {
+  os << "[";
+  string sep = "";
+  for (auto &x : v) {
+    os << sep << x;
+    sep = ", ";
+  };
+  return os << "]";
+}
+
+void debug_() { cerr << "\e[39m" << endl; }
+
+template <typename Head, typename... Tail> void debug_(Head H, Tail... T) {
+  cerr << H << " ";
+  debug_(T...);
+}
+
+class UnionFind {
+public:
+  vector<int> par;
+  /**
+   *  グループの総数
+   */
+  UnionFind(int n) { par = vector<int>(n, -1); }
+
+  int find_root(int a) {
+    if (par[a] < 0)
+      return a;
+    return par[a] = find_root(par[a]);
+  }
+
+  int is_same_group(int a, int b) { return find_root(a) == find_root(b); }
+
+  void unite(int a, int b) {
+    if (is_same_group(a, b))
+      return;
+    int root_a = find_root(a), root_b = find_root(b);
+    if (group_size(root_a) > group_size(b)) {
+      par[root_a] += par[root_b];
+      par[root_b] = root_a;
+    } else {
+      par[root_b] += par[root_a];
+      par[root_a] = root_b;
+    }
+  }
+  int group_size(int a) { return -par[find_root(a)]; }
+  /**
+   * グループの総数を返す。
+   */
+  int count_groups() {
+    unordered_set<int> groups;
+    rep(i, par.size()) { groups.insert(find_root(i)); }
+    return groups.size();
+  }
+};
+
+int main() {
+  int n, m;
+  cin >> n >> m;
+  vector<pair<ll, vector<int>>> groups;
+  rep(_, m) {
+    int k;
+    ll c;
+    cin >> k >> c;
+    vector<int> kv(k);
+    rep(i, k) {
+      cin >> kv[i];
+      kv[i]--;
+    }
+
+    groups.push_back({c, kv});
+  }
+  sort(begin(groups), end(groups));
+
+  UnionFind uf(n);
+  ll res = 0;
+  for (auto &[c, group] : groups) {
+    rep(i, group.size() - 1) {
+      if (!uf.is_same_group(group[i], group[i + 1])) {
+        res += c;
+        uf.unite(group[i], group[i + 1]);
+      }
+    }
+  }
+  if(uf.group_size(0) == n)
+    cout << res << endl;
+  else
+    cout << -1 << endl;
+
+  return 0;
+}
