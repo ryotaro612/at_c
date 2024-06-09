66c01ccc5b60c9a9537d6d8e0f75afc77b2ad989
Ryotaro <ryotaro612.ooo@gmail.com>
2024-06-09 09:50:42 +0900

    b

---
+++ src/abc357/b.cc
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ld = long double;
+using ull = unsigned long long;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#ifndef ONLINE_JUDGE
+#define dbg(...)														\
+  cerr << "\e[91m" << __func__ << ":" << __LINE__ << " " << #__VA_ARGS__ \
+  << " = ";																\
+  debug_(__VA_ARGS__);
+#else
+#define dbg(...)
+#endif
+template <typename Os, typename... Ts>
+Os &operator<<(Os &os, const pair<Ts...> &p) {
+  return os << "{" << p.first << ", " << p.second << "}";
+}
+template <typename Os, typename T>
+typename enable_if<is_same<Os, ostream>::value, Os &>::type
+operator<<(Os &os, const T &v) {
+  os << "[";
+  string sep = "";
+  for (auto &x : v) {
+    os << sep << x;
+    sep = ", ";
+  };
+  return os << "]";
+}
+
+void debug_() { cerr << "\e[39m" << endl; }
+
+template <typename Head, typename... Tail> void debug_(Head H, Tail... T) {
+  cerr << H << " ";
+  debug_(T...);
+}
+int main() {
+  string s;
+
+
+  cin >> s;
+  int n_upper = 0;
+  for(auto c: s) {
+	if('A' <= c && c <= 'Z')
+	  n_upper++;
+  }
+  if(n_upper < (int)s.size() - n_upper) {
+	for(auto &c: s) {
+	  c = tolower(c);
+	}	
+	
+  } else {
+	for(auto &c: s) {
+	  c = toupper(c);
+	}
+
+  }
+  cout << s << endl;  
+  return 0;
+}
