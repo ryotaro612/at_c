cb65b59319a9ab98a14164417ca23601f6334439
Ryotaro <ryotaro612.ooo@gmail.com>
2024-06-09 09:50:48 +0900

    c

---
+++ src/abc357/c.cc
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ld = long double;
+using ull = unsigned long long;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#ifndef ONLINE_JUDGE
+#define dbg(...)														\
+  cerr << "\e[91m" << __func__ << ":" << __LINE__ << " " << #__VA_ARGS__ \
+  << " = ";																\
+  debug_(__VA_ARGS__);
+#else
+#define dbg(...)
+#endif
+template <typename Os, typename... Ts>
+Os &operator<<(Os &os, const pair<Ts...> &p) {
+  return os << "{" << p.first << ", " << p.second << "}";
+}
+template <typename Os, typename T>
+typename enable_if<is_same<Os, ostream>::value, Os &>::type
+operator<<(Os &os, const T &v) {
+  os << "[";
+  string sep = "";
+  for (auto &x : v) {
+    os << sep << x;
+    sep = ", ";
+  };
+  return os << "]";
+}
+
+void debug_() { cerr << "\e[39m" << endl; }
+
+template <typename Head, typename... Tail> void debug_(Head H, Tail... T) {
+  cerr << H << " ";
+  debug_(T...);
+}
+vector<string> solve(int n) {
+  if(n == 0) {
+	return {"#"};
+  }
+  vector<string> tile = solve(n-1);
+  vector<string> res;
+  rep(i, tile.size()) {
+	string a = string(tile[i]) + string(tile[i]) + string(tile[i]);
+	res.push_back(a);
+  }
+  rep(i, tile.size()) {
+	string a = string(tile[i]);
+	string white = string(a.size(), '.');
+	string b = a + white + a;
+	res.push_back(b);
+  }  
+  rep(i, tile.size()) {
+	string a = string(tile[i]) + string(tile[i]) + string(tile[i]);
+	res.push_back(a);
+  }
+  return res;
+}
+
+int main() {
+
+  int n;
+  cin >> n;
+  vector<string> res = solve(n);
+  for(auto s: res) {
+	cout << s << endl;
+  }
+  return 0;
+}
