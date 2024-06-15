d4d6230a67c72a71ca3581504d0f0eac0249b213
Ryotaro <ryotaro612.ooo@gmail.com>
2024-06-15 23:25:31 +0900

    c

---
+++ src/abc358/c.cc
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ld = long double;
+using ull = unsigned long long;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#ifndef ONLINE_JUDGE
+#define dbg(...)                                                               \
+  cerr << "\e[91m" << __func__ << ":" << __LINE__ << " " << #__VA_ARGS__       \
+       << " = ";                                                               \
+  debug_(__VA_ARGS__);
+#else
+#define dbg(...)
+#endif
+template <typename Os, typename... Ts>
+Os &operator<<(Os &os, const pair<Ts...> &p) {
+  return os << "{" << p.first << ", " << p.second << "}";
+}
+template <typename Os, typename T>
+typename enable_if<is_same<Os, ostream>::value, Os &>::type
+operator<<(Os &os, const T &v) {
+  os << "[";
+  string sep = "";
+  for (auto &x : v) {
+    os << sep << x;
+    sep = ", ";
+  };
+  return os << "]";
+}
+
+void debug_() { cerr << "\e[39m" << endl; }
+
+template <typename Head, typename... Tail> void debug_(Head H, Tail... T) {
+  cerr << H << " ";
+  debug_(T...);
+}
+int main() {
+  int n, m;
+  cin >> n >> m;
+  vector<string> sv(n);
+  rep(i, n) { cin >> sv[i]; }
+  int res = n;
+  rep(i, 1 << n) {
+    int mask = 0;
+    int counter = 0;
+    rep(j, n) {
+      if (i & (1 << j)) {
+        counter++;
+        rep(k, m) {
+          if (sv[j][k] == 'o')
+            mask |= 1 << k;
+        }
+      }
+    }
+    if (mask == ((1 << m) - 1)) {
+      res = min(res, counter);
+    }
+  }
+  cout << res << endl;
+  return 0;
+}
