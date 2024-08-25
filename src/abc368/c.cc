dbd412d73415785129ef4dab805ab33f52668f76
Ryotaro <ryotaro612.ooo@gmail.com>
2024-08-25 23:15:46 +0900

    c

--- src/abc368/c.cc
+++ src/abc368/c.cc
@@ -5,9 +5,9 @@ using ld = long double;
 using ull = unsigned long long;
 #define rep(i, n) for (int i = 0; i < (int)(n); i++)
 #ifndef ONLINE_JUDGE
-#define dbg(...)														\
-  cerr << "\e[91m" << __func__ << ":" << __LINE__ << " " << #__VA_ARGS__ \
-  << " = ";																\
+#define dbg(...)                                                               \
+  cerr << "\e[91m" << __func__ << ":" << __LINE__ << " " << #__VA_ARGS__       \
+       << " = ";                                                               \
   debug_(__VA_ARGS__);
 #else
 #define dbg(...)
@@ -35,5 +35,25 @@ template <typename Head, typename... Tail> void debug_(Head H, Tail... T) {
   debug_(T...);
 }
 int main() {
+  int n;
+  cin >> n;
+  vector<ll> hv(n);
+  rep(i, n) cin >> hv[i];
+  ll t = 0;
+  for (auto e : hv) {
+    if (e > 5ll) {
+      t += (e / 5ll) * 3ll;
+      e %= 5ll;
+    }
+    while (e > 0) {
+      t++;
+      if (t % 3ll == 0)
+        e -= 3ll;
+      else
+        e--;
+    }
+    dbg(t);
+  }
+  cout << t << endl;
   return 0;
 }
