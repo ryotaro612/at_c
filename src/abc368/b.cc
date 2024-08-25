5b3ab01ca8a3dbb5d6a4b63b35f2f0c6c4da15e8
Ryotaro <ryotaro612.ooo@gmail.com>
2024-08-25 11:02:08 +0900

    b

---
+++ src/abc368/b.cc
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ld = long double;
+using ull = unsigned long long;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#ifndef ONLINE_JUDGE
+#define dbg(...)                                                               \
+  cerr << "\e[91m" << __func__ << ":" << __LINE__ << " " << #__VA_ARGS__       \
+       << " = ";                                                               \
+  debug_(__VA_ARGS__);
+#else
+#define dbg(...)
+#endif
+template <typename Os, typename... Ts>
+Os &operator<<(Os &os, const pair<Ts...> &p) {
+  return os << "{" << p.first << ", " << p.second << "}";
+}
+template <typename Os, typename T>
+typename enable_if<is_same<Os, ostream>::value, Os &>::type
+operator<<(Os &os, const T &v) {
+  os << "[";
+  string sep = "";
+  for (auto &x : v) {
+    os << sep << x;
+    sep = ", ";
+  };
+  return os << "]";
+}
+
+void debug_() { cerr << "\e[39m" << endl; }
+
+template <typename Head, typename... Tail> void debug_(Head H, Tail... T) {
+  cerr << H << " ";
+  debug_(T...);
+}
+int main() {
+  int n;
+  cin >> n;
+  vector<int> av(n);
+  rep(i, n) cin >> av[i];
+
+  int res = 0;
+  while (true) {
+    int n_pos = 0;
+    rep(i, n) {
+      if (av[i] > 0)
+        n_pos++;
+    }
+	dbg(av);
+    if (n_pos <= 1) {
+      cout << res << endl;
+      return 0;
+    }
+    sort(begin(av), end(av));
+    reverse(begin(av), end(av));
+    av[0]--;
+    av[1]--;
+    res++;
+  }
+
+  return 0;
+}
