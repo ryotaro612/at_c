cd8915f614fc29f3e8bd51abcdf9acda0f04ae5e
Ryotaro <ryotaro612.ooo@gmail.com>
2024-09-01 13:47:17 +0900

    c

---
+++ src/abc369/c.cc
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ld = long double;
+using ull = unsigned long long;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#ifndef ONLINE_JUDGE
+#define dbg(...)                                                               \
+  cerr << "\e[91m" << __func__ << ":" << __LINE__ << " " << #__VA_ARGS__       \
+       << " = ";                                                               \
+  debug_(__VA_ARGS__);
+#else
+#define dbg(...)
+#endif
+template <typename Os, typename... Ts>
+Os &operator<<(Os &os, const pair<Ts...> &p) {
+  return os << "{" << p.first << ", " << p.second << "}";
+}
+template <typename Os, typename T>
+typename enable_if<is_same<Os, ostream>::value, Os &>::type
+operator<<(Os &os, const T &v) {
+  os << "[";
+  string sep = "";
+  for (auto &x : v) {
+    os << sep << x;
+    sep = ", ";
+  };
+  return os << "]";
+}
+
+void debug_() { cerr << "\e[39m" << endl; }
+
+template <typename Head, typename... Tail> void debug_(Head H, Tail... T) {
+  cerr << H << " ";
+  debug_(T...);
+}
+int main() {
+  int n;
+  cin >> n;
+  vector<ll> av(n);
+  rep(i, n) cin >> av[i];
+  ll res = 0;
+  for (int left = 0, right = 0; left < n; left++) {
+	dbg(left, right);
+    right = max(left, right);
+
+    while (right < n-1 && (left == right || av[left + 1] - av[left] ==
+                                              av[right + 1] - av[right])) {
+	  dbg(right);
+      right++;
+    }
+    res += (ll)(right - left + 1);
+  }
+  cout << res << endl;
+  return 0;
+}
