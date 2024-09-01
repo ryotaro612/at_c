d146a134ac889efdbe896aad3b31015ca1b4f42f
Ryotaro <ryotaro612.ooo@gmail.com>
2024-09-01 12:52:00 +0900

    ax

---
+++ src/abc369/a.cc
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ld = long double;
+using ull = unsigned long long;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#ifndef ONLINE_JUDGE
+#define dbg(...)                                                               \
+  cerr << "\e[91m" << __func__ << ":" << __LINE__ << " " << #__VA_ARGS__       \
+       << " = ";                                                               \
+  debug_(__VA_ARGS__);
+#else
+#define dbg(...)
+#endif
+template <typename Os, typename... Ts>
+Os &operator<<(Os &os, const pair<Ts...> &p) {
+  return os << "{" << p.first << ", " << p.second << "}";
+}
+template <typename Os, typename T>
+typename enable_if<is_same<Os, ostream>::value, Os &>::type
+operator<<(Os &os, const T &v) {
+  os << "[";
+  string sep = "";
+  for (auto &x : v) {
+    os << sep << x;
+    sep = ", ";
+  };
+  return os << "]";
+}
+
+void debug_() { cerr << "\e[39m" << endl; }
+
+template <typename Head, typename... Tail> void debug_(Head H, Tail... T) {
+  cerr << H << " ";
+  debug_(T...);
+}
+int main() {
+  int a, b;
+  cin >> a >> b;
+  if (b < a)
+    swap(a, b);
+  int res = 0;
+  for (int x = -1000; x <= 1000; x++) {
+    if (x < a) {
+      if (a - x == b - a)
+        res++;
+    } else if (a <= x && x <= b) {
+      if (x - a == b - x)
+        res++;
+    } else {
+      if (b - a == x - b)
+        res++;
+    }
+  }
+  cout << res << endl;
+  return 0;
+}
