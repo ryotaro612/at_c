afc9c8e74168abda4228378b1e00c3697e61fb92
Ryotaro <ryotaro612.ooo@gmail.com>
2024-09-29 15:46:07 +0900

    b

---
+++ src/abc373/b.cc
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ld = long double;
+using ull = unsigned long long;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#ifndef ONLINE_JUDGE
+#define dbg(...)														\
+  cerr << "\e[91m" << __func__ << ":" << __LINE__ << " " << #__VA_ARGS__ \
+  << " = ";																\
+  debug_(__VA_ARGS__);
+#else
+#define dbg(...)
+#endif
+template <typename Os, typename... Ts>
+Os &operator<<(Os &os, const pair<Ts...> &p) {
+  return os << "{" << p.first << ", " << p.second << "}";
+}
+template <typename Os, typename T>
+typename enable_if<is_same<Os, ostream>::value, Os &>::type
+operator<<(Os &os, const T &v) {
+  os << "[";
+  string sep = "";
+  for (auto &x : v) {
+    os << sep << x;
+    sep = ", ";
+  };
+  return os << "]";
+}
+
+void debug_() { cerr << "\e[39m" << endl; }
+
+template <typename Head, typename... Tail> void debug_(Head H, Tail... T) {
+  cerr << H << " ";
+  debug_(T...);
+}
+int main() {
+  string s;
+  cin >> s;
+  map<char, int> pos;
+  rep(i, s.size()) {
+	pos[s[i]] = i;
+  }
+  int res = 0;
+  int p = pos['A'];
+  rep(i, 25) {
+	res += abs(pos['A' + i + 1] - p);
+	p = pos['A' + i + 1];
+  }
+  cout << res << endl;
+  return 0;
+}
