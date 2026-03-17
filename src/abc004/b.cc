0fdfabea3c1916f4a4c5e6276dafec3cec5d7397
Ryotaro Nakamura <ryotaro612.ooo@gmail.com>
2026-03-17 13:24:05 +0900

    b

---
+++ src/abc004/b.cc
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <set>
+#include <string>
+#include <vector>
+using namespace std;
+using ll = long long;
+using ld = long double;
+using ull = unsigned long long;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#ifndef ONLINE_JUDGE
+#define dbg(...)                                                               \
+  cerr << "\e[91m" << __func__ << ":" << __LINE__ << " " << #__VA_ARGS__       \
+       << " = ";                                                               \
+  debug_(__VA_ARGS__);
+#else
+#define dbg(...)
+#endif
+template <typename Os, typename... Ts>
+Os &operator<<(Os &os, const pair<Ts...> &p) {
+  return os << "{" << p.first << ", " << p.second << "}";
+}
+template <typename Os, typename T>
+typename enable_if<is_same<Os, ostream>::value, Os &>::type
+operator<<(Os &os, const T &v) {
+  os << "[";
+  string sep = "";
+  for (auto &x : v) {
+    os << sep << x;
+    sep = ", ";
+  };
+  return os << "]";
+}
+
+void debug_() { cerr << "\e[39m" << endl; }
+
+template <typename Head, typename... Tail> void debug_(Head H, Tail... T) {
+  cerr << H << " ";
+  debug_(T...);
+}
+int main() {
+  vector<string> sv(4);
+  rep(i, 4) { getline(cin, sv[i]); }
+  dbg(sv);
+
+  for (int i = 3; i >= 0; i--) {
+    reverse(begin(sv[i]), end(sv[i]));
+    cout << sv[i] << endl;
+  }
+  return 0;
+}
