01b5239a0ebc54f00a8fd1fa51e4464c76c47586
Ryotaro Nakamura <ryotaro612.ooo@gmail.com>
2026-08-20 02:32:16 +0900

    a

---
+++ src/abc019/a.py
@@ -0,0 +1,15 @@
+import os
+import sys
+from collections import defaultdict, deque
+
+sys.setrecursionlimit(10**7)
+
+
+def dbg(*args):
+    if not os.environ.get("ATCODER"):
+        print("\033[92m {}\033[00m".format(args))
+
+
+al = list(map(int, input().split()))
+
+print(sorted(al)[1])
