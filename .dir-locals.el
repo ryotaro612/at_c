27deacd8507ba612475198cca7c54edd48467944
Ryotaro Nakamura <ryotaro612.ooo@gmail.com>
2026-09-12 01:35:43 +0900

    temp

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,11 +1,13 @@
 ;;; Directory Local Variables            -*- no-byte-compile: t -*-
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((python-mode . ((eglot-server-programs . (((python-mode python-ts-mode) . ("uv" "run" "pyright-langserver" "--stdio"))))
-		 (eval . (reformatter-define atc-py-format
-					     :program "uv"
-					     :args '("run" "ruff" "format" "-")))
-		 (eval . (atc-py-format-on-save-mode))))
+ ((python-ts-mode  
+  . ((eglot-server-programs  
+      . (((python-mode python-ts-mode) . ("uv" "run" "pyright-langserver" "--stdio"))))
+     ;; apheleia をこのプロジェクト用フォーマッタに割り当てる  
+     (eval . (setf (alist-get 'atc-uv-ruff apheleia-formatters)  
+                   '("uv" "run" "ruff" "format" "--stdin-filename" filepath "-")))  
+     (apheleia-formatter . atc-uv-ruff)))  
  (c++-mode . ((eglot-server-programs . (((c++-mode)
                                          "clangd" 
                                          "--query-driver=/opt/homebrew/bin/g++-15"
